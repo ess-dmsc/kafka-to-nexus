2e82c02127c34ab67b143d2b4c27cbb146beca12
Dominik Werder <dominik.werder@psi.ch>
2017-02-16 14:23:03 +0100

    Small changes while testing

--- src/helper.cxx
+++ src/helper.cxx
@@ -1,10 +1,17 @@
 #include "helper.h"
+#include "logger.h"
 #include <fstream>
+#include <unistd.h>
 
 std::vector<char> gulp(std::string fname) {
+	std::array<char, 256> cwd;
+	getcwd(cwd.data(), cwd.size());
+	LOG(3, "gulp: {}  cwd: {}", fname, cwd.data());
 	std::vector<char> ret;
 	std::ifstream ifs(fname, std::ios::binary | std::ios::ate);
 	auto n1 = ifs.tellg();
+	LOG(3, "try to resize: {}", n1);
+	if (n1 <= 0) return ret;
 	ret.resize(n1);
 	ifs.seekg(0);
 	ifs.read(ret.data(), n1);
