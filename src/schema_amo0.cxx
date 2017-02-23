33f996be96f5502828678f1afda0be5259ac3b8d
Michele Brambilla <michele.brambilla@psi.ch>
2017-02-23 15:27:08 +0100

    initial writing of event data

--- src/schema_amo0.cxx
+++ src/schema_amo0.cxx
@@ -4,6 +4,7 @@
 #include <hdf5.h>
 #include "schemas/amo0_psi_sinq_schema_generated.h"
 
+#include <iterator>
 #include <iostream>
 
 namespace BrightnESS {
@@ -23,15 +24,16 @@ std::string sourcename_impl(Msg msg) override;
 uint64_t ts_impl(Msg msg) override;
 };
 
+
 class writer : public FBSchemaWriter {
 ~writer() override;
 void init_impl(std::string const & sourcename, Msg msg) override;
 WriteResult write_impl(Msg msg) override;
-using DT = double;
-// DataSet::getId() will be -1 for the default constructed
-hid_t ds = -1;
-hid_t dsp = -1;
-hid_t dcpl = -1;
+  hid_t grp_event = -1;
+  hid_t to,id,tz,ei; //datasets
+  hid_t dsp = -1; // dataspace
+  hid_t dpl = -1; // data properties list
+  uint64_t pid = -1;
 };
 
 std::unique_ptr<FBSchemaWriter> reader::create_writer_impl() {
@@ -40,6 +42,7 @@ std::unique_ptr<FBSchemaWriter> reader::create_writer_impl() {
 
 std::string reader::sourcename_impl(Msg msg) {
   auto event = EventGenerator::FlatBufs::AMOR::GetEvent(msg.data);
+  
   if (!event->htype()) {
     LOG(3, "WARNING message has no source name");
     return "";
@@ -69,134 +72,124 @@ void writer::init_impl(std::string const & sourcename, Msg msg) {
 	// // TODO
 	// // This is just a unbuffered, low-performance write.
 	// // Add buffering after it works.
-	// auto file = hdf_file->h5file_detail().h5file();
-	// auto epicspv = FlatBufs::f141_epics_nt::GetEpicsPV(msg.data);
-	// // TODO verify buffer
-	// if (epicspv->pv_type() != FlatBufs::f141_epics_nt::PV::NTScalarArrayDouble) {
-	// 	LOG(2, "SORRY cant handle that at the moment");
-	// 	return;
-	// }
-	// auto pv = (FlatBufs::f141_epics_nt::NTScalarArrayDouble*) epicspv->pv();
-	// if (!pv) {
-	// 	LOG(2, "ERROR no data was sent");
-	// 	return;
-	// }
-	// auto value = pv->value();
-	// if (!value) {
-	// 	LOG(2, "ERROR no value");
-	// 	return;
-	// }
-	// auto ncols = value->size();
-	// LOG(1, "f141_epics_nt::init_impl  v.size() == {}", ncols);
-	// using std::vector;
-	// using std::array;
-	// auto dt = nat_type<DT>();
-	// // H5S_UNLIMITED
-	// std::array<hsize_t, 2> sizes_ini {{0, ncols}};
-	// std::array<hsize_t, 2> sizes_max {{H5S_UNLIMITED, H5S_UNLIMITED}};
+	auto file = hdf_file->h5file_detail().h5file();
+	hsize_t ncols = 1000000;
+	LOG(1, "amo0::init_impl  v.size() == {}", ncols);
+	// NEEDS:
+	// event_time_offset : event->data()[0:nelem-1]
+	// event_id : event->data()[nelem:2*nelem-1]
+	// event_time_zero : timestamp
+	// event_index : ???
+	
+	grp_event = H5Gcreate1(file,"NXEvent_data",0);
 
-	// // TODO
-	// // Lifetime?  Must it live as long as dataset exists?
-	// this->dsp = H5Screate_simple(sizes_ini.size(), sizes_ini.data(), sizes_max.data());
-	// if (true) {
-	// 	// Just check if it works as I think it should
-	// 	LOG(0, "DataSpace isSimple {}", H5Sis_simple(dsp));
-	// 	auto ndims = H5Sget_simple_extent_ndims(dsp);
-	// 	LOG(0, "DataSpace getSimpleExtentNdims {}", ndims);
-	// 	LOG(0, "DataSpace getSimpleExtentNpoints {}", H5Sget_simple_extent_npoints(dsp));
-	// 	std::vector<hsize_t> get_sizes_now;
-	// 	std::vector<hsize_t> get_sizes_max;
-	// 	get_sizes_now.resize(ndims);
-	// 	get_sizes_max.resize(ndims);
-	// 	H5Sget_simple_extent_dims(dsp, get_sizes_now.data(), get_sizes_max.data());
-	// 	for (int i1 = 0; i1 < ndims; ++i1) {
-	// 		LOG(0, "H5Sget_simple_extent_dims {:3} {:3}", get_sizes_now.at(i1), get_sizes_max.at(i1));
-	// 	}
-	// }
+	{
+	  auto dt = nat_type<uint32_t>();
+	  std::array<hsize_t, 1> sizes_ini {{0}};
+	  std::array<hsize_t, 1> sizes_max {{H5S_UNLIMITED}};
+	  this->dsp = H5Screate_simple(sizes_ini.size(), sizes_ini.data(), sizes_max.data());
 
-	// // TODO
-	// // Lifetime of this guy?
-	// // Together with the dataset?
-	// this->dcpl = H5Pcreate(H5P_DATASET_CREATE);
-	// std::array<hsize_t, 2> sizes_chk {{std::max(64*1024/H5Tget_size(dt)/ncols, (size_t)1), ncols}};
-	// H5Pset_chunk(dcpl, sizes_chk.size(), sizes_chk.data());
-	// this->ds = H5Dcreate1(file, sourcename.c_str(), dt, dsp, dcpl);
+	  if(true) {
+	    LOG(0, "\tDataSpace isSimple {}", H5Sis_simple(dsp));
+	    auto ndims = H5Sget_simple_extent_ndims(dsp);
+	    LOG(0, "\tDataSpace getSimpleExtentNdims {}", ndims);
+	    LOG(0, "\tDataSpace getSimpleExtentNpoints {}", 
+	  	H5Sget_simple_extent_npoints(dsp));
+	    std::vector<hsize_t> get_sizes_now;
+	    std::vector<hsize_t> get_sizes_max;
+	    get_sizes_now.resize(ndims);
+	    get_sizes_max.resize(ndims);
+	    H5Sget_simple_extent_dims(dsp, get_sizes_now.data(), get_sizes_max.data());
+	    for (int i1 = 0; i1 < ndims; ++i1) {
+	      LOG(0, "\tH5Sget_simple_extent_dims {:3} {:3}", 
+	  	  get_sizes_now.at(i1), get_sizes_max.at(i1));
+	    }
+	  }
+
+	  this->dpl = H5Pcreate(H5P_DATASET_CREATE);
+	  std::array<hsize_t, 1> sizes_chk {{ncols}};
+	  H5Pset_chunk(dpl, sizes_chk.size(), sizes_chk.data());
+	  this->to = H5Dcreate1(grp_event,"event_time_offset", dt, dsp, dpl);
+	  this->id = H5Dcreate1(grp_event,"event_id", dt, dsp, dpl);
+	  H5Sclose(dsp);
+	  H5Pclose(dpl);
+	}
+	{
+	  auto dt = nat_type<uint64_t>();
+	  std::array<hsize_t, 1> sizes_ini {{0}};
+	  std::array<hsize_t, 1> sizes_max {{H5S_UNLIMITED}};
+	  this->dsp = H5Screate_simple(sizes_ini.size(), sizes_ini.data(), sizes_max.data());
+	  this->dpl = H5Pcreate(H5P_DATASET_CREATE);
+	  std::array<hsize_t, 1> sizes_chk {{ncols}};
+	  H5Pset_chunk(dpl, sizes_chk.size(), sizes_chk.data());
+	  this->tz = H5Dcreate1(grp_event,"event_time_zero", dt, dsp, dpl);
+	  this->ei = H5Dcreate1(grp_event,"event_index", dt, dsp, dpl);
+	  H5Sclose(dsp);
+	  H5Pclose(dpl);
+	}
+
 }
 
 
 WriteResult writer::write_impl(Msg msg) {
+  using A = std::array<hsize_t, 1>;
+
   auto event = EventGenerator::FlatBufs::AMOR::GetEvent(msg.data);
-  std::cout << "PID: " << event->pid() << std::endl;
-	// LOG(0, "f141_epics_nt::write_impl");
-	// // TODO cache these values later, but for now, let's make it verbose:
+  if( (event->pid() != pid+1) && (pid < -1) ) {
+    LOG(0, "amo0 stream event loss: {} -> {}", pid, event->pid());
+    // TODO write into nexus log
+  }
+  pid = event->pid();
 
-	// // Just being explicit here
-	// auto & ds = this->ds;
-	// auto tgt = H5Dget_space(ds);
-	// auto ndims = H5Sget_simple_extent_ndims(tgt);
-	// std::vector<hsize_t> get_sizes_now;
-	// std::vector<hsize_t> get_sizes_max;
-	// get_sizes_now.resize(ndims);
-	// get_sizes_max.resize(ndims);
-	// H5Sget_simple_extent_dims(tgt, get_sizes_now.data(), get_sizes_max.data());
-	// for (int i1 = 0; i1 < ndims; ++i1) {
-	// 	LOG(0, "H5Sget_simple_extent_dims {:3} {:3}", get_sizes_now.at(i1), get_sizes_max.at(i1));
-	// }
 
-	// auto epicspv = FlatBufs::f141_epics_nt::GetEpicsPV(msg.data);
-	// auto pv = (FlatBufs::f141_epics_nt::NTScalarArrayDouble*)epicspv->pv();
-	// if (pv->value()->size() != get_sizes_now.at(1)) {
-	// 	LOG(7, "ERROR this message is not compatible with the previous ones");
-	// 	return {-1};
-	// }
+  A event_size = {{event->data()->size()/2}};
+  auto dt = nat_type<uint32_t>();
 
-	// get_sizes_now.at(0) += 1;
-	// herr_t err;
-	// err = H5Dextend(ds, get_sizes_now.data());
-	// if (err < 0) {
-	// 	LOG(7, "ERROR can not extend dataset");
-	// 	return {-1};
-	// }
-	// H5Sclose(tgt);
+  // later factor out
+  auto & ds = this->to;
+  auto tgt = H5Dget_space(ds);
+  auto ndims = H5Sget_simple_extent_ndims(tgt);
+  LOG(1, "DataSpace getSimpleExtentNdims {}", ndims);
+  LOG(1, "DataSpace getSimpleExtentNpoints {}", 
+      H5Sget_simple_extent_npoints(tgt));
 
-	// tgt = H5Dget_space(ds);
-	// using A = std::array<hsize_t, 2>;
-	// A mem_size = {{1, get_sizes_now.at(1)}};
-	// auto mem = H5Screate_simple(2, mem_size.data(), nullptr);
-	// {
-	// 	A hsl_start {{0, 0}};
-	// 	A hsl_count {{1, get_sizes_now.at(1)}};
-	// 	err = H5Sselect_hyperslab(mem, H5S_SELECT_SET, hsl_start.data(), nullptr, hsl_count.data(), nullptr);
-	// 	if (err < 0) {
-	// 		LOG(7, "ERROR can not select mem hyperslab");
-	// 		return {-1};
-	// 	}
-	// }
-	// {
-	// 	A hsl_start {{get_sizes_now.at(0)-1, 0}};
-	// 	A hsl_count {{1, get_sizes_now.at(1)}};
-	// 	err = H5Sselect_hyperslab(tgt, H5S_SELECT_SET, hsl_start.data(), nullptr, hsl_count.data(), nullptr);
-	// 	if (err < 0) {
-	// 		LOG(7, "ERROR can not select tgt hyperslab");
-	// 		return {-1};
-	// 	}
-	// }
-	// for (int i1 = 0; i1 < ndims; ++i1) {
-	// 	LOG(0, "H5Sget_simple_extent_dims {:3} {:3}", get_sizes_now.at(i1), get_sizes_max.at(i1));
-	// }
-	// auto dt = nat_type<DT>();
-	// err = H5Dwrite(ds, dt, mem, tgt, H5P_DEFAULT, pv->value()->data());
-	// if (err < 0) {
-	// 	LOG(7, "ERROR writing failed");
-	// 	return {-1};
-	// }
-	// H5Sclose(mem);
-	// auto fwdinfo = epicspv->fwdinfo();
-	// if (!fwdinfo) {
-	// 	LOG(3, "ERROR no time data sent");
-	// 	return {-1};
-	// }
-	// return {(int64_t)fwdinfo->ts_data()};
+  A get_sizes_now;
+  A get_sizes_max;
+  H5Sget_simple_extent_dims(tgt, get_sizes_now.data(), get_sizes_max.data());
+  for (uint i1 = 0; i1 < get_sizes_now.size(); ++i1) {
+    LOG(1, "H5Sget_simple_extent_dims {:3} {:3}", get_sizes_now.at(i1), get_sizes_max.at(i1));
+  }
+  H5Sclose(tgt);
+
+  A new_sizes(get_sizes_now);
+  for (uint i1 = 0; i1 < new_sizes.size(); ++i1) {
+    new_sizes.at(i1) += event_size.at(i1);
+  }
+  herr_t err = H5Dextend(ds, new_sizes.data());
+  if (err < 0) {
+    LOG(7, "ERROR can not extend dataset");
+    return {-1};
+  }
+
+  tgt = H5Dget_space(ds);
+  err = H5Sselect_hyperslab(tgt, H5S_SELECT_SET, get_sizes_now.data(), nullptr, 
+			    event_size.data(), nullptr);
+  if (err < 0) {
+    LOG(7, "ERROR can not select mem hyperslab");
+    return {-1};
+  }
+  auto mem = H5Screate_simple(event_size.size(), event_size.data(), nullptr);
+
+  err = H5Dwrite(ds, dt, mem, tgt, H5P_DEFAULT, event->data()->data());
+  if (err < 0) {
+    LOG(7, "ERROR writing failed");
+    return {-1};
+  }
+
+  H5Sclose(mem);
+  H5Sclose(tgt);
+
+  return {(int64_t)event->timestamp()};
 }
 
 
