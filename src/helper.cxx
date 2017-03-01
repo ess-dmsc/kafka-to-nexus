0d7a20f438a1dbf194dd76f898cb8bf31030bee5
Dominik Werder <dominik.werder@psi.ch>
2017-03-01 07:26:54 +0100

    Simplify blob reading

--- src/helper.cxx
+++ src/helper.cxx
@@ -1,17 +1,12 @@
 #include "helper.h"
-#include "logger.h"
-#include <array>
 #include <fstream>
 #include <unistd.h>
 
 std::vector<char> gulp(std::string fname) {
-	std::array<char, 256> cwd;
-	getcwd(cwd.data(), cwd.size());
-	LOG(3, "gulp: {}  cwd: {}", fname, cwd.data());
 	std::vector<char> ret;
 	std::ifstream ifs(fname, std::ios::binary | std::ios::ate);
+	if (!ifs.good()) return ret;
 	auto n1 = ifs.tellg();
-	LOG(3, "try to resize: {}", n1);
 	if (n1 <= 0) return ret;
 	ret.resize(n1);
 	ifs.seekg(0);
