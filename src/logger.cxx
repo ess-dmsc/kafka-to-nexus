77a9d6d4c86114483c760d2987e3ebd5f44ef8fa
Dominik Werder <dominik.werder@psi.ch>
2017-03-01 07:33:08 +0100

    Base path for relative FILE annotation to project root

--- src/logger.cxx
+++ src/logger.cxx
@@ -9,6 +9,6 @@ int log_level = 3;
 
 int prefix_len() {
 	// Yes, depends on the name of this very file without the path:
-	static int n1 = strlen(__FILE__) - strlen("logger.cxx");
+	static int n1 = strlen(__FILE__) - strlen("src/logger.cxx");
 	return n1;
 }
