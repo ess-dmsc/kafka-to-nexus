c84593234b23becb8693de5c90e295ec36042ed0
Dominik Werder <dominik.werder@psi.ch>
2017-03-02 10:49:29 +0100

    Add support for kafka gelf and graylog logger in log interface

--- src/logger.h
+++ src/logger.h
@@ -1,47 +1,40 @@
 #pragma once
 
-#include <stdarg.h>
+#include <string>
 #include <fmt/format.h>
 
 #ifdef _MSC_VER
 
 #define LOG(level, fmt, ...) { \
-	if (level >= log_level) { \
-		dwlog(level, fmt, __FILE__, __LINE__, __FUNCSIG__, __VA_ARGS__); \
-	} \
+	dwlog(level, fmt, __FILE__, __LINE__, __FUNCSIG__, __VA_ARGS__); \
 }
 
 #else
 
 #define LOG(level, fmt, args...) { \
-	if (level >= log_level) { \
-		dwlog(level, fmt, __FILE__, __LINE__, __PRETTY_FUNCTION__, ## args); \
-	} \
+	dwlog(level, fmt, __FILE__, __LINE__, __PRETTY_FUNCTION__, ## args); \
 }
 
 #endif
 
-
-
 extern int log_level;
 
-int prefix_len();
+void dwlog_inner(int level, char const * file, int line, char const * func, std::string const & s1);
 
 template <typename ...TT>
 void dwlog(int level, char const * fmt, char const * file, int line, char const * func, TT const & ... args) {
-	// Here, we assume that this file is still called logger.cpp and lives in the root of the project
-	int npre = prefix_len();
-	int const n2 = strlen(file);
-	if (npre > n2) {
-		fmt::print("ERROR in logging API: npre > n2\n");
-		npre = 0;
-	}
-	auto f1 = file + npre;
-	// TODO merge into one invocation..
+	if (level < log_level) return;
 	try {
-		fmt::print("{}:{}: [{}]  {}\n", f1, line, level, fmt::format(fmt, args...));
+		dwlog_inner(level, file, line, func, fmt::format(fmt, args...));
 	}
 	catch (fmt::FormatError & e) {
-		fmt::print("ERROR  fmt::FormatError {}:{}\n", f1, line);
+		dwlog_inner(level, file, line, func, fmt::format("ERROR in format: {}: {}", e.what(), fmt));
 	}
 }
+
+void use_log_file(std::string fname);
+
+void log_kafka_gelf_start(std::string broker, std::string topic);
+void log_kafka_gelf_stop();
+
+void fwd_graylog_logger_enable(std::string address);
