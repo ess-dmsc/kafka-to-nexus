cd697d3562f8485c03b164cd116e802f544bef31
Dominik Werder <dominik.werder@psi.ch>
2017-03-07 11:06:01 +0100

    Helper binary_to_hex

--- src/helper.cxx
+++ src/helper.cxx
@@ -13,3 +13,19 @@ std::vector<char> gulp(std::string fname) {
 	ifs.read(ret.data(), n1);
 	return ret;
 }
+
+std::vector<char> binary_to_hex(char const * data, int len) {
+	std::vector<char> ret;
+	ret.reserve(len * 5 / 4);
+	for (int i1 = 0; i1 < len; ++i1) {
+		auto c = (uint8_t)data[i1];
+		for (auto & v : std::array<uint8_t, 2>{ {(uint8_t)(c >> 4), (uint8_t)(c & 0x0f)} }) {
+			if (v < 10) v += 48;
+			else v += 97 - 10;
+			ret.push_back(v);
+		}
+		if (i1 %    8  == 7) ret.push_back(' ');
+		if (i1 % (4*8) == 4*8-1) ret.push_back('\n');
+	}
+	return ret;
+}
