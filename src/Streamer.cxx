14c73b6c92241565bbc6e2c56e2835778ab3114d
Dominik Werder <dominik.werder@psi.ch>
2017-03-07 15:49:46 +0100

    Do not reconsume last message.  Fix until new streamer lands.

--- src/Streamer.cxx
+++ src/Streamer.cxx
@@ -51,7 +51,9 @@ BrightnESS::FileWriter::Streamer::Streamer(const std::string &broker,
   }
 
   // sets the current offset
-  get_last_offset();
+  if (offset != RdKafka::Topic::OFFSET_END) {
+    get_last_offset();
+  }
 }
 
 BrightnESS::FileWriter::Streamer::Streamer(const Streamer &other)
