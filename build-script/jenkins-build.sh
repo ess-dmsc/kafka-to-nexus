142526af192583b9fb1cc5759d04f4858f5ed2c6
Dominik Werder <dominik.werder@psi.ch>
2017-03-29 10:48:50 +0200

    Remove from Jenkins build script the install path

--- build-script/jenkins-build.sh
+++ build-script/jenkins-build.sh
@@ -2,4 +2,4 @@ find . -maxdepth 3
 rm -rf build
 mkdir -p build
 cd build
-cmake -DCMAKE_INCLUDE_PATH=../repos/streaming-data-types\;../repos/googletest -DCMAKE_INSTALL_PREFIX=`cd ../install; pwd` -DREQUIRE_GTEST=1 ../repos/kafka-to-nexus  &&  make VERBOSE=1
+cmake -DCMAKE_INCLUDE_PATH=../repos/streaming-data-types\;../repos/googletest -DREQUIRE_GTEST=1 ../repos/kafka-to-nexus  &&  make VERBOSE=1
