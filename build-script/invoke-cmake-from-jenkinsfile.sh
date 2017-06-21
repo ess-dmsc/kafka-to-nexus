d34ddc6da9490899e639ac45ec2acd28363c27fb
Afonso Mukai <afonso.mukai@esss.se>
2017-06-21 14:52:41 +0200

    Add graylog-logger support to Jenkinsfile

--- build-script/invoke-cmake-from-jenkinsfile.sh
+++ build-script/invoke-cmake-from-jenkinsfile.sh
@@ -4,4 +4,5 @@ cmake ../code \
 -DCMAKE_INCLUDE_PATH=../googletest\;../streaming-data-types\;$DM_ROOT/usr/include\;$DM_ROOT/usr/lib \
 -DCMAKE_LIBRARY_PATH=$DM_ROOT/usr/lib \
 -Dflatc=$DM_ROOT/usr/bin/flatc \
--DREQUIRE_GTEST=1
+-DREQUIRE_GTEST=1 \
+-DUSE_GRAYLOG_LOGGER=1
