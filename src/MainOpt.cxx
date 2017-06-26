ess-dmsc/kafka-to-nexus

commit 15c5bf28a2cd8783c8ad27cf2116784edf9dbf2b

Pass command broker/topic as URI

src/MainOpt.cxx
@@ -2,6 +2,7 @@
 #include "uri.h"
 #include <getopt.h>
 
+using BrightnESS::uri::URI;
 
 /**
 Parses the options using getopt and returns a MainOpt
@@ -13,10 +14,9 @@ std::pair<int, std::unique_ptr<MainOpt>> parse_opt(int argc, char ** argv) {
 	g_main_opt.store(opt.get());
 	static struct option long_options[] = {
 		{"help",                            no_argument,              0, 'h'},
-		{"broker-command-address",          required_argument,        0,  0 },
-		{"broker-command-topic",            required_argument,        0,  0 },
+		{"broker-command",                  required_argument,        0,  0 },
 		{"kafka-gelf",                      required_argument,        0,  0 },
-		{"graylog-logger-address",          required_argument,        0,  0 },
+		{"graylog-logger",                  required_argument,        0,  0 },
 		{"teamid",                          required_argument,        0,  0 },
 		{"assets-dir",                      required_argument,        0,  0 },
 		{0, 0, 0, 0},
@@ -44,11 +44,12 @@ std::pair<int, std::unique_ptr<MainOpt>> parse_opt(int argc, char ** argv) {
 			if (std::string("help") == lname) {
 				opt->help = true;
 			}
-			if (std::string("broker-command-address") == lname) {
-				opt->master_config.command_listener.address = optarg;
-			}
-			if (std::string("broker-command-topic") == lname) {
-				opt->master_config.command_listener.topic = optarg;
+			if (std::string("broker-command") == lname) {
+				URI x(optarg);
+				x.default_host("localhost");
+				x.default_port(9092);
+				x.default_path("kafka-to-nexus.command");
+				opt->master_config.command_listener.broker = x;
 			}
 			if (std::string("kafka-gelf") == lname) {
 				opt->kafka_gelf = optarg;

src/CommandListener.cxx
@@ -45,11 +45,11 @@ CommandListener::~CommandListener() {
 void CommandListener::start() {
 	KafkaW::BrokerOpt opt;
 	opt.poll_timeout_ms = 500;
-	opt.address = config.address;
+	opt.address = config.broker.host_port;
 	opt.conf_strings["group.id"] = "kafka-to-nexus.CommandListener";
 	consumer.reset(new KafkaW::Consumer(opt));
 	consumer->on_rebalance_assign = config.on_rebalance_assign;
-	consumer->add_topic(config.topic);
+	consumer->add_topic(config.broker.topic);
 	if (config.start_at_command_offset >= 0) {
 		int n1 = config.start_at_command_offset;
 		consumer->on_rebalance_start = [n1] (rd_kafka_topic_partition_list_t * plist) {

src/CommandListener.h
@@ -3,6 +3,7 @@
 #include <thread>
 #include "Master_handler.h"
 #include "KafkaW.h"
+#include "uri.h"
 
 
 namespace BrightnESS {
@@ -19,10 +20,12 @@ virtual void operator() (int partition, std::string const & topic, std::string c
 
 /// Settings for the Kafka command broker and topic.
 struct CommandListenerConfig {
-std::string address = "localhost:9092";
-std::string topic = "kafka-to-nexus.command";
+uri::URI broker;
 std::function<void()> * on_rebalance_assign = nullptr;
 int64_t start_at_command_offset = -1;
+CommandListenerConfig() :
+	broker("kafka://localhost:9092/kafka-to-nexus.command")
+{ }
 };
 
 class PollStatus {

src/kafka-to-nexus.cxx
@@ -40,13 +40,13 @@ int main(int argc, char ** argv) {
 		       "      Kafka brokers to connect with for configuration updates.\n"
 		       "      Default: %s\n"
 		       "\n",
-			opt->master_config.command_listener.address.c_str());
+			opt->master_config.command_listener.broker.host_port.c_str());
 
 		printf("  --broker-command-topic      <topic-name>\n"
 		       "      Topic name to listen to for configuration updates.\n"
 		       "      Default: %s\n"
 		       "\n",
-			opt->master_config.command_listener.topic.c_str());
+			opt->master_config.command_listener.broker.topic.c_str());
 
 		printf("  --assets-dir                <path>\n"
 		       "      Path where program can find some supplementary files.\n"

src/tests/roundtrip.cxx
@@ -26,16 +26,16 @@ using std::string;
 using std::array;
 using std::vector;
 
-int64_t produce_command_from_string(CommandListenerConfig & config, std::string const & cmd) {
+int64_t produce_command_from_string(uri::URI const & uri, std::string const & cmd) {
 	KafkaW::BrokerOpt opt;
-	opt.address = config.address;
+	opt.address = uri.host_port;
 	KafkaW::Producer p(opt);
 	std::promise<int64_t> offset;
 	std::function<void(rd_kafka_message_t const * msg)> cb = [&offset](rd_kafka_message_t const * msg) {
 		offset.set_value(msg->offset);
 	};
 	p.on_delivery_ok = &cb;
-	KafkaW::Producer::Topic pt(p, config.topic);
+	KafkaW::Producer::Topic pt(p, uri.topic);
 	pt.do_copy();
 	pt.produce((void*)cmd.data(), cmd.size(), nullptr);
 	p.poll_while_outq();
@@ -50,9 +50,9 @@ int64_t produce_command_from_string(CommandListenerConfig & config, std::string 
 
 
 /// Produce a command from a json file
-int64_t produce_command_from_file(CommandListenerConfig & config, std::string file) {
+int64_t produce_command_from_file(uri::URI const & uri, std::string file) {
 	auto v1 = gulp(file.c_str());
-	return produce_command_from_string(config, std::string(v1.data(), v1.size()));
+	return produce_command_from_string(uri, std::string(v1.data(), v1.size()));
 }
 
 
@@ -79,7 +79,7 @@ void roundtrip_simple_01(MainOpt & opt) {
 	Master m(opt.master_config);
 	opt.master = &m;
 	auto fn_cmd = "tests/msg-conf-new-01.json";
-	auto of = produce_command_from_file(opt.master_config.command_listener, fn_cmd);
+	auto of = produce_command_from_file(opt.master_config.command_listener.broker, fn_cmd);
 	opt.master_config.command_listener.start_at_command_offset = of - 1;
 	std::thread t1([&m]{
 		ASSERT_NO_THROW( m.run() );
@@ -156,6 +156,14 @@ void roundtrip_simple_01(MainOpt & opt) {
 }
 
 
+TEST_F(Roundtrip, simple_01) {
+	// disabled
+	return;
+	BrightnESS::FileWriter::Test::roundtrip_simple_01(*opt);
+}
+
+
+
 
 
 void roundtrip_remote_kafka(MainOpt & opt, string fn_cmd) {
@@ -167,7 +175,7 @@ void roundtrip_remote_kafka(MainOpt & opt, string fn_cmd) {
 	using std::string;
 
 	// One broker for commands and data for this test
-	string broker_common = opt.master_config.command_listener.address;
+	auto & broker_common = opt.master_config.command_listener.broker;
 
 	auto json_data = gulp(fn_cmd);
 	Document d;
@@ -178,12 +186,12 @@ void roundtrip_remote_kafka(MainOpt & opt, string fn_cmd) {
 	}
 	auto & a = d.GetAllocator();
 	d.RemoveMember("broker");
-	d.AddMember("broker", Value(broker_common.c_str(), a), a);
+	d.AddMember("broker", Value(broker_common.host_port.c_str(), a), a);
 	StringBuffer cmd_buf1;
 	PrettyWriter<StringBuffer> wr(cmd_buf1);
 	d.Accept(wr);
 
-	auto of = produce_command_from_string(opt.master_config.command_listener, cmd_buf1.GetString());
+	auto of = produce_command_from_string(broker_common, cmd_buf1.GetString());
 	opt.master_config.command_listener.start_at_command_offset = of - 1;
 	Master m(opt.master_config);
 	opt.master = &m;
@@ -208,14 +216,14 @@ void roundtrip_remote_kafka(MainOpt & opt, string fn_cmd) {
 	{
 		// Produce sample data using the nt types scheme only
 		KafkaW::BrokerOpt bopt;
-		bopt.address = broker_common;
+		bopt.address = broker_common.host_port;
 		KafkaW::Producer prod(bopt);
 		for (size_t i3 = 0; i3 < test_sourcenames.size(); ++i3) {
 			KafkaW::Producer::Topic topic(prod, test_topics[i3]);
 			topic.do_copy();
 			auto & sourcename = test_sourcenames[i3];
 			BrightnESS::FileWriter::Msg msg;
-			BrightnESS::FlatBufs::ev42::synth synth(sourcename, 64*1024, 1);
+			BrightnESS::FlatBufs::ev42::synth synth(sourcename, 64, 1);
 
 			for (int i1 = 0; i1 < 32; ++i1) {
 				auto fb = synth.next(i1);
@@ -251,6 +259,6 @@ void roundtrip_remote_kafka(MainOpt & opt, string fn_cmd) {
 }
 
 
-TEST(Roundtrip, ev42_remote_kafka) {
+TEST_F(Roundtrip, ev42_remote_kafka) {
 	BrightnESS::FileWriter::Test::roundtrip_remote_kafka(*Roundtrip::opt, "tests/msg-cmd-new-03.json");
 }

src/tests/tests.cxx
@@ -20,12 +20,6 @@ int main(int argc, char ** argv) {
 
 static_assert(RD_KAFKA_RESP_ERR_NO_ERROR == 0, "Make sure that NO_ERROR is and stays 0");
 
-TEST_F(Roundtrip, simple_01) {
-	// disabled
-	return;
-	BrightnESS::FileWriter::Test::roundtrip_simple_01(*opt);
-}
-
 #if 0
 	if (false) {
 		// test if log messages arrive on all destinations

src/uri.cxx
@@ -28,6 +28,23 @@ uint32_t ol;
 PCRE2_SIZE * ov;
 };
 
+
+void URI::update_deps() {
+	if (port != 0) {
+		host_port = fmt::format("{}:{}", host, port);
+	}
+	else {
+		host_port = host;
+	}
+	// check if the path could be a valid topic
+	auto mdd = pcre2_match_data_create(16, nullptr);
+	if (0 <= pcre2_match(re_topic, (uchar*)path.data(), path.size(), 0, 0, mdd, nullptr)) {
+		topic = MD(mdd).cg(1).substr(path.data());
+	}
+	pcre2_match_data_free(mdd);
+}
+
+
 URI::URI(std::string uri) {
 	using std::vector;
 	using std::string;
@@ -58,23 +75,22 @@ URI::URI(std::string uri) {
 			path = m.cg(0).substr(p0);
 		}
 	}
-	// check if the path could be a valid topic
-	if (0 <= pcre2_match(re_topic, (uchar*)path.data(), path.size(), 0, 0, mdd, nullptr)) {
-		topic = MD(mdd).cg(1).substr(path.data());
-	}
-
 	pcre2_match_data_free(mdd);
+	update_deps();
 }
+
 
 pcre2_code * URI::re1 = nullptr;
 pcre2_code * URI::re_no_host = nullptr;
 pcre2_code * URI::re_topic = nullptr;
+
 
 void p_regerr(int err) {
 	std::array<unsigned char, 512> s1;
 	auto n = pcre2_get_error_message(err, s1.data(), s1.size());
 	fmt::print("err in regex: [{}, {}] {:.{}}\n", err, n, (char*)s1.data(), n);
 }
+
 
 bool URI::compile() {
 	int err = 0;
@@ -108,7 +124,33 @@ bool URI::compile() {
 	return true;
 }
 
+
+void URI::default_port(int port_) {
+	if (port == 0) {
+		port = port_;
+	}
+	update_deps();
+}
+
+
+void URI::default_path(std::string path_) {
+	if (path.size() == 0) {
+		path = path_;
+	}
+	update_deps();
+}
+
+
+void URI::default_host(std::string host_) {
+	if (host.size() == 0) {
+		host = host_;
+	}
+	update_deps();
+}
+
+
 bool URI::compiled = compile();
+
 
 #if HAVE_GTEST
 TEST(URI, host) {
@@ -125,9 +167,19 @@ TEST(URI, host_port) {
 }
 TEST(URI, scheme_host_port) {
 	URI u1("http://my.host:345");
+	u1.default_port(123);
 	ASSERT_EQ(u1.scheme, "http");
 	ASSERT_EQ(u1.host, "my.host");
+	ASSERT_EQ(u1.host_port, "my.host:345");
 	ASSERT_EQ(u1.port, (uint32_t)345);
+}
+TEST(URI, scheme_host_port_default) {
+	URI u1("http://my.host");
+	u1.default_port(123);
+	ASSERT_EQ(u1.scheme, "http");
+	ASSERT_EQ(u1.host, "my.host");
+	ASSERT_EQ(u1.host_port, "my.host:123");
+	ASSERT_EQ(u1.port, (uint32_t)123);
 }
 TEST(URI, scheme_host_port_pathdefault) {
 	URI u1("kafka://my-host.com:8080/");
@@ -197,6 +249,15 @@ TEST(URI, reltopic) {
 	ASSERT_EQ(u1.port, (uint32_t)0);
 	ASSERT_EQ(u1.path, "topic-name.test");
 	ASSERT_EQ(u1.topic, "topic-name.test");
+}
+TEST(URI, host_default_topic) {
+	URI u1("//my.host");
+	u1.default_path("/some-path");
+	ASSERT_EQ(u1.scheme, "");
+	ASSERT_EQ(u1.host, "my.host");
+	ASSERT_EQ(u1.port, (uint32_t)0);
+	ASSERT_EQ(u1.path, "/some-path");
+	ASSERT_EQ(u1.topic, "some-path");
 }
 #endif
 

src/uri.h
@@ -15,17 +15,23 @@ class URI {
 public:
 using uchar = unsigned char;
 URI(std::string uri);
+void default_host(std::string host);
+void default_port(int port);
+void default_path(std::string path);
 bool is_kafka_with_topic() const;
 std::string scheme;
 std::string host;
+std::string host_port;
 uint32_t port = 0;
 std::string path;
 std::string topic;
+private:
 static pcre2_code * re1;
 static pcre2_code * re_no_host;
 static pcre2_code * re_topic;
 static bool compile();
 static bool compiled;
+void update_deps();
 };
 
 }

commit c2513ce9a0696b0da37105fba25e32888de32de3

Fix command line option --graylog-logger-address, bug introduced in 15c5bf2

src/MainOpt.cxx
@@ -62,7 +62,7 @@ std::pair<int, std::unique_ptr<MainOpt>> parse_opt(int argc, char **argv) {
       {"config-file", required_argument, 0, 0},
       {"broker-command", required_argument, 0, 0},
       {"kafka-gelf", required_argument, 0, 0},
-      {"graylog-logger", required_argument, 0, 0},
+      {"graylog-logger-address", required_argument, 0, 0},
       {"use-signal-handler", required_argument, 0, 0},
       {"teamid", required_argument, 0, 0},
       {"assets-dir", required_argument, 0, 0},

src/logger.cxx
@@ -85,7 +85,6 @@ void Logger::log_kafka_gelf_stop() {
 }
 
 void Logger::fwd_graylog_logger_enable(std::string address) {
-#ifdef HAVE_GRAYLOG_LOGGER
   auto addr = address;
   int port = 12201;
   auto col = address.find(":");
@@ -93,6 +92,7 @@ void Logger::fwd_graylog_logger_enable(std::string address) {
     addr = address.substr(0, col);
     port = strtol(address.c_str() + col + 1, nullptr, 10);
   }
+#ifdef HAVE_GRAYLOG_LOGGER
   Log::RemoveAllHandlers();
   LOG(4, "Enable graylog_logger on {}:{}", addr, port);
   Log::AddLogHandler(new GraylogInterface(addr, port));