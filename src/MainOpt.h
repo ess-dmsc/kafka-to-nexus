0bb97651c88ded137a2f053ed12ce816ae12c683
Dominik Werder <dominik.werder@psi.ch>
2017-08-02 14:48:49 +0200

    Add comments on MainOpt members

--- src/MainOpt.h
+++ src/MainOpt.h
@@ -23,22 +23,32 @@ struct MainOpt {
   bool verbose = false;
   bool gtest = false;
   bool use_signal_handler = true;
-  // FileWriter::MasterConfig master_config;
+  /// Kafka options, they get parsed from the configuration file and passed on
+  /// to the StreamMaster.
   std::map<std::string, std::string> kafka;
+  /// Can optionally log in Graylog GELF format to a Kafka topic.
   std::string kafka_gelf;
+  /// Can optionally use the `graylog_logger` library to log to this address.
   std::string graylog_logger_address;
-  std::atomic<FileWriter::Master *> master;
+  /// Used by the signal handler.
+  std::atomic<FileWriter::Master *> master{nullptr};
+  /// The parsed configuration file given by the `--config-file` option.
   rapidjson::Document config_file;
-  // rapidjson::Value const *config_file = nullptr;
-
+  /// Keeps commands contained in the configuration file.  The configuration
+  /// file may contain commands which are executed before any other command
+  /// from the Kafka command topic.
   std::vector<rapidjson::Document> commands_from_config_file;
+  /// Called on startup when a `--config-file` is found.
   int parse_config_file(std::string fname);
-  // CommandListenerConfig command_listener;
-  uint64_t teamid = 0;
-
+  /// Kafka broker and topic where file writer commands are published.
   uri::URI command_broker_uri{"kafka://localhost:9092/kafka-to-nexus.command"};
+
+  // For testing.
   std::function<void(rd_kafka_topic_partition_list_s *)> on_rebalance_assign;
+  // For testing.
   int64_t start_at_command_offset = -1;
+  /// Was/is used for testing during development.
+  uint64_t teamid = 0;
 };
 
 std::pair<int, std::unique_ptr<MainOpt>> parse_opt(int argc, char **argv);
