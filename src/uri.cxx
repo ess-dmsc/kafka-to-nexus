f0a59c7cc0db167d2b0af815a8e1a5a4c29b0eba
Dominik Werder <dominik.werder@psi.ch>
2017-08-04 08:44:13 +0200

    Update uri handling

--- src/uri.cxx
+++ src/uri.cxx
@@ -4,91 +4,126 @@
 
 namespace uri {
 
-// POD
-class CG {
-public:
-  PCRE2_SIZE a, b, n;
+using std::array;
+using std::move;
+using std::swap;
+
+struct CG {
+  PCREX_SIZE a, b, n;
+  char const *s;
   std::string substr(char const *p0) { return std::string(p0 + a, n); }
+  std::string substr() { return std::string(s + a, n); }
 };
 
-class MD {
-public:
-  MD(pcre2_match_data *md) {
-    ol = pcre2_get_ovector_count(md);
-    ov = pcre2_get_ovector_pointer(md);
+#if use_pcre2
+MD::MD(PCREX_SPTR subject) : subject(subject) {
+  md = pcre2_match_data_create(16, nullptr);
+  if (!md) {
+    throw std::runtime_error("allocation failed");
   }
-  CG cg(uint32_t i) {
-    if (i >= ol)
-      throw std::runtime_error("out-of-bounds capture group requested");
-    return {ov[i * 2 + 0], ov[i * 2 + 1], ov[i * 2 + 1] - ov[i * 2 + 0]};
-  }
-  uint32_t ol;
-  PCRE2_SIZE *ov;
-};
+}
+MD::~MD() { pcre2_match_data_free(md); }
+string MD::substr(int i) {
+  if (!ok)
+    string();
+  if (i >= ncap)
+    string();
+  auto ov = pcre2_get_ovector_pointer(md) + 2 * i;
+  if (ov[0] == PCRE2_UNSET)
+    return string();
+  string::size_type n(ov[1] - ov[0]);
+  if (n > 0)
+    return {(char *)subject + ov[0], n};
+  return string();
+}
+#else
+MD::MD(PCREX_SPTR subject) : subject(subject) {}
+string MD::substr(int i) {
+  if (!ok)
+    string();
+  if (i >= ncap)
+    string();
+  auto ov = ovec.data() + 2 * i;
+  if (ov[0] < 0)
+    return string();
+  string::size_type n(ov[1] - ov[0]);
+  if (n > 0)
+    return {subject + ov[0], n};
+  return string();
+}
+#endif
 
+#if use_pcre2
 void p_regerr(int err) {
-  std::array<unsigned char, 512> s1;
+  std::array<unsigned char, 256> s1;
   auto n = pcre2_get_error_message(err, s1.data(), s1.size());
   fmt::print("err in regex: [{}, {}] {:.{}}\n", err, n, (char *)s1.data(), n);
 }
+#else
+void p_regerr(char const *s1) { fmt::print("err in regex: {}\n", s1); }
+#endif
 
-static_ini::static_ini() {
-  using uchar = unsigned char;
+#if use_pcre2
+Re::Re(char const *s) { init((rchar const *)s); }
+Re::Re(rchar const *s) { init(s); }
+void Re::init(rchar const *s) {
   int err = 0;
-  size_t errpos = 0;
-  {
-    auto s1 = (uchar *)"^\\s*(([a-z]+):)?//(([-._A-Za-z0-9]+)(:([0-9]+))?)(/"
-                       "([-./_A-Za-z0-9]*))?\\s*$";
-    auto re =
-        pcre2_compile_8(s1, PCRE2_ZERO_TERMINATED, 0, &err, &errpos, nullptr);
-    if (!re) {
-      p_regerr(err);
-      throw std::runtime_error("can not compile regex");
-    }
-    URI::re1 = re;
-  }
-  {
-    auto s1 = (uchar *)"^\\s*(([-._A-Za-z0-9]+)(:([0-9]+))?)(/([-./"
-                       "_A-Za-z0-9]*))?\\s*$";
-    auto re =
-        pcre2_compile_8(s1, PCRE2_ZERO_TERMINATED, 0, &err, &errpos, nullptr);
-    if (!re) {
-      p_regerr(err);
-      throw std::runtime_error("can not compile regex");
-    }
-    URI::re_host_no_slashes = re;
-  }
-  {
-    auto s1 = (uchar *)"^/?([-./_A-Za-z0-9]*)$";
-    auto re =
-        pcre2_compile_8(s1, PCRE2_ZERO_TERMINATED, 0, &err, &errpos, nullptr);
-    if (!re) {
-      p_regerr(err);
-      throw std::runtime_error("can not compile regex");
-    }
-    URI::re_no_host = re;
-  }
-  {
-    auto s1 = (uchar *)"^/?([-._A-Za-z0-9]+)$";
-    auto re =
-        pcre2_compile_8(s1, PCRE2_ZERO_TERMINATED, 0, &err, &errpos, nullptr);
-    if (!re) {
-      p_regerr(err);
-      throw std::runtime_error("can not compile regex");
-    }
-    URI::re_topic = re;
+  pcre_erroff_t errpos = 0;
+  re = pcre2_compile_8(s, PCRE2_ZERO_TERMINATED, 0, &err, &errpos, nullptr);
+  if (!re) {
+    p_regerr(err);
+    throw std::runtime_error("can not compile regex");
   }
 }
+Re::~Re() { pcre2_code_free(re); }
+MD Re::match(string const &s) {
+  MD md((PCREX_SPTR)s.data());
+  auto x =
+      pcre2_match(re, (PCREX_SPTR)s.data(), s.size(), 0, 0, md.md, nullptr);
+  if (x >= 0) {
+    md.ok = true;
+  }
+  return md;
+}
+#else
+Re::Re(rchar const *s) {
+  if (!s) {
+    throw std::runtime_error("empty regular expression");
+  }
+  char const *errptr = nullptr;
+  pcre_erroff_t errpos = 0;
+  re = pcre_compile(s, 0, &errptr, &errpos, nullptr);
+  if (!re) {
+    p_regerr(errptr);
+    throw std::runtime_error("can not compile regex");
+  }
+}
+Re::~Re() { pcre_free(re); }
+MD Re::match(string const &s) {
+  MD md(s.data());
+  auto x = pcre_exec(re, nullptr, s.data(), s.size(), 0, 0, md.ovec.data(),
+                     md.ovec.size());
+  if (x > 0) {
+    md.ok = true;
+    md.ncap = x;
+  }
+  return md;
+}
+#endif
+Re::Re(Re &&x) { swap(*this, x); }
+Re &Re::operator=(Re &&x) {
+  swap(*this, x);
+  return *this;
+}
+void swap(Re &x, Re &y) { swap(x.re, y.re); }
 
-static_ini::~static_ini() {
-  if (auto &x = URI::re1)
-    pcre2_code_free(x);
-  if (auto &x = URI::re_host_no_slashes)
-    pcre2_code_free(x);
-  if (auto &x = URI::re_no_host)
-    pcre2_code_free(x);
-  if (auto &x = URI::re_topic)
-    pcre2_code_free(x);
+static_ini::static_ini() {
+  URI::re1 = Re((rchar *)"^\\s*(([a-z]+):)?//(([-._A-Za-z0-9]+)(:([0-9]+))?)(/"
+                         "[-./_A-Za-z0-9]*)?\\s*$");
+  URI::re_host_no_slashes = Re(
+      (rchar *)"^\\s*(([-._A-Za-z0-9]+)(:([0-9]+))?)(/[-./_A-Za-z0-9]*)?\\s*$");
+  URI::re_no_host = Re((rchar *)"^/?([-./_A-Za-z0-9]*)$");
+  URI::re_topic = Re((rchar *)"^/?([-._A-Za-z0-9]+)$");
 }
 
 void URI::update_deps() {
@@ -98,12 +133,10 @@ void URI::update_deps() {
     host_port = host;
   }
   // check if the path could be a valid topic
-  auto mdd = pcre2_match_data_create(16, nullptr);
-  if (0 <= pcre2_match(re_topic, (uchar *)path.data(), path.size(), 0, 0, mdd,
-                       nullptr)) {
-    topic = MD(mdd).cg(1).substr(path.data());
+  auto md = re_topic.match(path);
+  if (md.ok) {
+    topic = md.substr(1);
   }
-  pcre2_match_data_free(mdd);
 }
 
 URI::~URI() {}
@@ -115,57 +148,46 @@ URI::URI(std::string uri) { init(uri); }
 void URI::init(std::string uri) {
   using std::vector;
   using std::string;
-  auto p0 = uri.data();
-  auto mdd = pcre2_match_data_create(16, nullptr);
   bool match = false;
   if (!match) {
-    int x;
-    x = pcre2_match(re1, (uchar *)uri.data(), uri.size(), 0, 0, mdd, nullptr);
-    if (x >= 0) {
+    auto md = re1.match(uri);
+    if (md.ok) {
       match = true;
-      MD m(mdd);
-      scheme = m.cg(2).substr(p0);
-      host = m.cg(4).substr(p0);
-      auto cg = m.cg(6);
-      if (cg.n > 0) {
-        port = strtoul(string(p0 + cg.a, cg.n).data(), nullptr, 10);
+      scheme = md.substr(2);
+      host = md.substr(4);
+      auto port_s = md.substr(6);
+      if (port_s.size() > 0) {
+        port = strtoul(port_s.data(), nullptr, 10);
       }
-      path = m.cg(7).substr(p0);
+      path = md.substr(7);
     }
   }
   if (!match && !require_host_slashes) {
-    int x;
-    x = pcre2_match(re_host_no_slashes, (uchar *)uri.data(), uri.size(), 0, 0,
-                    mdd, nullptr);
-    if (x >= 0) {
+    auto md = re_host_no_slashes.match(uri);
+    if (md.ok) {
       match = true;
-      MD m(mdd);
-      host = m.cg(2).substr(p0);
-      auto cg = m.cg(4);
-      if (cg.n > 0) {
-        port = strtoul(string(p0 + cg.a, cg.n).data(), nullptr, 10);
+      host = md.substr(2);
+      auto port_s = md.substr(4);
+      if (port_s.size() > 0) {
+        port = strtoul(port_s.data(), nullptr, 10);
       }
-      path = m.cg(5).substr(p0);
+      path = md.substr(5);
     }
   }
   if (!match) {
-    int x;
-    x = pcre2_match(re_no_host, (uchar *)uri.data(), uri.size(), 0, 0, mdd,
-                    nullptr);
-    if (x >= 0) {
+    auto md = re_no_host.match(uri);
+    if (md.ok) {
       match = true;
-      MD m(mdd);
-      path = m.cg(0).substr(p0);
+      path = md.substr(0);
     }
   }
-  pcre2_match_data_free(mdd);
   update_deps();
 }
 
-pcre2_code *URI::re1 = nullptr;
-pcre2_code *URI::re_host_no_slashes = nullptr;
-pcre2_code *URI::re_no_host = nullptr;
-pcre2_code *URI::re_topic = nullptr;
+Re URI::re1(".");
+Re URI::re_host_no_slashes(".");
+Re URI::re_no_host(".");
+Re URI::re_topic(".");
 
 void URI::default_port(int port_) {
   if (port == 0) {
@@ -189,134 +211,4 @@ void URI::default_host(std::string host_) {
 }
 
 static_ini URI::compiled;
-
-#if HAVE_GTEST
-TEST(URI, host) {
-  URI u1("//myhost");
-  ASSERT_EQ(u1.scheme, "");
-  ASSERT_EQ(u1.host, "myhost");
-  ASSERT_EQ(u1.port, (uint32_t)0);
 }
-TEST(URI, ip) {
-  URI u1("//127.0.0.1");
-  ASSERT_EQ(u1.scheme, "");
-  ASSERT_EQ(u1.host, "127.0.0.1");
-  ASSERT_EQ(u1.port, (uint32_t)0);
-}
-TEST(URI, host_port) {
-  URI u1("//myhost:345");
-  ASSERT_EQ(u1.scheme, "");
-  ASSERT_EQ(u1.host, "myhost");
-  ASSERT_EQ(u1.port, (uint32_t)345);
-}
-TEST(URI, host_port_noslashes) {
-  URI u1;
-  u1.require_host_slashes = false;
-  u1.init("myhost:345");
-  ASSERT_EQ(u1.scheme, "");
-  ASSERT_EQ(u1.host, "myhost");
-  ASSERT_EQ(u1.port, (uint32_t)345);
-}
-TEST(URI, ip_port) {
-  URI u1("//127.0.0.1:345");
-  ASSERT_EQ(u1.scheme, "");
-  ASSERT_EQ(u1.host, "127.0.0.1");
-  ASSERT_EQ(u1.port, (uint32_t)345);
-}
-TEST(URI, scheme_host_port) {
-  URI u1("http://my.host:345");
-  u1.default_port(123);
-  ASSERT_EQ(u1.scheme, "http");
-  ASSERT_EQ(u1.host, "my.host");
-  ASSERT_EQ(u1.host_port, "my.host:345");
-  ASSERT_EQ(u1.port, (uint32_t)345);
-}
-TEST(URI, scheme_host_port_default) {
-  URI u1("http://my.host");
-  u1.default_port(123);
-  ASSERT_EQ(u1.scheme, "http");
-  ASSERT_EQ(u1.host, "my.host");
-  ASSERT_EQ(u1.host_port, "my.host:123");
-  ASSERT_EQ(u1.port, (uint32_t)123);
-}
-TEST(URI, scheme_host_port_pathdefault) {
-  URI u1("kafka://my-host.com:8080/");
-  ASSERT_EQ(u1.scheme, "kafka");
-  ASSERT_EQ(u1.host, "my-host.com");
-  ASSERT_EQ(u1.port, (uint32_t)8080);
-  ASSERT_EQ(u1.path, "/");
-}
-TEST(URI, scheme_host_port_path) {
-  URI u1("kafka://my-host.com:8080/som_e");
-  ASSERT_EQ(u1.scheme, "kafka");
-  ASSERT_EQ(u1.host, "my-host.com");
-  ASSERT_EQ(u1.port, (uint32_t)8080);
-  ASSERT_EQ(u1.path, "/som_e");
-  ASSERT_EQ(u1.topic, "som_e");
-}
-TEST(URI, scheme_host_port_pathlonger) {
-  URI u1("kafka://my_host.com:8080/some/longer");
-  ASSERT_EQ(u1.scheme, "kafka");
-  ASSERT_EQ(u1.host, "my_host.com");
-  ASSERT_EQ(u1.port, (uint32_t)8080);
-  ASSERT_EQ(u1.path, "/some/longer");
-  ASSERT_EQ(u1.topic, "");
-}
-TEST(URI, host_topic) {
-  URI u1("//my.host/the-topic");
-  ASSERT_EQ(u1.scheme, "");
-  ASSERT_EQ(u1.host, "my.host");
-  ASSERT_EQ(u1.port, (uint32_t)0);
-  ASSERT_EQ(u1.topic, "the-topic");
-}
-TEST(URI, host_port_topic) {
-  URI u1("//my.host:789/the-topic");
-  ASSERT_EQ(u1.scheme, "");
-  ASSERT_EQ(u1.host, "my.host");
-  ASSERT_EQ(u1.port, (uint32_t)789);
-  ASSERT_EQ(u1.topic, "the-topic");
-}
-TEST(URI, abspath) {
-  URI u1("/mypath/sub");
-  ASSERT_EQ(u1.scheme, "");
-  ASSERT_EQ(u1.host, "");
-  ASSERT_EQ(u1.port, (uint32_t)0);
-  ASSERT_EQ(u1.path, "/mypath/sub");
-  ASSERT_EQ(u1.topic, "");
-}
-TEST(URI, relpath) {
-  URI u1("mypath/sub");
-  ASSERT_EQ(u1.scheme, "");
-  ASSERT_EQ(u1.host, "");
-  ASSERT_EQ(u1.port, (uint32_t)0);
-  ASSERT_EQ(u1.path, "mypath/sub");
-  ASSERT_EQ(u1.topic, "");
-}
-TEST(URI, abstopic) {
-  URI u1("/topic-name.test");
-  ASSERT_EQ(u1.scheme, "");
-  ASSERT_EQ(u1.host, "");
-  ASSERT_EQ(u1.port, (uint32_t)0);
-  ASSERT_EQ(u1.path, "/topic-name.test");
-  ASSERT_EQ(u1.topic, "topic-name.test");
-}
-TEST(URI, reltopic) {
-  URI u1("topic-name.test");
-  ASSERT_EQ(u1.scheme, "");
-  ASSERT_EQ(u1.host, "");
-  ASSERT_EQ(u1.port, (uint32_t)0);
-  ASSERT_EQ(u1.path, "topic-name.test");
-  ASSERT_EQ(u1.topic, "topic-name.test");
-}
-TEST(URI, host_default_topic) {
-  URI u1("//my.host");
-  u1.default_path("/some-path");
-  ASSERT_EQ(u1.scheme, "");
-  ASSERT_EQ(u1.host, "my.host");
-  ASSERT_EQ(u1.port, (uint32_t)0);
-  ASSERT_EQ(u1.path, "/some-path");
-  ASSERT_EQ(u1.topic, "some-path");
-}
-#endif
-
-} // namespace uri
--- src/uri.h
+++ src/uri.h
@@ -1,18 +1,70 @@
 #pragma once
 
+#ifndef use_pcre2
+#define use_pcre2 0
+#endif
+#include <array>
 #include <string>
+#if use_pcre2
 #define PCRE2_CODE_UNIT_WIDTH 8
 #include <pcre2.h>
-
+#else
+#include <pcre.h>
+#endif
 #if HAVE_GTEST
 #include <gtest/gtest.h>
 #endif
 
 namespace uri {
 
+using std::array;
+using std::string;
+
+#if use_pcre2
+using rchar = unsigned char;
+using pcre_re_t = pcre2_code;
+using pcre_erroff_t = PCRE2_SIZE;
+using PCREX_SIZE = PCRE2_SIZE;
+using PCREX_SPTR = PCRE2_SPTR;
+#else
+using rchar = char;
+using pcre_re_t = pcre;
+using pcre_erroff_t = int;
+using PCREX_SIZE = int;
+using PCREX_SPTR = char const *;
+#endif
+
+struct MD {
+  MD(PCREX_SPTR subject);
+  string substr(int i);
+  PCREX_SPTR subject;
+  bool ok = false;
+  int ncap = 0;
+#if use_pcre2
+  ~MD();
+  pcre2_match_data *md;
+  array<PCREX_SIZE, 24> ovec;
+#else
+  array<int, 24> ovec;
+#endif
+};
+
+struct Re {
+#if use_pcre2
+  Re(char const *s);
+#endif
+  Re(rchar const *s);
+  ~Re();
+  Re(Re &&);
+  Re &operator=(Re &&);
+  void init(rchar const *s);
+  MD match(string const &s);
+  pcre_re_t *re = nullptr;
+  friend void swap(Re &x, Re &y);
+};
+
 struct static_ini {
   static_ini();
-  ~static_ini();
 };
 
 class URI {
@@ -35,13 +87,12 @@ public:
   bool require_host_slashes = true;
 
 private:
-  static pcre2_code *re1;
-  static pcre2_code *re_host_no_slashes;
-  static pcre2_code *re_no_host;
-  static pcre2_code *re_topic;
+  static Re re1;
+  static Re re_host_no_slashes;
+  static Re re_no_host;
+  static Re re_topic;
   static static_ini compiled;
   void update_deps();
   friend struct static_ini;
 };
-
-} // namespace uri
+}
