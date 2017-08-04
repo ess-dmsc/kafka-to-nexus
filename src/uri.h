052cda868559f03fef6d5f39d9df52c2b4364e40
Dominik Werder <dominik.werder@psi.ch>
2017-08-04 11:22:56 +0200

    Documentation

--- src/uri.h
+++ src/uri.h
@@ -10,14 +10,21 @@ using std::array;
 using std::vector;
 using std::string;
 
+/// Match data from a regex_match
 struct MD {
+  /// Keep the pointer to the haystack
   MD(char const * subject);
-  string substr(uint8_t i);
-  char const * subject;
+  /// Whether the match was successful or not
   bool ok = false;
+  /// Returns the ith match or string() if out of bounds
+  string substr(uint8_t i);
+  /// Holds the matched substrings
   vector<string> matches;
+  /// The haystack
+  char const * subject;
 };
 
+/// Wraps a std::regex
 struct Re {
   Re(std::regex * re);
   Re(Re &&);
@@ -27,17 +34,33 @@ struct Re {
   friend void swap(Re &x, Re &y);
 };
 
+/// Thin parser for URIs.
 class URI {
 public:
+  /// Creates a default URI with all members empty. You can (should) run
+  /// URI::parse to fill it
   URI();
+  /// Creates and parses the given URI
   URI(string uri);
+  /// Parses the given `uri`
   void parse(string uri);
+  /// Given a `http://www.example.com` scheme will contain `http`
   std::string scheme;
+  /// Just the parsed hostname
   std::string host;
+  /// If port was specified (or already non-zero before `URI::parse`) it
+  /// contains `host:port`
   std::string host_port;
+  /// The port number if specified, or zero to indicate that the port is not
+  /// specified
   uint32_t port = 0;
+  /// The path of the URL
   std::string path;
+  /// If the path can be a valid Kafka topic name, then it is non-empty
   std::string topic;
+  /// Whether we require two slashes before the hostname, as required by the
+  /// standard.  Otherwise ambiguous because `host/path` could also be a
+  /// `path/path`.
   bool require_host_slashes = true;
 
 private:
