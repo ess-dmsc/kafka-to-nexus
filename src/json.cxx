42e9c3c78b9d8319d1e3d51a1614431f206893be
Dominik Werder <dominik.werder@psi.ch>
2017-08-11 13:24:27 +0200

    More comments and more consistent variable names

--- src/json.cxx
+++ src/json.cxx
@@ -37,9 +37,18 @@ rapidjson::Document merge(rapidjson::Value const &v1,
     Value const *v;
   };
   struct stack_element {
+    // Invariant: `it1` is always a iterator into the members of a json object,
+    // and that json object is part of the output document which is later
+    // returned from this function.
     Value::ConstMemberIterator it1;
+    // Invariant: `v1` is always a json object and part of the output document
+    // which is later returned from this function.
     Value *v1;
+    // Invariant: `v2` is always a json object and part of the 2nd input
+    // document to this function.
     Value const *v2;
+    // Remember the elements to be copied from the 2nd input document into the
+    // document `ret` to be returned.
     deque<to_copy_element> to_copy;
   };
   deque<stack_element> stack;
@@ -48,29 +57,54 @@ rapidjson::Document merge(rapidjson::Value const &v1,
   while (stack.size() > 0) {
     auto &se = stack.back();
     if (se.it1 != se.v1->MemberEnd()) {
+      // Naming scheme:
+      // m for 'member'
+      // (1|2) if it is out of 1st or 2nd input document.
+      // (n|v) name or value.
+      auto &m1n = se.it1->name;
+      auto &m1v = se.it1->value;
+      // Let's see if the 2nd input document has a member of the same name. If
+      // not, there is nothing to do.
       auto it2 = se.v2->FindMember(se.it1->name.GetString());
       if (it2 != se.v2->MemberEnd()) {
-        if (se.it1->value.IsObject()) {
-          if (it2->value.IsObject()) {
-            stack.push_back(
-                stack_element{se.it1->value.MemberBegin(),
-                              &se.v1->operator[](se.it1->name.GetString()),
-                              &it2->value,
-                              {}});
+        auto &m2v = it2->value;
+        if (m1v.IsObject()) {
+          if (m2v.IsObject()) {
+            // If both 1st and 2nd input are json objects at this level then we
+            // recursively continue the comparison.
+            stack_element e{m1v.MemberBegin(),
+                            &se.v1->operator[](m1n.GetString()),
+                            &m2v,
+                            {}};
+            stack.push_back(e);
           } else {
-            se.to_copy.push_back({se.it1->name.GetString(), &it2->value});
+            // it2->value is not a json object, even though it1->value is.
+            // We therefore remember that we want to use it to replace whatever
+            // is currently stored at the key se.it1->name.
+            se.to_copy.push_back({m1n.GetString(), &m2v});
           }
         } else {
-          se.to_copy.push_back({se.it1->name.GetString(), &it2->value});
+          // it1->value is not a json object.
+          // We therefore remember that we want to use it2->value to replace
+          // what is currently stored at the key se.it1->name.
+          se.to_copy.push_back({m1n.GetString(), &m2v});
         }
       }
+      // Advance the iterator over the elements at the current level of the 1st
+      // input document.
       ++se.it1;
     } else {
+      // Go through the to-do list of items that we have to copy from the 2nd
+      // input document.
+      // First erase the existing key in the output.
+      // Then copy over the new one.
       for (auto &ce : se.to_copy) {
         se.v1->EraseMember(ce.name.c_str());
         se.v1->AddMember(Value(ce.name.c_str(), a), Value().CopyFrom(*ce.v, a),
                          a);
       }
+      // Copy over any key from 2nd input document which did not yet exist in
+      // the output.
       for (auto it2 = se.v2->MemberBegin(); it2 != se.v2->MemberEnd(); ++it2) {
         if (se.v1->FindMember(it2->name) == se.v1->MemberEnd()) {
           se.v1->AddMember(Value(it2->name, a), Value().CopyFrom(it2->value, a),
