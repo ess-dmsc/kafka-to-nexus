a25c4f31b77dedb98b81c1a406c355cdd3f01ac4
Dominik Werder <dominik.werder@psi.ch>
2017-08-15 10:39:02 +0200

    Add timestamp to src/FlatbufferReader.h

--- src/FlatbufferReader.h
+++ src/FlatbufferReader.h
@@ -14,18 +14,17 @@ namespace FileWriter {
 /// Interface for reading essential information from the flatbuffer which is
 /// needed for example to extract timing information and to identify the
 /// responsible `HDFWriterModule`.
+/// Example: Please see `./f142/f142_rw.cxx`
 
 class FlatbufferReader {
 public:
   typedef std::unique_ptr<FlatbufferReader> ptr;
-  // Run the flatbuffer verification code and return the result.
+  /// Run the flatbuffer verification and return the result.
   virtual bool verify(Msg const &msg) const = 0;
-  // Extract the 'sourcename' from the flatbuffer message.
+  /// Extract the 'sourcename' from the flatbuffer message.
   virtual std::string sourcename(Msg const &msg) const = 0;
-  // Extract the timestamp.
-  // TODO discuss with Michele if he actually needs this, or if we always use
-  // the Kafka timestamp.
-  // uint64_t timestamp(Msg const & msg) const = 0;
+  /// Extract the timestamp.
+  virtual uint64_t timestamp(Msg const &msg) const = 0;
 };
 
 using FBID = std::array<char, 4>;
