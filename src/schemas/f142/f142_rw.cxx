ea1d6b90b308d90b92b511e8c9d060b8e1627bf8
Dominik Werder <dominik.werder@psi.ch>
2017-08-25 09:19:52 +0200

    Remove usage of SchemaRegistry and FBSchemaWriter from f142

--- src/schemas/f142/f142_rw.cxx
+++ src/schemas/f142/f142_rw.cxx
@@ -2,7 +2,6 @@
 #include "../../HDFFile.h"
 #include "../../HDFFile_h5.h"
 #include "../../HDFWriterModule.h"
-#include "../../SchemaRegistry.h"
 #include "../../h5.h"
 #include "../../helper.h"
 #include "schemas/f142_logdata_generated.h"
@@ -19,14 +18,6 @@ using std::string;
 template <typename T> using uptr = std::unique_ptr<T>;
 using FBUF = LogData;
 using h5::append_ret;
-
-class reader : public FBSchemaReader {
-  std::unique_ptr<FBSchemaWriter> create_writer_impl() override;
-  bool verify_impl(Msg msg) override;
-  std::string sourcename_impl(Msg msg) override;
-  uint64_t ts_impl(Msg msg) override;
-  uint64_t teamid_impl(Msg &msg) override;
-};
 
 class writer_typed_base {
 public:
@@ -53,164 +44,7 @@ public:
   uptr<h5::h5d_chunked_1d<DT>> ds;
 };
 
-class writer : public FBSchemaWriter {
-  ~writer() override;
-  void init_impl(std::string const &sourcename, hid_t hdf_group,
-                 Msg msg) override;
-  WriteResult write_impl(Msg msg) override;
-  uptr<writer_typed_base> impl;
-  uptr<h5::h5d_chunked_1d<uint64_t>> ds_timestamp;
-  uptr<h5::h5d_chunked_1d<uint64_t>> ds_cue_timestamp_zero;
-  uptr<h5::h5d_chunked_1d<uint64_t>> ds_cue_index;
-  uptr<h5::h5d_chunked_1d<uint64_t>> ds_seq_data;
-  uptr<h5::h5d_chunked_1d<uint64_t>> ds_seq_fwd;
-  uptr<h5::h5d_chunked_1d<uint64_t>> ds_ts_data;
-  bool do_flush_always = false;
-  bool do_writer_forwarder_internal = false;
-  uint64_t total_written_bytes = 0;
-  uint64_t index_at_bytes = 0;
-  uint64_t index_every_bytes = std::numeric_limits<uint64_t>::max();
-  uint64_t ts_max = 0;
-};
-
 static FBUF const *get_fbuf(char *data) { return GetLogData(data); }
-
-std::unique_ptr<FBSchemaWriter> reader::create_writer_impl() {
-  return std::unique_ptr<FBSchemaWriter>(new writer);
-}
-
-bool reader::verify_impl(Msg msg) {
-  auto veri = flatbuffers::Verifier((uint8_t *)msg.data, msg.size);
-  if (VerifyLogDataBuffer(veri)) {
-    return true;
-  }
-  return false;
-}
-
-std::string reader::sourcename_impl(Msg msg) {
-  auto fbuf = get_fbuf(msg.data);
-  auto s1 = fbuf->source_name();
-  if (!s1) {
-    LOG(4, "WARNING message has no source name");
-    return "";
-  }
-  return s1->str();
-}
-
-uint64_t reader::ts_impl(Msg msg) {
-  auto fbuf = get_fbuf(msg.data);
-  return fbuf->timestamp();
-}
-
-uint64_t reader::teamid_impl(Msg &msg) {
-  auto fbuf = get_fbuf(msg.data);
-  if (fbuf->fwdinfo_type() == forwarder_internal::fwdinfo_1_t) {
-    return ((fwdinfo_1_t *)fbuf->fwdinfo())->teamid();
-  }
-  return 0;
-}
-
-writer::~writer() {}
-
-template <typename T, typename V> using WA = writer_typed_array<T, V>;
-template <typename T, typename V> using WS = writer_typed_scalar<T, V>;
-
-void writer::init_impl(string const &sourcename, hid_t hdf_group, Msg msg) {
-  // This is just a unbuffered, low-performance write.
-  // Improved write on separate branch.
-  // Want to gather performance data first for this baseline implementation
-  // in the integration tests.
-  auto fbuf = get_fbuf(msg.data);
-  auto &hg = hdf_group;
-  string s("value");
-
-  // todo, unify ...
-  if (config_file) {
-    if (auto x = get_int(config_file, "nexus.indices.index_every_mb")) {
-      index_every_bytes = (int)x * 1024 * 1024;
-    }
-  }
-
-  if (config_stream) {
-    if (auto x = get_int(config_stream, "nexus.indices.index_every_mb")) {
-      index_every_bytes = (int)x * 1024 * 1024;
-    }
-  }
-
-  auto impl_fac = [&hg, &s](Value x) {
-    using R = writer_typed_base *;
-    if (x == Value::Byte)
-      return (R) new WS<int8_t, Byte>(hg, s);
-    if (x == Value::Short)
-      return (R) new WS<int16_t, Short>(hg, s);
-    if (x == Value::Int)
-      return (R) new WS<int32_t, Int>(hg, s);
-    if (x == Value::Long)
-      return (R) new WS<int64_t, Long>(hg, s);
-    if (x == Value::UByte)
-      return (R) new WS<uint8_t, UByte>(hg, s);
-    if (x == Value::UShort)
-      return (R) new WS<uint16_t, UShort>(hg, s);
-    if (x == Value::UInt)
-      return (R) new WS<uint32_t, UInt>(hg, s);
-    if (x == Value::ULong)
-      return (R) new WS<uint64_t, ULong>(hg, s);
-    if (x == Value::Double)
-      return (R) new WS<double, Double>(hg, s);
-    if (x == Value::Float)
-      return (R) new WS<float, Float>(hg, s);
-    hsize_t ncols = 1;
-    if (x == Value::ArrayByte)
-      return (R) new WA<int8_t, ArrayByte>(hg, s, ncols);
-    if (x == Value::ArrayShort)
-      return (R) new WA<int16_t, ArrayShort>(hg, s, ncols);
-    if (x == Value::ArrayInt)
-      return (R) new WA<int32_t, ArrayInt>(hg, s, ncols);
-    if (x == Value::ArrayLong)
-      return (R) new WA<int64_t, ArrayLong>(hg, s, ncols);
-    if (x == Value::ArrayUByte)
-      return (R) new WA<uint8_t, ArrayUByte>(hg, s, ncols);
-    if (x == Value::ArrayUShort)
-      return (R) new WA<uint16_t, ArrayUShort>(hg, s, ncols);
-    if (x == Value::ArrayUInt)
-      return (R) new WA<uint32_t, ArrayUInt>(hg, s, ncols);
-    if (x == Value::ArrayULong)
-      return (R) new WA<uint64_t, ArrayULong>(hg, s, ncols);
-    if (x == Value::ArrayDouble)
-      return (R) new WA<double, ArrayDouble>(hg, s, ncols);
-    if (x == Value::ArrayFloat)
-      return (R) new WA<float, ArrayFloat>(hg, s, ncols);
-    return (writer_typed_base *)nullptr;
-  };
-  impl.reset(impl_fac(fbuf->value_type()));
-  if (!impl) {
-    LOG(4, "Could not create a writer implementation for value_type {}",
-        (int)fbuf->value_type());
-  }
-
-  this->ds_timestamp =
-      h5::h5d_chunked_1d<uint64_t>::create(hdf_group, "time", 64 * 1024);
-  this->ds_cue_timestamp_zero = h5::h5d_chunked_1d<uint64_t>::create(
-      hdf_group, "cue_timestamp_zero", 64 * 1024);
-  this->ds_cue_index =
-      h5::h5d_chunked_1d<uint64_t>::create(hdf_group, "cue_index", 64 * 1024);
-  if (!ds_timestamp || !ds_cue_timestamp_zero || !ds_cue_index) {
-    impl.reset();
-    return;
-  }
-  if (do_writer_forwarder_internal) {
-    this->ds_seq_data = h5::h5d_chunked_1d<uint64_t>::create(
-        hdf_group, sourcename + "__fwdinfo_seq_data", 64 * 1024);
-    this->ds_seq_fwd = h5::h5d_chunked_1d<uint64_t>::create(
-        hdf_group, sourcename + "__fwdinfo_seq_fwd", 64 * 1024);
-    this->ds_ts_data = h5::h5d_chunked_1d<uint64_t>::create(
-        hdf_group, sourcename + "__fwdinfo_ts_data", 64 * 1024);
-    if (!ds_seq_data || !ds_seq_fwd || !ds_ts_data) {
-      impl.reset();
-      return;
-    }
-  }
-}
 
 writer_typed_base::~writer_typed_base() {}
 
@@ -265,66 +99,6 @@ append_ret writer_typed_scalar<DT, FV>::write_impl(FBUF const *fbuf) {
   auto v2 = v1->value();
   return this->ds->append_data_1d(&v2, 1);
 }
-
-WriteResult writer::write_impl(Msg msg) {
-  auto fbuf = get_fbuf(msg.data);
-  if (!impl) {
-    LOG(5, "sorry, but we were unable to initialize for this kind of messages");
-    return {-1};
-  }
-  auto wret = impl->write_impl(fbuf);
-  if (!wret) {
-    LOG(5, "write failed");
-  }
-  total_written_bytes += wret.written_bytes;
-  ts_max = std::max(fbuf->timestamp(), ts_max);
-  if (total_written_bytes > index_at_bytes + index_every_bytes) {
-    this->ds_cue_timestamp_zero->append_data_1d(&ts_max, 1);
-    this->ds_cue_index->append_data_1d(&wret.ix0, 1);
-    index_at_bytes = total_written_bytes;
-  }
-  {
-    auto x = fbuf->timestamp();
-    this->ds_timestamp->append_data_1d(&x, 1);
-  }
-  if (do_writer_forwarder_internal) {
-    if (fbuf->fwdinfo_type() == forwarder_internal::fwdinfo_1_t) {
-      auto fi = (fwdinfo_1_t *)fbuf->fwdinfo();
-      {
-        auto x = fi->seq_data();
-        this->ds_seq_data->append_data_1d(&x, 1);
-      }
-      {
-        auto x = fi->seq_fwd();
-        this->ds_seq_fwd->append_data_1d(&x, 1);
-      }
-      {
-        auto x = fi->ts_data();
-        this->ds_ts_data->append_data_1d(&x, 1);
-      }
-    }
-  }
-
-  if (do_flush_always) {
-    auto file = hdf_file->h5file_detail().h5file();
-    auto err = H5Fflush(file, H5F_SCOPE_LOCAL);
-    if (err < 0) {
-      LOG(4, "error while flushing");
-    }
-  }
-  return {(int64_t)fbuf->timestamp()};
-}
-
-class Info : public SchemaInfo {
-public:
-  FBSchemaReader::ptr create_reader() override;
-};
-
-FBSchemaReader::ptr Info::create_reader() {
-  return FBSchemaReader::ptr(new reader);
-}
-
-SchemaRegistry::Registrar<Info> g_registrar(fbid_from_str("f142"));
 
 class FlatbufferReader : public FileWriter::FlatbufferReader {
   bool verify(Msg const &msg) const;
@@ -386,6 +160,9 @@ FileWriter::HDFWriterModule::ptr HDFWriterModule::create() {
   return FileWriter::HDFWriterModule::ptr(new HDFWriterModule);
 }
 
+template <typename T, typename V> using WA = writer_typed_array<T, V>;
+template <typename T, typename V> using WS = writer_typed_scalar<T, V>;
+
 HDFWriterModule::InitResult
 HDFWriterModule::init_hdf(hid_t hdf_group,
                           rapidjson::Value const &config_stream,
@@ -410,7 +187,9 @@ HDFWriterModule::init_hdf(hid_t hdf_group,
 
   string s("value");
 
-  if (auto x = get_int(&config_stream, "nexus.indices.index_every_mb")) {
+  if (auto x = get_int(&config_stream, "nexus.indices.index_every_kb")) {
+    index_every_bytes = (int)x * 1024;
+  } else if (auto x = get_int(&config_stream, "nexus.indices.index_every_mb")) {
     index_every_bytes = (int)x * 1024 * 1024;
   }
 
