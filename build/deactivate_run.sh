ae93df2616ccb9661c5100cab842cbf5361f2274
Martin Shetty <martukas@gmail.com>
2018-01-18 14:55:07 +0100

    updated more packages

---
+++ build/deactivate_run.sh
@@ -0,0 +1,4 @@
+export DYLD_LIBRARY_PATH=/home/bdv/software/root_build/lib
+export LD_LIBRARY_PATH=/home/bdv/software/root_build/lib
+export PATH=/home/bdv/software/root_build/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+export PS1="$OLD_PS1"
