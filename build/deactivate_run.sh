7fdca6769fd0d81a02d5547d1c5382452b584700
Martin Shetty <martukas@gmail.com>
2018-01-18 14:58:06 +0100

    gitignore added

--- build/deactivate_run.sh
+++
@@ -1,4 +0,0 @@
-export DYLD_LIBRARY_PATH=/home/bdv/software/root_build/lib
-export LD_LIBRARY_PATH=/home/bdv/software/root_build/lib
-export PATH=/home/bdv/software/root_build/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
-export PS1="$OLD_PS1"
