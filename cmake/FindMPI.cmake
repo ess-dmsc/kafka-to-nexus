69c6ae02e70c757fc788c1812144036ff805bdfb
Dominik Werder <dominik.werder@psi.ch>
2018-01-25 14:29:02 +0100

    Remove parallel-related cmake code

--- cmake/FindMPI.cmake
+++
@@ -1,8 +0,0 @@
-find_path(MPI_INCLUDE_DIR NAMES mpi.h)
-find_library(MPI_LIBRARIES NAMES mpi)
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(MPI DEFAULT_MSG
-	MPI_INCLUDE_DIR
-	MPI_LIBRARIES
-)
