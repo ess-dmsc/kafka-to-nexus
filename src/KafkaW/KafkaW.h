f69c266103506e7a4085cb0e05e6e9d9a52890f9
Dominik Werder <dominik.werder@psi.ch>
2018-02-26 13:14:34 +0100

    Clean unused includes

--- src/KafkaW/KafkaW.h
+++ src/KafkaW/KafkaW.h
@@ -5,11 +5,3 @@
 #include "Msg.h"
 #include "Producer.h"
 #include "ProducerTopic.h"
-#include "logger.h"
-#include <atomic>
-#include <functional>
-#include <librdkafka/rdkafka.h>
-#include <map>
-#include <memory>
-#include <string>
-#include <vector>
