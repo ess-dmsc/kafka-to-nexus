759c68f269b41cc4b30d0f7d6d0e082a416b5e3d
Matthew D Jones <matthew.d.jones@tessella.com>
2018-03-01 17:23:50 +0000

    Re #74. Use in-memory hdf file for unit test

--- src/tests/HDFFileTest.cpp
+++ src/tests/HDFFileTest.cpp
@@ -2,12 +2,24 @@
 #include <gtest/gtest.h>
 #include <h5cpp/hdf5.hpp>
 
+FileWriter::HDFFile createInMemoryTestFile(const std::string &Filename) {
+  auto fapl = H5Pcreate(H5P_FILE_ACCESS);
+  // _core uses the in-memory file driver
+  H5Pset_fapl_core(fapl, 100 * 1024, H5P_DEFAULT);
+
+  auto file_id = H5Fcreate(Filename.c_str(), H5F_ACC_TRUNC, H5P_DEFAULT, fapl);
+  H5Pclose(fapl);
+
+  FileWriter::HDFFile TestFile;
+  TestFile.h5file = hdf5::file::File(hdf5::ObjectHandle(file_id));
+
+  return TestFile;
+}
+
 TEST(HDFFileTest, givenCommandContainsVectorAttrItIsWrittenToFile) {
   using namespace hdf5;
 
-  FileWriter::HDFFile TestFile;
-  TestFile.h5file =
-      file::create("test-vector-attribute.h5", file::AccessFlags::TRUNCATE);
+  auto TestFile = createInMemoryTestFile("test-vector-attribute.nxs");
 
   std::string CommandWithVectorAttr = R""({
     "nexus_structure": {
