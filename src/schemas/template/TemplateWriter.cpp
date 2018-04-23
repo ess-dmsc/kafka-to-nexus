3f0290fe5bc8c7698744972879cc37ce8365fd25
Jonas Nilsson <jonas.nilsson@esss.se>
2018-04-23 14:33:05 +0200

    Added template (example) file writing module.

---
+++ src/schemas/template/TemplateWriter.cpp
@@ -0,0 +1,13 @@
+#include "TemplateWriter.h"
+
+namespace TemplateWriter {
+  
+  // Instantiates a ReaderClass used for extracting source names, timestamps and verifying a flatbuffers
+  static FileWriter::FlatbufferReaderRegistry::Registrar<ReaderClass>
+  RegisterReader("test");
+  
+  // Creates a factory function used to instantiate zero or more WriterClass, i.e. one for every data source which produces data with the file id "test".
+  static FileWriter::HDFWriterModuleRegistry::Registrar<WriterClass>
+  RegisterWriter("test");
+  
+} // namespace TemplateWriter
