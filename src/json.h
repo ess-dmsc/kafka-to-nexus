fc3eddde72f6e32525a74dc7996e8c31e6026e52
Dominik Werder <dominik.werder@psi.ch>
2018-05-28 11:43:06 +0200

    Remove rapidjson related code from src/json.h

--- src/json.h
+++ src/json.h
@@ -1,21 +1,8 @@
 #pragma once
 
 #include <nlohmann/json.hpp>
-#include <rapidjson/document.h>
 #include <stdexcept>
 #include <string>
-
-class RapidjsonParseError : public std::runtime_error {
-public:
-  RapidjsonParseError(std::string msg) : std::runtime_error(msg) {}
-};
-
-rapidjson::Document stringToRapidjsonOrThrow(std::string const &JsonString);
-
-std::string json_to_string(rapidjson::Value const &jd);
-
-rapidjson::Document merge(rapidjson::Value const &v1,
-                          rapidjson::Value const &v2);
 
 template <typename T> class JsonMaybe {
 public:
