e2cb20975f52fe1592982cc7bdc60ba2cac42746
Dominik Werder <dominik.werder@psi.ch>
2018-05-28 11:34:17 +0200

    Remove rapidjson from helper tests

--- src/tests/helper.cpp
+++ src/tests/helper.cpp
@@ -1,7 +1,5 @@
 #include "helper.h"
 #include <gtest/gtest.h>
-#include <rapidjson/prettywriter.h>
-#include <rapidjson/stringbuffer.h>
 
 using std::string;
 using std::vector;
@@ -40,83 +38,4 @@ TEST(helper, Split07) {
   auto v = split("/some/longer/thing/for/testing", "/");
   ASSERT_TRUE(v ==
               vector<string>({"some", "longer", "thing", "for", "testing"}));
-}
-
-using namespace rapidjson;
-
-static Document make_test_doc() {
-  Document d;
-  d.Parse("{\"a\":{\"aa\":\"text\",\"bb\":456}}");
-  return d;
-}
-
-TEST(helper, make_test_doc) { ASSERT_FALSE(make_test_doc().HasParseError()); }
-
-TEST(helper, GetString01) {
-  Document d;
-  d.SetObject();
-  auto &a = d.GetAllocator();
-  d.AddMember("mem00", Value("s1", a), a);
-  Value v2;
-  v2.SetObject();
-  v2.AddMember("mem10", Value("s2", a), a);
-  d.AddMember("mem01", v2.Move(), a);
-
-  {
-    Value va;
-    va.SetArray();
-    va.PushBack(Value("something_a_0", a), a);
-    va.PushBack(Value("something_a_1", a), a);
-    va.PushBack(Value(1234), a);
-    d.AddMember("mem02", va, a);
-  }
-
-  StringBuffer buf1;
-  PrettyWriter<StringBuffer> wr(buf1);
-  d.Accept(wr);
-  auto s1 = get_string(&d, "mem00");
-  ASSERT_EQ(s1.v, "s1");
-  s1 = get_string(&d, "mem01.mem10");
-  ASSERT_EQ(s1.v, "s2");
-  s1 = get_string(&d, "mem02.1");
-  ASSERT_EQ(s1.v, "something_a_1");
-}
-
-TEST(helper, GetString02) {
-  auto d = make_test_doc();
-  auto s = get_string(&d, "a.aa");
-  ASSERT_EQ(s.v, "text");
-}
-
-TEST(helper, GetString03) {
-  Document d;
-  d.Parse("{\"a\":{\"aa\":\"text\",\"bb\":unquoted}}");
-  ASSERT_TRUE(d.HasParseError());
-}
-
-TEST(helper, GetInt01) {
-  auto d = make_test_doc();
-  auto r = get_int(&d, "a.bb");
-  ASSERT_EQ((bool)r, true);
-  ASSERT_EQ((int)r, 456);
-  if (auto g = get_int(&d, "a.bb")) {
-    ASSERT_EQ((int)g, 456);
-  } else {
-    // We should not be here
-    ASSERT_TRUE(false);
-  }
-  if (get_int(&d, "blabla")) {
-    // we should not be here
-    ASSERT_TRUE(false);
-  }
-}
-
-TEST(helper, GetObject) {
-  Document d;
-  d.Parse("{\"a\":{\"b\":{\"k\":\"the object\"}}}");
-  auto o1 = get_object(d, "a.b").v;
-  auto o2 = get_object(d, "a.b").v;
-  ASSERT_EQ(o1, o2);
-  ASSERT_EQ(get_string(o1, "k").v, "the object");
-  ASSERT_EQ(get_string(o2, "k").v, "the object");
 }
