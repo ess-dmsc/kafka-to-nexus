8e226062d0ed01959daf8a54ab0f9ae2f219aa96
Dominik Werder <dominik.werder@psi.ch>
2018-06-26 11:44:27 +0200

    Add headers and empty implementation

---
+++ src/schemas/hs00/Reader.h
@@ -0,0 +1,16 @@
+#include "../../FlatbufferReader.h"
+#include "../../HDFWriterModule.h"
+#include "../../h5.h"
+
+namespace FileWriter {
+namespace Schemas {
+namespace hs00 {
+
+class Reader : public FileWriter::FlatbufferReader {
+  bool verify(Msg const &msg) const override;
+  std::string source_name(Msg const &msg) const override;
+  uint64_t timestamp(Msg const &msg) const override;
+};
+}
+}
+}
---
+++ src/schemas/hs00/Writer.h
@@ -0,0 +1,30 @@
+#include "../../FlatbufferReader.h"
+#include "../../HDFWriterModule.h"
+#include "../../h5.h"
+
+namespace FileWriter {
+namespace Schemas {
+namespace hs00 {
+
+template <typename T> using uptr = std::unique_ptr<T>;
+
+class Writer : public FileWriter::HDFWriterModule {
+public:
+  static FileWriter::HDFWriterModule::ptr create();
+  void parse_config(std::string const &ConfigurationStream,
+                    std::string const &ConfigurationModule) override;
+  InitResult init_hdf(hdf5::node::Group &HDFGroup,
+                      std::string const &HDFAttributes) override;
+  InitResult reopen(hdf5::node::Group &HDFGroup) override;
+  WriteResult write(Msg const &msg) override;
+  int32_t flush() override;
+  int32_t close() override;
+  void enable_cq(CollectiveQueue *cq, HDFIDStore *hdf_store, int mpi_rank) {}
+
+  hsize_t ChunkBytes = 1 << 18;
+  bool DoFlushEachWrite = false;
+  uint64_t TotalWrittenBytes = 0;
+};
+}
+}
+}
