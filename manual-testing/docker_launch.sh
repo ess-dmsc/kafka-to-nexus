e3ffc242034f8f320430ca7dc7c2b3f35fd75646
Matthew D Jones <matthew.d.jones@tessella.com>
2018-06-29 13:03:37 +0100

    Fix docker launch script

--- manual-testing/docker_launch.sh
+++ manual-testing/docker_launch.sh
@@ -21,9 +21,9 @@ fi
 echo /kafka_to_nexus/kafka-to-nexus --command-uri ${COMMAND_URI:="//localhost:9092/TEST_writerCommand"} \
   --status-uri ${STATUS_URI:="//localhost:9092/TEST_writerStatus"} \
   --graylog-logger-address ${GRAYLOG_ADDRESS:="localhost:12201"} -v 3 \
-  --hdf-output-prefix "/output-files/"}
+  --hdf-output-prefix ${HDF_OUTPUT_PREFIX:="/output-files/"}
 
 /kafka_to_nexus/kafka-to-nexus --command-uri ${COMMAND_URI:="//localhost:9092/TEST_writerCommand"} \
   --status-uri ${STATUS_URI:="//localhost:9092/TEST_writerStatus"} \
   --graylog-logger-address ${GRAYLOG_ADDRESS:="localhost:12201"} -v 3 \
-  --hdf-output-prefix "/output-files/"}
+  --hdf-output-prefix ${HDF_OUTPUT_PREFIX:="/output-files/"}
