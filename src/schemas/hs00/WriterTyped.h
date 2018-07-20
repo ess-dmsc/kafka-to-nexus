69dab635c92857c1e3d92c51b7ef987eb9294d5b
Dominik Werder <dominik.werder@psi.ch>
2018-07-20 10:24:45 +0200

    Add class WriterTyped

---
+++ src/schemas/hs00/WriterTyped.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "Shape.h"
+#include "json.h"
+#include <h5cpp/hdf5.hpp>
+#include <vector>
+
+namespace FileWriter {
+namespace Schemas {
+namespace hs00 {
+
+template <typename DataType, typename EdgeType> class WriterTyped {
+private:
+  using json = nlohmann::json;
+
+public:
+  /// Create a WriterTyped from Json, used when a new write command arrives at
+  /// the file writer.
+  static WriterTyped createFromJson(json const &Json);
+
+  /// Create the HDF structures. used on arrival of a new write command for the
+  /// file writer to create the initial structure of the HDF file for this
+  /// writer module.
+  void createHDFStructure(hdf5::node::Group const &Group);
+
+private:
+  Shape<EdgeType> OurShape;
+};
+}
+}
+}
--- src/tests/EventHistogramWriterTests.cpp
+++ src/tests/EventHistogramWriterTests.cpp
@@ -2,30 +2,33 @@
 #include "schemas/hs00/Dimension.h"
 #include "schemas/hs00/Exceptions.h"
 #include "schemas/hs00/Shape.h"
+#include "schemas/hs00/WriterTyped.h"
 #include <gtest/gtest.h>
+#include <h5cpp/hdf5.hpp>
 
 using json = nlohmann::json;
 using FileWriter::Schemas::hs00::UnexpectedJsonInput;
 using FileWriter::Schemas::hs00::Dimension;
 using FileWriter::Schemas::hs00::Shape;
+using FileWriter::Schemas::hs00::WriterTyped;
 
 TEST(EventHistogramWriter, DimensionWithInconsistentSizeThrows) {
   auto Json = json::parse(R"""({
-		"size": 4,
-		"label": "Velocity",
-		"unit": "m/s",
-		"edges": [2, 3, 4, 5]
-	})""");
+    "size": 4,
+    "label": "Velocity",
+    "unit": "m/s",
+    "edges": [2, 3, 4, 5]
+  })""");
   ASSERT_THROW(Dimension<double>::createFromJson(Json), UnexpectedJsonInput);
 }
 
 TEST(EventHistogramWriter, DimensionCreatedFromValidInput) {
   auto Json = json::parse(R"""({
-		"size": 4,
-		"label": "Velocity",
-		"unit": "m/s",
-		"edges": [2, 3, 4, 5, 6]
-	})""");
+    "size": 4,
+    "label": "Velocity",
+    "unit": "m/s",
+    "edges": [2, 3, 4, 5, 6]
+  })""");
   auto Dim = Dimension<double>::createFromJson(Json);
   ASSERT_EQ(Dim.getSize(), 4u);
   ASSERT_EQ(Dim.getLabel(), "Velocity");
@@ -41,25 +44,58 @@ TEST(EventHistogramWriter, ShapeFromJsonThrowsIfInvalidInput) {
 
 TEST(EventHistogramWriter, ShapeCreatedFromValidInput) {
   auto Json = json::parse(R"""([
-		{
-			"size": 4,
-			"label": "Position",
-			"unit": "mm",
-			"edges": [2, 3, 4, 5, 6]
-		},
-		{
-			"size": 6,
-			"label": "Position",
-			"unit": "mm",
-			"edges": [-3, -2, -1, 0, 1, 2, 3]
-		},
-		{
-			"size": 3,
-			"label": "Time",
-			"unit": "ns",
-			"edges": [0, 2, 4, 6]
-		}
-	])""");
+    {
+      "size": 4,
+      "label": "Position",
+      "unit": "mm",
+      "edges": [2, 3, 4, 5, 6]
+    },
+    {
+      "size": 6,
+      "label": "Position",
+      "unit": "mm",
+      "edges": [-3, -2, -1, 0, 1, 2, 3]
+    },
+    {
+      "size": 3,
+      "label": "Time",
+      "unit": "ns",
+      "edges": [0, 2, 4, 6]
+    }
+  ])""");
   auto TheShape = Shape<double>::createFromJson(Json);
   ASSERT_EQ(TheShape.getNDIM(), 3u);
+}
+
+hdf5::file::File createFileInMemory(std::string Name) {
+  hdf5::property::FileAccessList FAPL;
+  FAPL.driver(hdf5::file::MemoryDriver());
+  return hdf5::file::create(Name, hdf5::file::AccessFlags::TRUNCATE,
+                            hdf5::property::FileCreationList(), FAPL);
+}
+
+TEST(EventHistogramWriter, WriterTypedCreatedFromValidJsonInput) {
+  auto Json = json::parse(R"""({
+    "shape": [
+      {
+        "size": 4,
+        "label": "Position",
+        "unit": "mm",
+        "edges": [2, 3, 4, 5, 6]
+      },
+      {
+        "size": 6,
+        "label": "Position",
+        "unit": "mm",
+        "edges": [-3, -2, -1, 0, 1, 2, 3]
+      },
+      {
+        "size": 3,
+        "label": "Time",
+        "unit": "ns",
+        "edges": [0, 2, 4, 6]
+      }
+    ]
+  })""");
+  auto TheWriterTyped = WriterTyped<uint64_t, double>::createFromJson(Json);
 }
