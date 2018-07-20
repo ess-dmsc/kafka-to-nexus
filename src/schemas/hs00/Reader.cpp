8c951d9df23c68345d969710bef21f3e80533c4f
Dominik Werder <dominik.werder@psi.ch>
2018-07-20 14:16:19 +0200

    Implement Reader

---
+++ src/schemas/hs00/Reader.cpp
@@ -0,0 +1,35 @@
+#include "Reader.h"
+#include "schemas/hs00_event_histogram_generated.h"
+
+namespace FileWriter {
+namespace Schemas {
+namespace hs00 {
+
+static EventHistogram const *getRoot(char const *Data) {
+  return GetEventHistogram(Data);
+}
+
+bool Reader::verify(Msg const &Message) const {
+  flatbuffers::Verifier Verifier((uint8_t *)Message.data(), Message.size());
+  return VerifyEventHistogramBuffer(Verifier);
+}
+
+std::string Reader::source_name(Msg const &Message) const {
+  auto Buffer = getRoot(Message.data());
+  auto Source = Buffer->source();
+  if (!Source) {
+    LOG(Sev::Notice, "message has no source_name");
+    return "";
+  }
+  return Source->str();
+}
+
+uint64_t Reader::timestamp(Msg const &Message) const {
+  auto Buffer = getRoot(Message.data());
+  return Buffer->timestamp();
+}
+
+static FlatbufferReaderRegistry::Registrar<Reader> RegisterReader("hs00");
+}
+}
+}
