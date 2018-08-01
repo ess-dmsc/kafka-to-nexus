c5738a98e84f15fff091f56b9a7663e6159e6f39
Jonas Nilsson <jonas.nilsson@esss.se>
2018-08-02 00:54:49 +0200

    Changed to old name.

--- src/logger.h
+++ src/logger.h
@@ -44,7 +44,7 @@ void dwlog(Sev level, char const *fmt, char const *file, int line,
   try {
     dwlog_inner(static_cast<int>(level), file, line, func,
                 fmt::format(fmt, args...));
-  } catch (fmt::format_error &e) {
+  } catch (fmt::FormatError &e) {
     dwlog_inner(static_cast<int>(level), file, line, func,
                 fmt::format("ERROR in format: {}: {}", e.what(), fmt));
   }
