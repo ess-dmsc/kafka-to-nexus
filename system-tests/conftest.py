6b3a299a0f4ba21aa9a853208c4778f05eedfd80
Jack Harper <jack.harper@stfc.ac.uk>
2018-08-22 10:10:54 +0100

    re #255 initial commit

---
+++ system-tests/conftest.py
@@ -0,0 +1,69 @@
+import os.path
+import pytest
+from confluent_kafka import Producer
+import docker
+
+
+def wait_until_kafka_ready(docker_cmd, docker_options):
+    print('Waiting for Kafka broker to be ready for system tests...')
+    conf = {'bootstrap.servers': 'localhost:9092',
+            'api.version.request': True}
+    producer = Producer(**conf)
+    kafka_ready = False
+
+    def delivery_callback(err, msg):
+        nonlocal n_polls
+        nonlocal kafka_ready
+        if not err:
+            print('Kafka is ready!')
+            kafka_ready = True
+
+    n_polls = 0
+    while n_polls < 10 and not kafka_ready:
+        producer.produce('waitUntilUp', value='Test message', on_delivery=delivery_callback)
+        producer.poll(10)
+        n_polls += 1
+
+    if not kafka_ready:
+        docker_cmd.down(docker_options)  # Bring down containers cleanly
+        raise Exception('Kafka broker was not ready after 100 seconds, aborting tests.')
+
+
+common_options = {"--no-deps": False,
+                  "--always-recreate-deps": False,
+                  "--scale": "",
+                  "--abort-on-container-exit": False,
+                  "SERVICE": "",
+                  "--remove-orphans": False,
+                  "--no-recreate": True,
+                  "--force-recreate": False,
+                  '--no-build': False,
+                  '--no-color': False,
+                  "--rmi": "none",
+                  "--volumes": True,  # Remove volumes when docker-compose down (don't persist kafka and zk data)
+                  "--follow": False,
+                  "--timestamps": False,
+                  "--tail": "all",
+                  "--detach": True,
+                  "--build": False
+                  }
+
+
+def build_filewriter_image():
+    client = docker.from_env()
+    print("Building Forwarder image", flush=True)
+    build_args = {}
+    if "http_proxy" in os.environ:
+        build_args["http_proxy"] = os.environ["http_proxy"]
+    if "https_proxy" in os.environ:
+        build_args["https_proxy"] = os.environ["https_proxy"]
+    image, logs = client.images.build(path="../", tag="forwarder:latest", rm=False, buildargs=build_args)
+    for item in logs:
+        print(item, flush=True)
+
+
+def run_containers(cmd, options):
+    print("Running docker-compose up", flush=True)
+    cmd.up(options)
+    print("\nFinished docker-compose up\n", flush=True)
+    wait_until_kafka_ready(cmd, options)
