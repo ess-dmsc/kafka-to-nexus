116506d70b0e95e2bb6532034b1c31f0137ee705
Jonas Nilsson <jonas.nilsson@esss.se>
2018-08-27 11:23:21 +0200

    Better file-writer module layout.

---
+++ src/schemas/ModuleFunction.cmake
@@ -0,0 +1,12 @@
+#=============================================================================
+# Generate file-writer module
+# Note that you will still need to manually inlclude the code the
+# 'kafka_to_nexus__objects' target.
+#=============================================================================
+function(create_module module_name)
+  add_library(${module_name} OBJECT
+    ${${module_name}_SRC}
+    ${${module_name}_INC}
+  )
+  target_include_directories(${module_name} PRIVATE ${path_include_common})
+endfunction(create_module)
