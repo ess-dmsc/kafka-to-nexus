82ae3b0b8eb7ef1c173324561bdee09822b0a6a5
Jack Harper <jack.harper@stfc.ac.uk>
2018-09-04 11:00:17 +0200

    re #255 removing more from command file

--- system-tests/test_filewriter.py
+++ system-tests/test_filewriter.py
@@ -2,6 +2,8 @@ from helpers.kafkahelpers import create_producer, send_writer_command
 from time import sleep
 import h5py
 import numpy as np
+import os
+
 
 def test_data_reaches_file(docker_compose):
     producer = create_producer()
@@ -19,12 +21,10 @@ def test_data_reaches_file(docker_compose):
     producer.flush()
 
     # Allow time for the file writing to complete
-    sleep(5)
+    sleep(30)
+    file = h5py.File("output-files/output_file.nxs", mode='r')
 
-    file = h5py.File("./output-files/output_file.nxs", 'r')
-
-    assert file is not None
-    assert file.swmr_mode == False
+    assert not file.swmr_mode
     assert file["entry/start_time"].value == '2016-04-12T02:58:52'
     assert file["entry/end_time"].value == '2016-04-12T03:29:11'
     assert file["entry/duration"].value == 1817.0
@@ -33,4 +33,4 @@ def test_data_reaches_file(docker_compose):
     assert file["entry/title"][...] == 'Blend 1.9_SANS'
     assert file["entry/user_1/affiliation"].value == 'ISIS, STFC'
     assert np.allclose(file["entry/instrument/monitor1/transformations/location"].attrs["vector"], np.array([0.0, 0.0, -1.0]))
-    assert file["entry/instrument/monitor2/data"].attrs["signal"] == 1
+    assert file["entry/instrument/monitor1/transformations/location"].attrs["transformation_type"] == "translation"
