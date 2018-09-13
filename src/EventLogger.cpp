f81094ae548e07221b87d9df0ad559c59e4a7e7e
Michele Brambilla <michele.brambilla@psi.ch>
2018-09-13 13:56:53 +0200

    Implement the class for event logging and add unit tests

--- src/EventLogger.cpp
+++ src/EventLogger.cpp
@@ -1,29 +1,4 @@
 #include "EventLogger.h"
-#include "Report.h"
-
-void FileWriter::logEvent(std::shared_ptr<KafkaW::ProducerTopic> Producer,
-                          FileWriter::StatusCode Code,
-                          const std::string &ServiceId,
-                          const std::string &JobId,
-                          const std::string &Message) {
-  if (!Producer) {
-    LOG(Sev::Warning,
-        "Can't produce filewriter event log, invalid KafkaW::ProducerTopic");
-    return;
-  }
-
-  nlohmann::json Event;
-  Event["type"] = "filewriter_event";
-  Event["code"] = convertStatusCodeToString(Code);
-  Event["timestamp"] = 0;
-  Event["service_id"] = ServiceId;
-  Event["job_id"] = JobId;
-  Event["message"] = Message;
-
-  std::string EventMessage = Event.dump();
-  Producer->produce(reinterpret_cast<unsigned char *>(&EventMessage[0]),
-                    EventMessage.size());
-}
 
 std::string FileWriter::convertStatusCodeToString(FileWriter::StatusCode Code) {
   switch (Code) {
@@ -46,14 +21,7 @@ std::string FileWriter::convertStatusCodeToString(FileWriter::StatusCode Code) {
   return "";
 };
 
-void FileWriter::EventLogger::create(std::shared_ptr<KafkaW::ProducerTopic> p,
-                                     const std::string &ServiceId,
-                                     const std::string &JobId) {}
 void FileWriter::EventLogger::log(FileWriter::StatusCode Code,
-                                  const std::string &Message) {}
-
-std::string
-FileWriter::EventLogger::createEventMessage(FileWriter::StatusCode Code,
-                                            const std::string &Message) {
-  return "";
+                                  const std::string &Message) {
+  doLogEvent(Code, Message);
 }
--- src/EventLogger.h
+++ src/EventLogger.h
@@ -1,33 +1,99 @@
 #pragma once
 
 #include "FileWriterTask.h"
+#include <chrono>
+#include <functional>
 
 namespace KafkaW {
 class ProducerTopic;
 }
 
 namespace FileWriter {
+//-----------------------------------------------------------------------------
+/// @brief      Enum class to map NICOS status code values. Close represent the
+/// successiful closere of a NeXus file, Error a generic, non critical error,
+/// Fail the impossibility to start the job.
+///
 enum class StatusCode : int {
   Start = 0,
   Close = 1,
   Error = 2,
   Fail = 3,
 };
+//------------------------------------------------------------------------------
+/// @brief      Convert the StatusCode into a string
+///
+/// @param[in]  Code  The StatusCode
+///
+/// @return     An upper case string that NICOS can interpret
+///
 std::string convertStatusCodeToString(StatusCode Code);
 
-void logEvent(std::shared_ptr<KafkaW::ProducerTopic> p, StatusCode Code,
+//------------------------------------------------------------------------------
+/// @brief      Builds a JSON string that contains an event log and emits the
+/// log via the producer. The event log
+/// is described by a code ("START","CLOSE","FAIL","ERROR"), the service and job
+/// ids, the system timestamp at wich the log has been emitted and a message.
+///
+/// @param[in]  Producer      The producer, for example KafkaW::ProducerTopic
+/// @param[in]  Code          The code
+/// @param[in]  ServiceId     The service identifier
+/// @param[in]  JobId         The job identifier
+/// @param[in]  Message       The message
+///
+/// @tparam     ProducerType  { description }
+///
+template <class ProducerType>
+void logEvent(std::shared_ptr<ProducerType> Producer, StatusCode Code,
               const std::string &ServiceId, const std::string &JobId,
-              const std::string &Message);
+              const std::string &Message) {
 
+  if (!Producer) {
+    LOG(Sev::Warning, "Can't produce filewriter event log, invalid Producer");
+    return;
+  }
+
+  std::chrono::system_clock::time_point Now = std::chrono::system_clock::now();
+  nlohmann::json Event;
+  Event["type"] = "filewriter_event";
+  Event["code"] = convertStatusCodeToString(Code);
+  Event["timestamp"] = std::chrono::duration_cast<std::chrono::milliseconds>(
+                           Now.time_since_epoch())
+                           .count();
+  Event["service_id"] = ServiceId;
+  Event["job_id"] = JobId;
+  Event["message"] = Message;
+
+  std::string EventMessage = Event.dump();
+  Producer->produce(reinterpret_cast<unsigned char *>(&EventMessage[0]),
+                    EventMessage.size());
+}
+
+//------------------------------------------------------------------------------
+/// @brief      Class for event logger. An EventLogger obects defines a unique
+/// pair of service and job id.
+///
 class EventLogger {
 public:
-  void create(std::shared_ptr<KafkaW::ProducerTopic> p,
-              const std::string &ServiceId, const std::string &JobId);
+  template <class ProducerType>
+  void create(std::shared_ptr<ProducerType> Producer,
+              const std::string &ServiceId, const std::string &JobId) {
+    using namespace std::placeholders;
+    doLogEvent =
+        std::bind(logEvent<ProducerType>, Producer, _1, ServiceId, JobId, _2);
+  }
+  //----------------------------------------------------------------------------
+  /// @brief      Builds a JSON string that contains an event log and emits the
+  /// log via the producer. All the event logs produces with this method have a
+  /// uniquely defined service id and job id.
+  ///
+  /// @param[in]  Code     The code
+  /// @param[in]  Message  The message
+  ///
   void log(StatusCode Code, const std::string &Message);
 
 private:
-  std::shared_ptr<KafkaW::ProducerTopic> Producer;
-  std::string createEventMessage(StatusCode Code, const std::string &Message);
-  nlohmann::json EventMsg;
+  std::function<void(FileWriter::StatusCode, const std::string &Message)>
+      doLogEvent;
 };
 }
