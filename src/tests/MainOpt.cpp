838ad2a869bd002da5dc986494ec1f3874ac9f36
Jack Harper <jack.harper@stfc.ac.uk>
2018-09-20 12:08:28 +0100

    re #275 forgot to add the actual test file

---
+++ src/tests/MainOpt.cpp
@@ -0,0 +1,35 @@
+#include "MainOpt.h"
+#include <gtest/gtest.h>
+#include <nlohmann/json.hpp>
+using nlohmann::json;
+
+TEST(MainOpt, MainOptTestParseJsonCommandsReturnsErrorIfNoCommandsJson) {
+  MainOpt Mainopt;
+  ASSERT_EQ(-1, Mainopt.parseJsonCommands());
+}
+
+TEST(MainOpt, findAndAddCommandsAddsNoCommandsIfJsonIsEmpty) {
+  MainOpt Mainopt;
+  Mainopt.CommandsJson = json::parse("{}");
+  Mainopt.findAndAddCommands();
+  Mainopt.CommandsFromJson.empty();
+}
+
+TEST(MainOpt, findAndAddCommandsAddsCommands) {
+  MainOpt Mainopt;
+  Mainopt.CommandsJson =
+      json::parse(R"({"commands":[{"some command":"as discussed above"}]})");
+  Mainopt.findAndAddCommands();
+  ASSERT_EQ("{\"some command\":\"as discussed above\"}",
+            Mainopt.CommandsFromJson.at(0));
+}
+
+TEST(MainOpt, findAndAddCommandsAddsMultipleCommands) {
+  MainOpt Mainopt;
+  Mainopt.CommandsJson = json::parse(
+      R"({"commands":[{"some command":"as discussed above"},{"another command":"test"}]})");
+  Mainopt.findAndAddCommands();
+  ASSERT_EQ("{\"some command\":\"as discussed above\"}",
+            Mainopt.CommandsFromJson.at(0));
+  ASSERT_EQ("{\"another command\":\"test\"}", Mainopt.CommandsFromJson.at(1));
+}
