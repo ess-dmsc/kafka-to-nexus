c14c1789c1fb5c121603613e12ce636a89b9b97a
Michele Brambilla <michele.brambilla@psi.ch>
2018-09-20 10:57:26 +0200

    Let the master know the ServiceId

--- src/CommandHandler.cpp
+++ src/CommandHandler.cpp
@@ -271,9 +271,9 @@ void CommandHandler::handleNew(std::string const &Command) {
   if (MasterPtr) {
     // Register the task with master.
     LOG(Sev::Info, "Write file with job_id: {}", Task->job_id());
-    auto s = std::unique_ptr<StreamMaster<Streamer>>(new StreamMaster<Streamer>(
-        Broker.host_port, std::move(Task), Config.StreamerConfiguration,
-        MasterPtr->getStatusProducer()));
+    auto s = std::unique_ptr<StreamMaster<Streamer>>(
+        new StreamMaster<Streamer>(Broker.host_port, std::move(Task), Config,
+                                   MasterPtr->getStatusProducer()));
     if (auto status_producer = MasterPtr->getStatusProducer()) {
       s->report(std::chrono::milliseconds{Config.status_master_interval});
     }
