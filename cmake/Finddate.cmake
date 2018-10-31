5b86fe5bf09f39a07ff416e425601f8baa65b50f
Jack Harper <jack.harper@stfc.ac.uk>
2018-10-31 13:47:21 +0000

    re #176 adding library
    Co-authored by: Michal Chorazak <michochozaku@gmail.com>

--- cmake/Finddate.cmake
+++ cmake/Finddate.cmake
@@ -1,6 +1,6 @@
 find_path(DATE_INCLUDE_DIR NAMES date/date.h)
 include(FindPackageHandleStandardArgs)
-find_library(DATE_LIBRARY NAMES date)
+find_library(DATE_LIBRARY NAMES libtz.a)
 
 find_package_handle_standard_args(DATE DEFAULT_MSG
 		DATE_INCLUDE_DIR
