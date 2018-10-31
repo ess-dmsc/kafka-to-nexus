07503fb7073ebe70887c0e7fc626dbfd7261c1d3
Jack Harper <jack.harper@stfc.ac.uk>
2018-10-31 13:36:50 +0000

    re #176 adding library

--- cmake/Finddate.cmake
+++ cmake/Finddate.cmake
@@ -1,5 +1,8 @@
 find_path(DATE_INCLUDE_DIR NAMES date/date.h)
 include(FindPackageHandleStandardArgs)
+find_library(DATE_LIBRARY NAMES date)
+
 find_package_handle_standard_args(DATE DEFAULT_MSG
 		DATE_INCLUDE_DIR
+		DATE_LIBRARY
 )
