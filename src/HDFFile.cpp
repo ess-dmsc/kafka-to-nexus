cf359a114829168c1ef2ab33b4a58632a0379d69
Matt Clarke <matt.clarke@esss.se>
2018-11-09 11:19:18 +0100

    Update HDFFile.cpp

--- src/HDFFile.cpp
+++ src/HDFFile.cpp
@@ -788,9 +788,9 @@ void HDFFile::init(const std::string &Filename,
     setCommonProps(fcpl, fapl);
     if (UseHDFSWMR) {
       H5File = 
-         hdf5::file::create(Filename, hdf5::file::AccessFlags::TRUNCATE |
-                                          hdf5::file::AccessFlags::SWMR_WRITE,
-                            fcpl, fapl);
+          hdf5::file::create(Filename, hdf5::file::AccessFlags::TRUNCATE |
+                                           hdf5::file::AccessFlags::SWMR_WRITE,
+                             fcpl, fapl);
       SWMREnabled = true;
     } else {
       H5File = hdf5::file::create(Filename, hdf5::file::AccessFlags::EXCLUSIVE,
