0de011fcf53a37a60d24b7de89adb0ec5668fa66
Dominik Werder <dominik.werder@psi.ch>
2018-11-22 11:24:07 +0100

    Disable test, filed new issue #360

--- src/tests/StreamerTest.cpp
+++ src/tests/StreamerTest.cpp
@@ -418,7 +418,10 @@ TEST_F(StreamerProcessTimingTest, EmptyMessageBeforeStop) {
   EXPECT_EQ(TestStreamer.pollAndProcess(Demuxer), ProcessMessageResult::OK);
 }
 
-TEST_F(StreamerProcessTimingTest, EmptyMessageSlightlyAfterStop) {
+// According to Michele, that functionality is currently broken and needs to be
+// revisited.
+// See issue #360
+TEST_F(StreamerProcessTimingTest, DISABLED_EmptyMessageSlightlyAfterStop) {
   FlatbufferReaderRegistry::Registrar<StreamerTestDummyReader> RegisterIt(
 
       ReaderKey);
