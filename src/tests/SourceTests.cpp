7e76f2aff0f3df860ba1234a6c77d3de31be4c51
Dominik Werder <dominik.werder@psi.ch>
2018-11-23 12:18:09 +0100

    Add unit tests for Source

---
+++ src/tests/SourceTests.cpp
@@ -0,0 +1,127 @@
+#include <FlatbufferMessage.h>
+#include <ProcessMessageResult.h>
+#include <Source.h>
+#include <flatbuffers/flatbuffers.h>
+#include <gtest/gtest.h>
+#include <trompeloeil.hpp>
+
+namespace ev42 {
+#include "schemas/ev42_events_generated.h"
+}
+
+using FileWriter::Source;
+using FileWriter::HDFWriterModule;
+using FileWriter::FlatbufferMessage;
+
+class WriterModuleDummy : public HDFWriterModule {
+public:
+  void parse_config(std::string const &ConfigurationStream,
+                    std::string const &ConfigurationModule) override {}
+  InitResult init_hdf(hdf5::node::Group &HDFGroup,
+                      std::string const &HDFAttributes) override {
+    return InitResult::OK();
+  }
+  InitResult reopen(hdf5::node::Group &HDFGrup) override {
+    return InitResult::OK();
+  }
+  WriteResult write(FlatbufferMessage const &Message) override {
+    return WriteResult::OK();
+  }
+  std::int32_t flush() override { return 0; }
+  std::int32_t close() override { return 0; }
+  void enable_cq(CollectiveQueue *cq, HDFIDStore *hdf_store,
+                 int mpi_rank) override {}
+};
+
+class WriterModuleMock : public WriterModuleDummy {
+public:
+  MAKE_MOCK1(write, WriteResult(FlatbufferMessage const &), override);
+};
+
+TEST(Source, ConstructorSetsMembers) {
+  std::string SourceName("TestSourceName");
+  std::string TopicName("TestTopicName");
+  std::string ModuleName("test");
+  auto WriterModule = std::make_unique<WriterModuleDummy>();
+  Source TestSource(SourceName, ModuleName, std::move(WriterModule));
+  TestSource.setTopic(TopicName);
+  ASSERT_EQ(TestSource.topic(), TopicName);
+  ASSERT_EQ(TestSource.sourcename(), SourceName);
+}
+
+TEST(Source, MovedSourceHasCorrectState) {
+  std::string SourceName("TestSourceName");
+  std::string TopicName("TestTopicName");
+  std::string ModuleName("test");
+  auto WriterModule = std::make_unique<WriterModuleDummy>();
+  Source TestSource(SourceName, ModuleName, std::move(WriterModule));
+  TestSource.setTopic(TopicName);
+  auto TestSource2 = std::move(TestSource);
+  ASSERT_EQ(TestSource2.topic(), TopicName);
+  ASSERT_EQ(TestSource2.sourcename(), SourceName);
+}
+
+TEST(Source, ProcessMessagePassesMessageToWriterModule) {
+  std::string SourceName("TestSourceName");
+  std::string TopicName("TestTopicName");
+  std::string ModuleName("ev42");
+  auto WriterModule = std::make_unique<WriterModuleMock>();
+  REQUIRE_CALL(*WriterModule, write(ANY(FlatbufferMessage const &)))
+      .RETURN(FileWriter::HDFWriterModule::WriteResult::OK());
+  Source TestSource(SourceName, ModuleName, std::move(WriterModule));
+  TestSource.setTopic(TopicName);
+  flatbuffers::FlatBufferBuilder Builder;
+  ev42::EventMessageBuilder EventMessage(Builder);
+  ev42::FinishEventMessageBuffer(Builder, EventMessage.Finish());
+  FlatbufferMessage Message(
+      reinterpret_cast<char *>(Builder.GetBufferPointer()), Builder.GetSize());
+  ASSERT_EQ(FileWriter::ProcessMessageResult::OK,
+            TestSource.process_message(Message));
+}
+
+TEST(Source, ProcessMessageReturnsErrorIfWriterModuleReturnsError) {
+  std::string SourceName("TestSourceName");
+  std::string TopicName("TestTopicName");
+  std::string ModuleName("ev42");
+  auto WriterModule = std::make_unique<WriterModuleMock>();
+  REQUIRE_CALL(*WriterModule, write(ANY(FlatbufferMessage const &)))
+      .RETURN(FileWriter::HDFWriterModule::WriteResult::ERROR_IO());
+  Source TestSource(SourceName, ModuleName, std::move(WriterModule));
+  TestSource.setTopic(TopicName);
+  flatbuffers::FlatBufferBuilder Builder;
+  ev42::EventMessageBuilder EventMessage(Builder);
+  ev42::FinishEventMessageBuffer(Builder, EventMessage.Finish());
+  FlatbufferMessage Message(
+      reinterpret_cast<char *>(Builder.GetBufferPointer()), Builder.GetSize());
+  ASSERT_EQ(FileWriter::ProcessMessageResult::ERR,
+            TestSource.process_message(Message));
+}
+
+TEST(Source, ProcessMessageWithNonMatchingSchemaIdReturnsError) {
+  std::string SourceName("TestSourceName");
+  std::string TopicName("TestTopicName");
+  std::string ModuleName("test");
+  auto WriterModule = std::make_unique<WriterModuleDummy>();
+  Source TestSource(SourceName, ModuleName, std::move(WriterModule));
+  TestSource.setTopic(TopicName);
+  flatbuffers::FlatBufferBuilder Builder;
+  ev42::EventMessageBuilder EventMessage(Builder);
+  ev42::FinishEventMessageBuffer(Builder, EventMessage.Finish());
+  FlatbufferMessage Message(
+      reinterpret_cast<char *>(Builder.GetBufferPointer()), Builder.GetSize());
+  ASSERT_EQ(FileWriter::ProcessMessageResult::ERR,
+            TestSource.process_message(Message));
+}
+
+TEST(Source, ProcessMessageWithoutWriterModuleReturnsError) {
+  std::string SourceName("TestSourceName");
+  std::string ModuleName("ev42");
+  Source TestSource(SourceName, ModuleName, {nullptr});
+  flatbuffers::FlatBufferBuilder Builder;
+  ev42::EventMessageBuilder EventMessage(Builder);
+  ev42::FinishEventMessageBuffer(Builder, EventMessage.Finish());
+  FlatbufferMessage Message(
+      reinterpret_cast<char *>(Builder.GetBufferPointer()), Builder.GetSize());
+  ASSERT_EQ(FileWriter::ProcessMessageResult::ERR,
+            TestSource.process_message(Message));
+}
