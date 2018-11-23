3a05bd73778ecbfacf402b988412d2bed817e513
Jack Harper <jack.harper@stfc.ac.uk>
2018-11-23 14:57:42 +0000

    re #344 fixing tests
    Signed-off-by: Jack Harper <jack.harper@stfc.ac.uk>

--- system-tests/test_filewriter_ignores_commands.py
+++ system-tests/test_filewriter_ignores_commands.py
@@ -3,13 +3,13 @@ from time import sleep
 from subprocess import check_output
 
 # Simple system test for checking that a file writer only responds to commands given with the correct service-id.
+# Exit command tested, shouldn't matter if the file-writing has started or not.
 
 def test_ignores_commands_with_incorrect_id(docker_compose_multiple_instances):
     sleep(10)
     producer = create_producer()
-    send_writer_command("commands/writer-exit-single.json", producer)
+    send_writer_command("commands/writer-exit-single.json", producer, "TEST_writerCommandMultiple")
     sleep(5)
     containers = check_output('docker ps', shell=True)
+    assert b"filewriter1" in containers
     assert b"filewriter2" not in containers
-
-
