0cea25e7b0afe2f639fef8f835964c223bc1a5fb
Jack Harper <jack.harper@stfc.ac.uk>
2018-12-05 12:59:01 +0000

    Update test_filewriter_ignores_commands.py

--- system-tests/test_filewriter_ignores_commands.py
+++ system-tests/test_filewriter_ignores_commands.py
@@ -12,7 +12,7 @@ def test_ignores_commands_with_incorrect_id(docker_compose_multiple_instances):
     send_writer_command("commands/writer-exit-single.json", producer, "TEST_writerCommandMultiple")
     # Wait for filewriter2 to exit
     found = False
-    for i in range(15):
+    for i in range(20):
         containers = check_output('docker ps', shell=True)
         if b"filewriter1" in containers and b"filewriter2" not in containers:
             found = True
