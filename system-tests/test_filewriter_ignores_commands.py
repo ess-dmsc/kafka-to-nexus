ac3abf12b3cd8a7425f4e2507409df012478eba9
Jack Harper <jack.harper@stfc.ac.uk>
2018-12-17 09:31:24 +0000

    possible fix for tests on jenkins
    Signed-off-by: Jack Harper <jack.harper@stfc.ac.uk>

--- system-tests/test_filewriter_ignores_commands.py
+++ system-tests/test_filewriter_ignores_commands.py
@@ -5,6 +5,7 @@ from subprocess import check_output
 # Simple system test for checking that a file writer only responds to commands given with the correct service-id.
 # Exit command tested, shouldn't matter if the file-writing has started or not.
 
+
 def test_ignores_commands_with_incorrect_id(docker_compose_multiple_instances):
     producer = create_producer()
     sleep(10)
@@ -12,7 +13,7 @@ def test_ignores_commands_with_incorrect_id(docker_compose_multiple_instances):
     send_writer_command("commands/writer-exit-single.json", producer, "TEST_writerCommandMultiple")
     # Wait for filewriter2 to exit
     found = False
-    for i in range(30):
+    for i in range(60):
         containers = check_output('docker ps', shell=True)
         if b"filewriter1" in containers and b"filewriter2" not in containers:
             found = True
