cfe7e70d06ea99d9a3b42660f1e6a81d585a04a9
Jack Harper <jack.harper@stfc.ac.uk>
2018-12-20 10:09:55 +0000

    re DM-1242 fixing system test
    Signed-off-by: Jack Harper <jack.harper@stfc.ac.uk>

--- system-tests/test_filewriter_ignores_commands.py
+++ system-tests/test_filewriter_ignores_commands.py
@@ -1,24 +1,21 @@
 from helpers.kafkahelpers import create_producer, send_writer_command
 from time import sleep
-from subprocess import check_output
+from docker import DockerClient
 
-# Simple system test for checking that a file writer only responds to commands given with the correct service-id.
-# Exit command tested, shouldn't matter if the file-writing has started or not.
 
-# Disabled for now - seems to be unreliable.
-#
-# def test_ignores_commands_with_incorrect_id(docker_compose_multiple_instances):
-#     producer = create_producer()
-#     sleep(10)
-#     # Command only filewriter2 to exit
-#     send_writer_command("commands/writer-exit-single.json", producer, "TEST_writerCommandMultiple")
-#     # Wait for filewriter2 to exit
-#     found = False
-#     for i in range(30):
-#         containers = check_output('docker ps', shell=True)
-#         if b"filewriter1" in containers and b"filewriter2" not in containers:
-#             found = True
-#             break
-#         else:
-#             sleep(1)
-#     assert found
+def test_ignores_commands_with_incorrect_id(docker_compose_multiple_instances):
+    producer = create_producer()
+    sleep(10)
+    # Command only filewriter2 to exit
+    send_writer_command("commands/writer-exit-single.json", producer, "TEST_writerCommandMultiple")
+    # Wait for filewriter2 to exit
+    client = DockerClient()
+    stopped = False
+    for i in range(30):
+        containers = client.containers.list(True)
+        for container in containers:
+            if "filewriter1" in container.name and "filewriter2" not in container.name:
+                stopped = True
+                break
+        sleep(1)
+    assert stopped
