91aa08acde41d27ff5dc88ab0934be24d440c3fe
Jonas Nilsson <jonas.nilsson@esss.se>
2019-01-10 22:32:08 +0100

    More code cleanup.

--- src/helper.h
+++ src/helper.h
@@ -12,8 +12,3 @@ int getpid_wrapper();
 std::string gethostname_wrapper();
 
 std::vector<char> readFileIntoVector(std::string const &FileName);
-
-std::vector<char> binaryToHex(char const *data, uint32_t len);
-
-std::vector<std::string> split(std::string const &input,
-                               std::string const &token);
