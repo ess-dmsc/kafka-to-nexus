28e9713f1b40f0bb6bcab9480af4e02a2ba096c2
mchorazak <michochozaku@gmail.com>
2019-02-22 13:00:18 +0000

    re DM-1168 demuxtopic logger

--- src/DemuxTopic.cpp
+++ src/DemuxTopic.cpp
@@ -1,14 +1,14 @@
 #include "DemuxTopic.h"
 #include "FlatbufferReader.h"
-#include "logger.h"
 #include <limits>
 #include <stdexcept>
 
 namespace FileWriter {
 
-DemuxTopic::DemuxTopic(std::string TopicName) : Topic(std::move(TopicName)) {}
+DemuxTopic::DemuxTopic(std::string TopicName)
+    : Topic(std::move(TopicName)), Logger(spdlog::get("filewriterlogger")) {}
 
-DemuxTopic::~DemuxTopic() { LOG(spdlog::level::trace, "dtor"); }
+DemuxTopic::~DemuxTopic() { Logger->trace("DemuxTopic destructor"); }
 
 DemuxTopic::DemuxTopic(DemuxTopic &&x) noexcept { swap(*this, x); }
 
@@ -21,16 +21,15 @@ std::string const &DemuxTopic::topic() const { return Topic; }
 
 ProcessMessageResult
 DemuxTopic::process_message(FlatbufferMessage const &Message) {
-  LOG(spdlog::level::trace, "Message received from: {}",
-      Message.getSourceName());
+  Logger->trace("Message received from: {}", Message.getSourceName());
   try {
     auto &CurrentSource = TopicSources.at(Message.getSourceName());
     auto ProcessingResult = CurrentSource.process_message(Message);
     ++messages_processed;
     return ProcessingResult;
   } catch (std::out_of_range &e) {
-    LOG(spdlog::level::trace, "Source with name \"{}\" is not in list.",
-        Message.getSourceName());
+    Logger->trace("Source with name \"{}\" is not in list.",
+                  Message.getSourceName());
     ++error_no_source_instance;
   }
   return ProcessMessageResult::ERR;
