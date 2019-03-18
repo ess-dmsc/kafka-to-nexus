2cca0c42e7d04732e215518a75c848d3f4456690
mchorazak <michochozaku@gmail.com>
2019-03-18 10:52:41 +0000

    DM-1168 use macro only in event callback

--- src/KafkaW/KafkaEventCb.h
+++ src/KafkaW/KafkaEventCb.h
@@ -2,33 +2,58 @@
 #include "logger.h"
 #include <librdkafka/rdkafkacpp.h>
 
+#ifdef _MSC_VER
+
+// The levels used in the LOG macro are defined in the spdlog::level namespace
+// in spdlog.h
+#define LOG(level, fmt, ...)                                                   \
+  { spdlog::get("filewriterlogger")->log(level, fmt, __VA_ARGS__); }
+#else
+#define LOG(level, fmt, args...)                                               \
+  { spdlog::get("filewriterlogger")->log(level, fmt, ##args); }
+#endif
+
 namespace KafkaW {
 class KafkaEventCb : public RdKafka::EventCb {
 public:
-  void event_cb(RdKafka::Event &event) override {
-    switch (event.type()) {
+  void event_cb(RdKafka::Event &Event) override {
+    switch (Event.type()) {
     case RdKafka::Event::EVENT_ERROR:
-      LOG(event.severity(),
-          "Kafka EVENT_ERROR id: {}  broker: {}  errno: {}  errorname: {}  "
+      LOG(spdlog::level::level_enum(LogLevels.at(Event.severity())),
+          "Kafka EVENT_ERROR id: {}  broker: {}  errorname: {}  "
           "errorstring: {}",
-          event.broker_id(), event.broker_name(), event.type(),
-          RdKafka::err2str(event.err()), event.str());
+          Event.broker_id(), Event.broker_name().c_str(),
+          RdKafka::err2str(Event.err()), Event.str());
       break;
     case RdKafka::Event::EVENT_STATS:
-      LOG(event.severity(), "Kafka Stats id: {} broker: {} message: {}",
-          event.broker_id(), event.broker_name(), event.str());
+      LOG(spdlog::level::level_enum(LogLevels.at(Event.severity())),
+          "Kafka Stats id: {} broker: {} message: {}", Event.broker_id(),
+          Event.broker_name(), Event.str());
       break;
     case RdKafka::Event::EVENT_LOG:
-      LOG(event.severity(),
+      LOG(spdlog::level::level_enum(LogLevels.at(Event.severity())),
           "Kafka Log id: {} broker: {} severity: {}, facilitystr: {}:{}",
-          event.broker_id(), event.broker_name(), event.severity(), event.fac(),
-          event.str());
+          Event.broker_id(), Event.broker_name(), Event.severity(), Event.fac(),
+          Event.str());
       break;
     default:
-      LOG(event.severity(), "Kafka Event {} ({}): {}", event.type(),
-          RdKafka::err2str(event.err()), event.str());
+      LOG(spdlog::level::level_enum(LogLevels.at(Event.severity())),
+          "Kafka Event {} ({}): {}", Event.type(),
+          RdKafka::err2str(Event.err()), Event.str());
       break;
     }
   };
+
+private:
+  std::map<RdKafka::Event::Severity, int> LogLevels{
+      {RdKafka::Event::Severity::EVENT_SEVERITY_DEBUG, SPDLOG_LEVEL_TRACE},
+      {RdKafka::Event::Severity::EVENT_SEVERITY_INFO, SPDLOG_LEVEL_DEBUG},
+      {RdKafka::Event::Severity::EVENT_SEVERITY_NOTICE, SPDLOG_LEVEL_INFO},
+      {RdKafka::Event::Severity::EVENT_SEVERITY_WARNING, SPDLOG_LEVEL_WARN},
+      {RdKafka::Event::Severity::EVENT_SEVERITY_ERROR, SPDLOG_LEVEL_ERROR},
+      {RdKafka::Event::Severity::EVENT_SEVERITY_CRITICAL,
+       SPDLOG_LEVEL_CRITICAL},
+      {RdKafka::Event::Severity::EVENT_SEVERITY_ALERT, SPDLOG_LEVEL_OFF},
+      {RdKafka::Event::Severity::EVENT_SEVERITY_EMERG, SPDLOG_LEVEL_OFF}};
 };
 }
--- src/logger.h
+++ src/logger.h
@@ -14,26 +14,7 @@
 extern int log_level;
 
 extern std::string g_ServiceID;
-#define UNUSED(x) (void)(x)
-#define LOG(level, fmt, args...)                                               \
-  { temporaryLogger(static_cast<int>(level), fmt, ##args); }
-
-enum class Sev : int {
-  Emergency = 0, // Do not use, reserved for system errors
-  Alert = 1,     // Do not use, reserved for system errors
-  Critical = 2,  // Critical, i.e. wake me up in the middle of the night
-  Error = 3,     // Error, I should fix this as soon as possible
-  Warning = 4,   // Could be an indication of a problem that needs fixing
-  Notice = 5,    // Notice, potentially important events that might need special
-                 // treatment
-  Info = 6,  // Informational, general run time info for checking the state of
-             // the application
-  Debug = 7, // Debug, give me a flood of information
-};
 
 void setUpLogging(const spdlog::level::level_enum &LoggingLevel,
                   const std::string &ServiceID, const std::string &LogFile,
                   const std::string &GraylogURI);
-
-template <typename... TT>
-void temporaryLogger(int level, char const *fmt, TT const &... args) {}
