0d3841f04f48ccbf07f7873f1cb4774a05cfa153
mchorazak <michochozaku@gmail.com>
2019-04-01 11:53:16 +0100

    DM-1168 set formatting patterns for sinks

--- src/logger.cpp
+++ src/logger.cpp
@@ -18,25 +18,29 @@ void setUpLogging(const spdlog::level::level_enum &LoggingLevel,
   if (!LogFile.empty()) {
     sinks.push_back(
         std::make_shared<spdlog::sinks::basic_file_sink_mt>(LogFile));
+    sinks[sinks.size() - 1]->set_pattern(
+        "[%Y-%m-%d %H:%M:%S.%f] [%l] [processID: %P]: %v");
   }
   if (!GraylogURI.getURIString().empty()) {
 #ifdef HAVE_GRAYLOG_LOGGER
     sinks.push_back(std::make_shared<spdlog::sinks::graylog_sink_mt>(
         GraylogURI.HostPort.substr(0, GraylogURI.HostPort.find(":")),
         GraylogURI.Port));
+    sinks[sinks.size() - 1]->set_pattern("[%l] [processID: %P]: %v");
+
 #else
     spdlog::log(
         spdlog::level::err,
-        "ERROR not compiled with support for graylog_logger. Would have used "
-        "{}",
+        "ERROR not compiled with support for graylog_logger. Would have used{}",
         GraylogURI.HostPort);
 #endif
-  } else {
-    sinks.push_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
   }
+  sinks.push_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+  sinks[sinks.size() - 1]->set_pattern(
+      "[%H:%M:%S.%f] [%l] [processID: %P]: %v");
   auto combined_logger = std::make_shared<spdlog::logger>(
       "filewriterlogger", begin(sinks), end(sinks));
   spdlog::register_logger(combined_logger);
   combined_logger->set_level(LoggingLevel);
-  combined_logger->flush_on(spdlog::level::critical);
+  combined_logger->flush_on(spdlog::level::err);
 }
