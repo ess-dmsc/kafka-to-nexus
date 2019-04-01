7e87311018e87e45afa12b13c136e97cf94176c4
mchorazak <michochozaku@gmail.com>
2019-04-01 12:20:23 +0100

    DM-1168 refactor

--- src/logger.cpp
+++ src/logger.cpp
@@ -16,18 +16,18 @@ void setUpLogging(const spdlog::level::level_enum &LoggingLevel,
 
   std::vector<spdlog::sink_ptr> sinks;
   if (!LogFile.empty()) {
-    sinks.push_back(
-        std::make_shared<spdlog::sinks::basic_file_sink_mt>(LogFile));
-    sinks[sinks.size() - 1]->set_pattern(
-        "[%Y-%m-%d %H:%M:%S.%f] [%l] [processID: %P]: %v");
+    auto FileSink =
+        std::make_shared<spdlog::sinks::basic_file_sink_mt>(LogFile);
+    FileSink->set_pattern("[%Y-%m-%d %H:%M:%S.%f] [%l] [processID: %P]: %v");
+    sinks.push_back(FileSink);
   }
-  if (!GraylogURI.getURIString().empty()) {
+  if (GraylogURI.getURIString() != "/") {
 #ifdef HAVE_GRAYLOG_LOGGER
-    sinks.push_back(std::make_shared<spdlog::sinks::graylog_sink_mt>(
+    auto GraylogSink = std::make_shared<spdlog::sinks::graylog_sink_mt>(
         GraylogURI.HostPort.substr(0, GraylogURI.HostPort.find(":")),
-        GraylogURI.Port));
-    sinks[sinks.size() - 1]->set_pattern("[%l] [processID: %P]: %v");
-
+        GraylogURI.Port);
+    GraylogSink->set_pattern("[%l] [processID: %P]: %v");
+    sinks.push_back(GraylogSink);
 #else
     spdlog::log(
         spdlog::level::err,
@@ -35,9 +35,9 @@ void setUpLogging(const spdlog::level::level_enum &LoggingLevel,
         GraylogURI.HostPort);
 #endif
   }
-  sinks.push_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
-  sinks[sinks.size() - 1]->set_pattern(
-      "[%H:%M:%S.%f] [%l] [processID: %P]: %v");
+  auto ConsoleSink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+  ConsoleSink->set_pattern("[%H:%M:%S.%f] [%l] [processID: %P]: %v");
+  sinks.push_back(ConsoleSink);
   auto combined_logger = std::make_shared<spdlog::logger>(
       "filewriterlogger", begin(sinks), end(sinks));
   spdlog::register_logger(combined_logger);
