581743597a5832a24507df17691158b7b7a925bb
michele-brambilla <michele.brambilla@psi.ch>
2019-06-18 15:28:36 +0200

    Some cleanup

--- src/schemas/ns10/NicosCacheWriter.h
+++ src/schemas/ns10/NicosCacheWriter.h
@@ -2,23 +2,7 @@
 
 /// \file
 ///
-/// \brief This file acts as a template for creating file writing modules.
-///
-/// All of the classes required are explained here. The
-/// only thing missing from this header file is the registration of the file
-/// writing module which must reside in an implementation file. See the
-/// accompanying TestWriter.cpp for instructions on how this should be done.
-///
-/// A file writing module comprises two classes. The first class is the
-/// flatbuffer reader which must implement the FileWriter::FlatbufferReader
-/// interface. The virtual functions to be overridden are called to verify the
-/// flatbuffer contents and extract a source name and a timestamp from the
-/// flatbuffer. See the documentation of the individual member functions for
-/// more information on how these should be implemented.
-/// The second class which you must implement is a class that inherits from the
-/// abstract class FileWriter::HDFWriterModule. This class should implement the
-/// actual writing of flatbuffer data to the HDF5 file. More information on the
-/// pure virtual functions that you must implement can be found below.
+/// \brief Writing module for the NICOS cache values.
 
 #pragma once
 #include "../../FlatbufferReader.h"
@@ -29,17 +13,15 @@
 
 namespace NicosCacheWriter {
 
+/// h5cpp dataset class that writers strings.
 class StringValue : public NeXusDataset::ExtensibleDataset<std::string> {
 public:
   StringValue() = default;
-  /// \brief Create the string_value dataset of NXLog.
-  /// \throw std::runtime_error if dataset already exists.
+  /// \brief Create the value dataset of NXLog.
   StringValue(hdf5::node::Group const &Parent, NeXusDataset::Mode CMode,
               size_t ChunkSize = 1024);
 };
 
-/// \brief This class is used to extract information from a flatbuffer which
-/// uses a specific four character file identifier.
 class CacheReader : public FileWriter::FlatbufferReader {
 public:
   bool verify(FileWriter::FlatbufferMessage const &Message) const override;
