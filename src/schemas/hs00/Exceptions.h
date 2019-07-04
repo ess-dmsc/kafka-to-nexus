5a6599b99bc20269e850a598745d2f1fbe0378cf
mchorazak <michochozaku@gmail.com>
2019-07-04 09:21:07 +0100

    fix cppcheck errors

--- src/schemas/hs00/Exceptions.h
+++ src/schemas/hs00/Exceptions.h
@@ -10,7 +10,8 @@ namespace hs00 {
 class UnexpectedJsonInput : public std::runtime_error {
 public:
   UnexpectedJsonInput() : std::runtime_error("UnexpectedJsonInput") {}
-  UnexpectedJsonInput(std::string Error) : std::runtime_error(Error) {}
+  explicit UnexpectedJsonInput(const std::string &Error)
+      : std::runtime_error(Error) {}
 };
 } // namespace hs00
 } // namespace Schemas
