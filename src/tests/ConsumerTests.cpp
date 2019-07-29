c23808b8ad7dedda6a76d51cf341d09a987ba40e
Matthew D Jones <matthew.d.jones@tessella.com>
2019-07-29 13:47:26 +0100

    Cover case of position lookup failing in getCurrentOffsets

--- src/tests/ConsumerTests.cpp
+++ src/tests/ConsumerTests.cpp
@@ -445,6 +445,38 @@ TEST_F(ConsumerTests, testGetCurrentOffsetsDoesNotThrowIfTopicExists) {
   }
 }
 
+TEST_F(ConsumerTests, testGetCurrentOffsetsThrowsIfPositionQueryFails) {
+  auto Metadata = new MockMetadata;
+  auto MockConsumer = std::make_unique<MockKafkaConsumer>(
+      RdKafka::ErrorCode::ERR_NO_ERROR, Metadata);
+  REQUIRE_CALL(*MockConsumer, close()).TIMES((1)).RETURN(RdKafka::ERR_NO_ERROR);
+  // TopicMetadata contains our test topic
+  std::string TopicName = "test_topic";
+  auto TopicMetadata = std::make_unique<MockTopicMetadata>(TopicName);
+  auto TopicVector =
+      RdKafka::Metadata::TopicMetadataVector{TopicMetadata.get()};
+  auto PartitionMetadata = std::make_unique<MockPartitionMetadata>();
+  auto PartitionMetadataVector =
+      RdKafka::TopicMetadata::PartitionMetadataVector{PartitionMetadata.get()};
+  REQUIRE_CALL(*PartitionMetadata, id()).TIMES((1)).RETURN(1);
+  REQUIRE_CALL(*TopicMetadata, partitions())
+      .TIMES(1)
+      .RETURN(&PartitionMetadataVector);
+  REQUIRE_CALL(*MockConsumer, position(_))
+      .TIMES(1)
+      .RETURN(RdKafka::ErrorCode::ERR__FAIL);
+  REQUIRE_CALL(*Metadata, topics()).TIMES(1).RETURN(&TopicVector);
+  {
+    auto Consumer = std::make_unique<KafkaW::Consumer>(
+        std::move(MockConsumer),
+        std::unique_ptr<RdKafka::Conf>(
+            RdKafka::Conf::create(RdKafka::Conf::CONF_GLOBAL)),
+        std::make_unique<KafkaW::KafkaEventCb>());
+    EXPECT_THROW(Consumer->getCurrentOffsets(TopicName), std::runtime_error)
+        << "Expected getCurrentOffsets to throw as the position lookup failed";
+  }
+}
+
 TEST_F(ConsumerTests,
        testOffsetsForTimesAllPartitionsReturnsOneValuePerPartition) {
   auto Metadata = new MockMetadata;
