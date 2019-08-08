cc1584330dbbf11a2aeff3f9827e3e7a7a48a483
Tobias Richter <tobias.richter@esss.se>
2019-08-08 17:14:47 +0200

    add license headers
    DM-1151

--- src/tests/Schema_f142Tests.cpp
+++ src/tests/Schema_f142Tests.cpp
@@ -1,3 +1,12 @@
+// SPDX-License-Identifier: BSD-2-Clause
+//
+// This code has been produced by the European Spallation Source
+// and its partner institutes under the BSD 2 Clause License.
+//
+// See LICENSE.md at the top level for license information.
+//
+// Screaming Udder!                              https://esss.se
+
 // This filename is chosen such that it shows up in searches after the
 // case-sensitive flatbuffer schema identifier.
 
