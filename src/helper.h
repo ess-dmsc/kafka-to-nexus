196633f53189018d3ae938656b774f79a5cc1e77
Matt Clarke <matt.clarke@esss.se>
2019-09-23 08:53:02 +0200

    Update helper.h

--- src/helper.h
+++ src/helper.h
@@ -17,3 +17,5 @@ int getpid_wrapper();
 std::string gethostname_wrapper();
 
 std::vector<char> readFileIntoVector(std::string const &FileName);
+
+std::chrono::duration<long long int, std::milli> getCurrentTimeStampMS();
