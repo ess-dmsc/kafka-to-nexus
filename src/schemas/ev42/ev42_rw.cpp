b7725025aa4870594dc683c46685c32186072a90
Jonas Nilsson <jonas.nilsson@esss.se>
2019-10-25 08:57:06 +0200

    Removed last use of the h5-files.

--- src/schemas/ev42/ev42_rw.cpp
+++ src/schemas/ev42/ev42_rw.cpp
@@ -16,7 +16,7 @@
 
 namespace {
 template <typename DataType>
-ArrayAdapter<const DataType>
+ArrayAdapter<const DataType> const
 getFBVectorAsArrayAdapter(const flatbuffers::Vector<DataType> *Data) {
   return {Data->data(), Data->size()};
 }
@@ -64,55 +64,33 @@ static FlatbufferReaderRegistry::Registrar<FlatbufferReader>
 void HDFWriterModule::parse_config(std::string const &ConfigurationStream) {
   auto ConfigurationStreamJson = json::parse(ConfigurationStream);
   try {
-    index_every_bytes =
+    EventIndexInterval =
         ConfigurationStreamJson["nexus"]["indices"]["index_every_kb"]
             .get<uint64_t>() *
         1024;
-    Logger->trace("index_every_bytes: {}", index_every_bytes);
+    Logger->trace("Event index interval: {}", EventIndexInterval);
   } catch (...) { /* it's ok if not found */
   }
   try {
-    index_every_bytes =
+    EventIndexInterval =
         ConfigurationStreamJson["nexus"]["indices"]["index_every_mb"]
             .get<uint64_t>() *
         1024 * 1024;
-    Logger->trace("index_every_bytes: {}", index_every_bytes);
+    Logger->trace("Event index interval: {}", EventIndexInterval);
   } catch (...) { /* it's ok if not found */
   }
   try {
-    chunk_bytes =
+    ChunkSizeBytes =
         ConfigurationStreamJson["nexus"]["chunk"]["chunk_kb"].get<uint64_t>() *
         1024;
-    Logger->trace("chunk_bytes: {}", chunk_bytes);
+    Logger->trace("chunk_bytes: {}", ChunkSizeBytes);
   } catch (...) { /* it's ok if not found */
   }
   try {
-    chunk_bytes =
+    ChunkSizeBytes =
         ConfigurationStreamJson["nexus"]["chunk"]["chunk_mb"].get<uint64_t>() *
         1024 * 1024;
-    Logger->trace("chunk_bytes: {}", chunk_bytes);
-  } catch (...) { /* it's ok if not found */
-  }
-  try {
-    buffer_size =
-        ConfigurationStreamJson["nexus"]["buffer"]["size_kb"].get<uint64_t>() *
-        1024;
-    Logger->trace("buffer_size: {}", buffer_size);
-  } catch (...) { /* it's ok if not found */
-  }
-  try {
-    buffer_size =
-        ConfigurationStreamJson["nexus"]["buffer"]["size_mb"].get<uint64_t>() *
-        1024 * 1024;
-    Logger->trace("buffer_size: {}", buffer_size);
-  } catch (...) { /* it's ok if not found */
-  }
-  try {
-    buffer_packet_max =
-        ConfigurationStreamJson["nexus"]["buffer"]["packet_max_kb"]
-            .get<uint64_t>() *
-        1024;
-    Logger->trace("buffer_packet_max: {}", buffer_packet_max);
+    Logger->trace("chunk_bytes: {}", ChunkSizeBytes);
   } catch (...) { /* it's ok if not found */
   }
   try {
@@ -125,8 +103,8 @@ void HDFWriterModule::parse_config(std::string const &ConfigurationStream) {
 
 void HDFWriterModule::createAdcDatasets(hdf5::node::Group &HDFGroup) const {
   // bytes to number of elements
-  size_t ChunkSizeFor32BitTypes = chunk_bytes / 8;
-  size_t ChunkSizeFor64BitTypes = chunk_bytes / 16;
+  size_t ChunkSizeFor32BitTypes = ChunkSizeBytes / 4;
+  size_t ChunkSizeFor64BitTypes = ChunkSizeBytes / 8;
 
   NeXusDataset::Amplitude(        // NOLINT(bugprone-unused-raii)
       HDFGroup,                   // NOLINT(bugprone-unused-raii)
@@ -157,35 +135,46 @@ void HDFWriterModule::createAdcDatasets(hdf5::node::Group &HDFGroup) const {
 HDFWriterModule::InitResult
 HDFWriterModule::init_hdf(hdf5::node::Group &HDFGroup,
                           std::string const &HDFAttributes) {
-
+  auto Create = NeXusDataset::Mode::Create;
+  size_t Chunk32Bit = ChunkSizeBytes / 4;
+  size_t Chunk64Bit = ChunkSizeBytes / 8;
   try {
-    ds_event_time_offset = h5::h5d_chunked_1d<uint32_t>::create(
-        HDFGroup, "event_time_offset", chunk_bytes);
-    ds_event_id =
-        h5::h5d_chunked_1d<uint32_t>::create(HDFGroup, "event_id", chunk_bytes);
-    ds_event_time_zero = h5::h5d_chunked_1d<uint64_t>::create(
-        HDFGroup, "event_time_zero", chunk_bytes);
-    ds_event_index = h5::h5d_chunked_1d<uint32_t>::create(
-        HDFGroup, "event_index", chunk_bytes);
-    ds_cue_index = h5::h5d_chunked_1d<uint32_t>::create(HDFGroup, "cue_index",
-                                                        chunk_bytes);
-    ds_cue_timestamp_zero = h5::h5d_chunked_1d<uint64_t>::create(
-        HDFGroup, "cue_timestamp_zero", chunk_bytes);
+
+    NeXusDataset::EventTimeOffset(        // NOLINT(bugprone-unused-raii)
+        HDFGroup,                   // NOLINT(bugprone-unused-raii)
+        Create, // NOLINT(bugprone-unused-raii)
+        Chunk32Bit);    // NOLINT(bugprone-unused-raii)
+
+    NeXusDataset::EventId(        // NOLINT(bugprone-unused-raii)
+        HDFGroup,                   // NOLINT(bugprone-unused-raii)
+        Create, // NOLINT(bugprone-unused-raii)
+        Chunk32Bit);    // NOLINT(bugprone-unused-raii)
+
+    NeXusDataset::EventTimeZero(        // NOLINT(bugprone-unused-raii)
+        HDFGroup,                   // NOLINT(bugprone-unused-raii)
+        Create, // NOLINT(bugprone-unused-raii)
+        Chunk64Bit);    // NOLINT(bugprone-unused-raii)
+
+    NeXusDataset::EventIndex(        // NOLINT(bugprone-unused-raii)
+        HDFGroup,                   // NOLINT(bugprone-unused-raii)
+        Create, // NOLINT(bugprone-unused-raii)
+        Chunk32Bit);    // NOLINT(bugprone-unused-raii)
+
+    NeXusDataset::CueIndex(        // NOLINT(bugprone-unused-raii)
+        HDFGroup,                   // NOLINT(bugprone-unused-raii)
+        Create, // NOLINT(bugprone-unused-raii)
+        Chunk32Bit);    // NOLINT(bugprone-unused-raii)
+
+    NeXusDataset::CueTimestampZero(        // NOLINT(bugprone-unused-raii)
+        HDFGroup,                   // NOLINT(bugprone-unused-raii)
+        Create, // NOLINT(bugprone-unused-raii)
+        Chunk64Bit);    // NOLINT(bugprone-unused-raii)
+
 
     if (RecordAdcPulseDebugData) {
       createAdcDatasets(HDFGroup);
     }
 
-    if (!ds_event_time_offset || !ds_event_id || !ds_event_time_zero ||
-        !ds_event_index || !ds_cue_index || !ds_cue_timestamp_zero) {
-      ds_event_time_offset.reset();
-      ds_event_id.reset();
-      ds_event_time_zero.reset();
-      ds_event_index.reset();
-      ds_cue_index.reset();
-      ds_cue_timestamp_zero.reset();
-      throw std::runtime_error("Dataset init failed");
-    }
     if (HDFGroup.attributes.exists("NX_class")) {
       Logger->info("NX_class already specified!");
     } else {
@@ -205,17 +194,14 @@ HDFWriterModule::init_hdf(hdf5::node::Group &HDFGroup,
 
 HDFWriterModule::InitResult
 HDFWriterModule::reopen(hdf5::node::Group &HDFGroup) {
+  auto Open = NeXusDataset::Mode::Open;
   try {
-    ds_event_time_offset =
-        h5::h5d_chunked_1d<uint32_t>::open(HDFGroup, "event_time_offset");
-    ds_event_id = h5::h5d_chunked_1d<uint32_t>::open(HDFGroup, "event_id");
-    ds_event_time_zero =
-        h5::h5d_chunked_1d<uint64_t>::open(HDFGroup, "event_time_zero");
-    ds_event_index =
-        h5::h5d_chunked_1d<uint32_t>::open(HDFGroup, "event_index");
-    ds_cue_index = h5::h5d_chunked_1d<uint32_t>::open(HDFGroup, "cue_index");
-    ds_cue_timestamp_zero =
-        h5::h5d_chunked_1d<uint64_t>::open(HDFGroup, "cue_timestamp_zero");
+    EventTimeOffset = NeXusDataset::EventTimeOffset(HDFGroup, Open);
+    EventId = NeXusDataset::EventId(HDFGroup, Open);
+    EventTimeZero = NeXusDataset::EventTimeZero(HDFGroup, Open);
+    EventIndex = NeXusDataset::EventIndex(HDFGroup, Open);
+    CueIndex = NeXusDataset::CueIndex(HDFGroup, Open);
+    CueTimestampZero = NeXusDataset::CueTimestampZero(HDFGroup, Open);
     if (RecordAdcPulseDebugData) {
       reopenAdcDatasets(HDFGroup);
     }
@@ -224,26 +210,6 @@ HDFWriterModule::reopen(hdf5::node::Group &HDFGroup) {
         "Failed to reopen datasets in HDF file with error message: \"{}\"",
         std::string(E.what()));
     return HDFWriterModule::InitResult::ERROR;
-  }
-
-  ds_event_time_offset->buffer_init(buffer_size, buffer_packet_max);
-  ds_event_id->buffer_init(buffer_size, buffer_packet_max);
-  ds_event_time_zero->buffer_init(buffer_size, buffer_packet_max);
-  ds_event_index->buffer_init(buffer_size, buffer_packet_max);
-  ds_cue_index->buffer_init(buffer_size, buffer_packet_max);
-  ds_cue_timestamp_zero->buffer_init(buffer_size, buffer_packet_max);
-
-  if (!ds_event_time_offset || !ds_event_id || !ds_event_time_zero ||
-      !ds_event_index || !ds_cue_index || !ds_cue_timestamp_zero) {
-    ds_event_time_offset.reset();
-    ds_event_id.reset();
-    ds_event_time_zero.reset();
-    ds_event_index.reset();
-    ds_cue_index.reset();
-    ds_cue_timestamp_zero.reset();
-    throw std::runtime_error(
-        fmt::format("ev42 could not init hdf_parent: {}",
-                    static_cast<std::string>(HDFGroup.link().path())));
   }
   return HDFWriterModule::InitResult::OK;
 }
@@ -259,31 +225,24 @@ void HDFWriterModule::reopenAdcDatasets(const hdf5::node::Group &HDFGroup) {
 }
 
 void HDFWriterModule::write(FlatbufferMessage const &Message) {
-  if (!ds_event_time_offset) {
-    throw FileWriter::HDFWriterModuleRegistry::WriterException(
-        "Error, time of flight not present.");
-  }
   auto EventMsgFlatbuffer = GetEventMessage(Message.data());
-  auto EventTimeAppendResultInfo = ds_event_time_offset->append_data_1d(
-      EventMsgFlatbuffer->time_of_flight()->data(),
-      EventMsgFlatbuffer->time_of_flight()->size());
-  auto EventIdAppendResultInfo =
-      ds_event_id->append_data_1d(EventMsgFlatbuffer->detector_id()->data(),
-                                  EventMsgFlatbuffer->detector_id()->size());
-  if (EventTimeAppendResultInfo.ix0 != EventIdAppendResultInfo.ix0) {
+  EventTimeOffset.appendArray(getFBVectorAsArrayAdapter(EventMsgFlatbuffer->time_of_flight()));
+  EventId.appendArray(getFBVectorAsArrayAdapter(EventMsgFlatbuffer->detector_id()));
+  if (EventMsgFlatbuffer->time_of_flight()->size() != EventMsgFlatbuffer->detector_id()->size()) {
     Logger->warn("written data lengths differ");
   }
-  auto pulse_time = EventMsgFlatbuffer->pulse_time();
-  ds_event_time_zero->append_data_1d(&pulse_time, 1);
-  uint32_t event_index = EventTimeAppendResultInfo.ix0;
-  ds_event_index->append_data_1d(&event_index, 1);
-  total_written_bytes += EventTimeAppendResultInfo.written_bytes;
-  ts_max = std::max(pulse_time, ts_max);
-  if (total_written_bytes > index_at_bytes + index_every_bytes) {
-    ds_cue_timestamp_zero->append_data_1d(&ts_max, 1);
-    ds_cue_index->append_data_1d(&event_index, 1);
-    index_at_bytes = total_written_bytes;
+  auto CurrentRefTime = EventMsgFlatbuffer->pulse_time();
+  auto CurrentNumberOfEvents = EventMsgFlatbuffer->detector_id()->size();
+  EventTimeZero.appendElement(CurrentRefTime);
+  EventIndex.appendElement(EventsWritten);
+  EventsWritten += CurrentNumberOfEvents;
+  if (EventsWritten > LastEventIndex + EventIndexInterval) {
+    auto LastRefTimeOffset = EventMsgFlatbuffer->time_of_flight()->operator[](CurrentNumberOfEvents - 1);
+    CueTimestampZero.appendElement(CurrentRefTime + LastRefTimeOffset);
+    CueIndex.appendElement(EventsWritten - 1);
+    LastEventIndex = EventsWritten - 1;
   }
+
 
   if (RecordAdcPulseDebugData) {
     writeAdcPulseData(Message);
@@ -349,12 +308,6 @@ void HDFWriterModule::padDatasetsWithZeroesEqualToNumberOfEvents(
 }
 
 int32_t HDFWriterModule::close() {
-  ds_event_time_offset.reset();
-  ds_event_id.reset();
-  ds_event_time_zero.reset();
-  ds_event_index.reset();
-  ds_cue_index.reset();
-  ds_cue_timestamp_zero.reset();
   return 0;
 }
 
