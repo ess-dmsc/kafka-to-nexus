4d643c95b6de73ba79aaa94041a2a675c368021a
Matt Clarke <matt.clarke@esss.se>
2019-11-20 13:24:40 +0100

    CppCheck

--- src/schemas/ns10/NicosCacheWriter.cpp
+++ src/schemas/ns10/NicosCacheWriter.cpp
@@ -52,7 +52,6 @@ CacheWriter::init_hdf(hdf5::node::Group &HDFGroup,
   const int DefaultChunkSize = ChunkSize.at(0);
   try {
     auto &CurrentGroup = HDFGroup;
-    // cppcheck-suppress unusedScopedObject
     NeXusDataset::DoubleValue(      // NOLINT(bugprone-unused-raii)
         CurrentGroup,               // NOLINT(bugprone-unused-raii)
         NeXusDataset::Mode::Create, // NOLINT(bugprone-unused-raii)
