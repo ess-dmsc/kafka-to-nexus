95e2164211aa62b9bd88ade84e1fb9931defe690
Matthew D Jones <matthew.d.jones@tessella.com>
2020-01-30 08:51:11 +0000

    Remove redundant find modules

--- cmake/FindDate.cmake
+++
@@ -1,9 +0,0 @@
-find_path(DATE_INCLUDE_DIR NAMES date/date.h)
-include(FindPackageHandleStandardArgs)
-find_library(DATE_LIBRARY NAMES tz)
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(DATE DEFAULT_MSG
-		DATE_INCLUDE_DIR
-		DATE_LIBRARY
-)
