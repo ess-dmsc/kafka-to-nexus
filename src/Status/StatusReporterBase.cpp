4942b5b87327d74b87c1b23df9979cb77ddb8a28
Matt Clarke <matt.clarke@esss.se>
2020-01-30 11:37:19 +0100

    Forgot the files!

---
+++ src/Status/StatusReporterBase.cpp
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: BSD-2-Clause
+//
+// This code has been produced by the European Spallation Source
+// and its partner institutes under the BSD 2 Clause License.
+//
+// See LICENSE.md at the top level for license information.
+//
+// Screaming Udder!                              https://esss.se
+
+#include "StatusReporter.h"
+#include "json.h"
+
+namespace Status {
+
+void StatusReporterBase::updateStatusInfo(StatusInfo const &NewInfo) {
+  const std::lock_guard<std::mutex> lock(StatusMutex);
+  Status = NewInfo;
+}
+
+void StatusReporterBase::updateStopTime(std::chrono::milliseconds StopTime) {
+  const std::lock_guard<std::mutex> lock(StatusMutex);
+  Status.StopTime = StopTime;
+}
+
+void StatusReporterBase::resetStatusInfo() {
+  updateStatusInfo({"", "", std::chrono::milliseconds(0)});
+}
+
+std::string StatusReporterBase::createReport() const {
+  auto Info = nlohmann::json::object();
+  std::lock_guard<std::mutex> const lock(StatusMutex);
+
+  Info["update_interval"] = Period.count();
+  Info["job_id"] = Status.JobId;
+  Info["file_being_written"] = Status.Filename;
+  Info["start_time"] = Status.StartTime.count();
+  Info["stop_time"] = Status.StopTime.count();
+
+  return Info.dump();
+}
+
+void StatusReporterBase::reportStatus() {
+  if (!StatusProducerTopic) {
+    return;
+  }
+
+  auto const StatusReport = createReport();
+  Logger->debug("status: {}", StatusReport);
+  StatusProducerTopic->produce(StatusReport);
+  postReportStatusActions();
+}
+
+} // namespace Status
