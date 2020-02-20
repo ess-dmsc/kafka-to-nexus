2ee30aebb6e5a9388374c2d191a32c00ae9aa622
Matthew D Jones <matthew.d.jones@tessella.com>
2020-02-20 15:34:17 +0000

    Trying to get python to output valid flatbuffers

--- system-tests/helpers/flatbufferhelpers.py
+++ system-tests/helpers/flatbufferhelpers.py
@@ -40,66 +40,69 @@ def create_f142_message(timestamp_unix_ms=None, source_name: str = "fw-test-help
 
 def create_runstart_message(job_id: str, filename: str, start_time: Optional[int] = None,
                             stop_time: Optional[int] = None, run_name: str = "test_run", nexus_structure: str = "{}",
-                            service_id: str = "", instrument_name: str = "TEST", broker: str = "localhost:9092"):
-    file_identifier = b"pl72"
+                            service_id: str = "", instrument_name: str = "TEST", broker: str = "localhost:9092") -> bytes:
     builder = flatbuffers.Builder(1024)
 
     if start_time is None:
         start_time = _seconds_to_nanoseconds(time())
+    if service_id is None:
+        service_id = ""
+    if stop_time is None:
+        stop_time = 0
 
-    run_name_offset = builder.CreateString(run_name)
-    instrument_name_offset = builder.CreateString(instrument_name)
-    nexus_structure_offset = builder.CreateString(nexus_structure)
-    job_id_offset = builder.CreateString(job_id)
-    broker_offset = builder.CreateString(broker)
-    if service_id is not None:
-        service_id_offset = builder.CreateString(service_id)
     filename_offset = builder.CreateString(filename)
+    service_id_offset = builder.CreateString(service_id)
+    broker_offset = builder.CreateString(broker)
+    job_id_offset = builder.CreateString(job_id)
+    nexus_structure_offset = builder.CreateString(nexus_structure)
+    instrument_name_offset = builder.CreateString(instrument_name)
+    run_name_offset = builder.CreateString(run_name)
 
     # Build the actual buffer
     RunStart.RunStartStart(builder)
+    # RunStart.RunStartAddDetectorSpectrumMap(builder, None)
+    RunStart.RunStartAddNPeriods(builder, 1)
     RunStart.RunStartAddFilename(builder, filename_offset)
-    if service_id is not None:
-        RunStart.RunStartAddServiceId(builder, service_id_offset)
+    RunStart.RunStartAddServiceId(builder, service_id_offset)
     RunStart.RunStartAddBroker(builder, broker_offset)
     RunStart.RunStartAddJobId(builder, job_id_offset)
     RunStart.RunStartAddNexusStructure(builder, nexus_structure_offset)
     RunStart.RunStartAddInstrumentName(builder, instrument_name_offset)
     RunStart.RunStartAddRunName(builder, run_name_offset)
-    if stop_time is not None:
-        RunStart.RunStartAddStopTime(builder, stop_time)
+    RunStart.RunStartAddStopTime(builder, stop_time)
     RunStart.RunStartAddStartTime(builder, start_time)
     run_start_message = RunStart.RunStartEnd(builder)
     builder.Finish(run_start_message)
 
     # Generate the output and replace the file_identifier
     buff = builder.Output()
-    buff[4:8] = file_identifier
+    buff[4:8] = b"pl72"
     return bytes(buff)
 
 
 def create_runstop_message(job_id: str, run_name: str = "test_run", service_id: str = "",
-                           stop_time: Optional[int] = None):
-    file_identifier = b"6s4t"
+                           stop_time: Optional[int] = None) -> bytes:
     builder = flatbuffers.Builder(1024)
 
-    run_name_offset = builder.CreateString(run_name)
+    if service_id is None:
+        service_id = ""
+    if stop_time is None:
+        stop_time = 0
+
+    service_id_offset = builder.CreateString(service_id)
     job_id_offset = builder.CreateString(job_id)
-    if service_id is not None:
-        service_id_offset = builder.CreateString(service_id)
+    run_name_offset = builder.CreateString(run_name)
 
     # Build the actual buffer
     RunStop.RunStopStart(builder)
-    if service_id is not None:
-        RunStop.RunStopAddServiceId(builder, service_id_offset)
+    RunStop.RunStopAddServiceId(builder, service_id_offset)
     RunStop.RunStopAddJobId(builder, job_id_offset)
     RunStop.RunStopAddRunName(builder, run_name_offset)
-    if stop_time is not None:
-        RunStop.RunStopAddStopTime(builder, stop_time)
+    RunStop.RunStopAddStopTime(builder, stop_time)
     run_stop_message = RunStop.RunStopEnd(builder)
     builder.Finish(run_stop_message)
 
     # Generate the output and replace the file_identifier
     buff = builder.Output()
-    buff[4:8] = file_identifier
+    buff[4:8] = b"6s4t"
     return bytes(buff)
