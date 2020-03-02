7bf004985c1f6df23f6ae557b9d76e7ed58717d5
Matt Clarke <matt.clarke@esss.se>
2020-03-02 10:34:29 +0100

    Check the job id before stopping.
    Also added system test.

--- system-tests/test_filewriter_ignores_commands.py
+++ system-tests/test_filewriter_ignores_commands.py
@@ -32,13 +32,13 @@ def test_ignores_commands_with_incorrect_service_id(docker_compose_multiple_inst
     consumer = create_consumer()
     consumer.subscribe(["TEST_writerStatus2"])
 
-    # poll a few times on the status topic to see if the filewriter2 has stopped writing files.
+    # Poll a few times on the status topic to see if the filewriter2 has stopped writing.
     stopped = False
 
     for i in range(30):
         msg = consumer.poll()
         if b'"file_being_written":""' in msg.value():
-            # filewriter2 is not currently writing a file - stop command has been processed.
+            # filewriter2 is not currently writing a file => stop command has been processed.
             stopped = True
             break
         sleep(1)
@@ -52,3 +52,38 @@ def test_ignores_commands_with_incorrect_service_id(docker_compose_multiple_inst
 
     # Check filewriter1's job queue is not empty
     assert b'"file_being_written":""' not in writer1msg.value()
+
+
+def test_ignores_commands_with_incorrect_job_id(docker_compose_multiple_instances):
+    producer = create_producer()
+    sleep(20)
+    service_id_1 = "filewriter1"
+    job_id = publish_run_start_message(
+        producer,
+        "commands/nexus_structure.json",
+        nexus_filename="output_file_ignores_stop_1.nxs",
+    )
+
+    sleep(10)
+
+    # Request stop but with slightly wrong job_id
+    publish_run_stop_message(producer, job_id[:-1], service_id=service_id_1)
+
+    consumer = create_consumer()
+    consumer.subscribe(["TEST_writerStatus1"])
+
+    # Poll a few times on the status topic to check the filewriter has not stopped writing.
+    stopped = False
+
+    for i in range(30):
+        msg = consumer.poll()
+        if b'"file_being_written":""' in msg.value():
+            # Filewriter is not currently writing a file => stop command has been processed incorrectly
+            stopped = True
+            break
+        sleep(1)
+
+    sleep(5)
+    consumer.unsubscribe()
+
+    assert not stopped
