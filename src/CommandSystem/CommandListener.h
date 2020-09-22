211008e1d478805a75a0281d20dc016194c97004
Jonas Nilsson <mjc.nilsson@gmail.com>
2020-09-22 13:29:12 +0200

    Add command listener.

---
+++ src/CommandSystem/CommandListener.h
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: BSD-2-Clause
+//
+// This code has been produced by the European Spallation Source
+// and its partner institutes under the BSD 2 Clause License.
+//
+// See LICENSE.md at the top level for license information.
+//
+// Screaming Udder!                              https://esss.se
+
+#pragma once
+
+#include "Kafka/Consumer.h"
+#include "MainOpt.h"
+#include "Msg.h"
+#include "logger.h"
+
+namespace Command {
+
+using FileWriter::Msg;
+
+/// Check for new commands on the topic, return them to the Master.
+class CommandListener {
+public:
+  CommandListener(uri::URI CommandTopicUri, Kafka::BrokerSettings Settings);
+
+  std::pair<Kafka::PollStatus, Msg> pollForCommand();
+
+protected:
+  std::string const KafkaAddress;
+  std::string const CommandTopic;
+  Kafka::BrokerSettings KafkaSettings;
+  void setUpConsumer();
+  std::unique_ptr<Kafka::ConsumerInterface> Consumer;
+  duration CurrentTimeOut;
+};
+} // namespace Command
