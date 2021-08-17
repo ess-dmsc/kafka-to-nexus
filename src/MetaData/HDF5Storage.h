1f2889087f9b0a3e1cf598b09fff83ba38fb0ae3
Jonas Nilsson <mjc.nilsson@gmail.com>
2021-08-17 11:23:32 +0200

    Added license header.

--- src/MetaData/HDF5Storage.h
+++ src/MetaData/HDF5Storage.h
@@ -1,3 +1,13 @@
+// SPDX-License-Identifier: BSD-2-Clause
+//
+// This code has been produced by the European Spallation Source
+// and its partner institutes under the BSD 2 Clause License.
+//
+// See LICENSE.md at the top level for license information.
+//
+// Screaming Udder!                              https://esss.se
+
+
 #pragma once
 
 #include <h5cpp/hdf5.hpp>
--- src/MetaData/Value.cpp
+++ src/MetaData/Value.cpp
@@ -1,6 +1,12 @@
+// SPDX-License-Identifier: BSD-2-Clause
 //
-// Created by Jonas Nilsson on 2021-07-06.
+// This code has been produced by the European Spallation Source
+// and its partner institutes under the BSD 2 Clause License.
 //
+// See LICENSE.md at the top level for license information.
+//
+// Screaming Udder!                              https://esss.se
+
 
 #include "Value.h"
 
