29de81da818512286c1546302f13537a89d0cffc
Jonas Nilsson <mjc.nilsson@gmail.com>
2021-09-15 00:54:41 +0200

    Fix use after return issue.

--- src/logger.h
+++ src/logger.h
@@ -77,26 +77,26 @@ void setUpLogging(const Log::Severity &LoggingLevel,
 
 template <typename... Args>
 void LOG_ERROR(std::string fmt, const Args &... args) {
-  Log::Msg(Log::Severity::Error, fmt::format(fmt, args...));
+  Log::FmtMsg(Log::Severity::Error, fmt, args...);
 }
 
 template <typename... Args>
 void LOG_WARN(std::string fmt, const Args &... args) {
-  Log::Msg(Log::Severity::Warning, fmt::format(fmt, args...));
+  Log::FmtMsg(Log::Severity::Warning, fmt, args...);
 }
 
 template <typename... Args>
 void LOG_INFO(std::string fmt, const Args &... args) {
-  Log::Msg(Log::Severity::Info, fmt::format(fmt, args...));
+  Log::FmtMsg(Log::Severity::Info, fmt, args...);
 }
 
 template <typename... Args>
 void LOG_DEBUG(std::string fmt, const Args &... args) {
-  Log::Msg(Log::Severity::Debug, fmt::format(fmt, args...));
+  Log::FmtMsg(Log::Severity::Debug, fmt, args...);
 }
 
 template <typename... Args>
 void LOG_TRACE(std::string fmt, const Args &... args) {
-  Log::Msg(Log::Severity::Debug, fmt::format(fmt, args...));
+  Log::FmtMsg(Log::Severity::Debug, fmt, args...);
 // Fix this, what do we do about log levels?
 }
