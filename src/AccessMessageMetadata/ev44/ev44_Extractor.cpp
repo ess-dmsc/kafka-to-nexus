6d90aff19c20a91c4a860141ac8a3d045259a946
Kenan Muric <kenan.muric@ess.eu>
2022-12-21 00:09:21 +0100

    adding ev44 extractor, ev44 writer and unit tests

---
+++ src/AccessMessageMetadata/ev44/ev44_Extractor.cpp
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: BSD-2-Clause
+//
+// This code has been produced by the European Spallation Source
+// and its partner institutes under the BSD 2 Clause License.
+//
+// See LICENSE.md at the top level for license information.
+//
+// Screaming Udder!                              https://esss.se
+
+#include "ev44_Extractor.h"
+#include <ev44_events_generated.h>
+
+namespace AccessMessageMetadata {
+
+bool ev44_Extractor::verify(FlatbufferMessage const &Message) const {
+  flatbuffers::Verifier VerifierInstance(
+      reinterpret_cast<const uint8_t *>(Message.data()), Message.size());
+  return VerifyEvent44MessageBuffer(VerifierInstance);
+}
+
+std::string
+ev44_Extractor::source_name(FlatbufferMessage const &Message) const {
+  auto fbuf = GetEvent44Message(Message.data());
+  auto NamePtr = fbuf->source_name();
+  if (NamePtr == nullptr) {
+    LOG_INFO("message has no source_name");
+    return "";
+  }
+  return NamePtr->str();
+}
+
+uint64_t
+ev44_Extractor::timestamp(FlatbufferMessage const &Message) const {
+  auto fbuf = GetEvent44Message(Message.data());
+  std::vector<int64_t> reference_time;
+  for (auto ref_time : *fbuf->reference_time()) {
+    reference_time.push_back(ref_time);
+  }
+  return reference_time[0];
+}
+
+static FileWriter::FlatbufferReaderRegistry::Registrar<ev44_Extractor>
+    RegisterReader("ev44");
+
+} // namespace AccessMessageMetadata
---
+++ src/WriterModule/ev44/ev44_Writer.h
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: BSD-2-Clause
+//
+// This code has been produced by the European Spallation Source
+// and its partner institutes under the BSD 2 Clause License.
+//
+// See LICENSE.md at the top level for license information.
+//
+// Screaming Udder!                              https://esss.se
+
+#include "FlatbufferMessage.h"
+#include "MetaData/Value.h"
+#include "NeXusDataset/NeXusDataset.h"
+#include "WriterModuleBase.h"
+
+namespace WriterModule {
+namespace ev44 {
+
+using FlatbufferMessage = FileWriter::FlatbufferMessage;
+/// \brief Write module for the ev44 flatbuffer schema.
+class ev44_Writer : public WriterModule::Base {
+public:
+  ev44_Writer()
+      : WriterModule::Base(true, "NXevent_data"),
+        EventsWrittenMetadataField("", "events") {}
+  InitResult init_hdf(hdf5::node::Group &HDFGroup) const override;
+  WriterModule::InitResult reopen(hdf5::node::Group &HDFGroup) override;
+
+  /// \brief Write flatbuffer message.
+  ///
+  /// \param FlatBufferMessage
+  void write(FlatbufferMessage const &Message) override;
+
+  NeXusDataset::EventTimeOffset EventTimeOffset;
+  NeXusDataset::EventId EventId;
+  NeXusDataset::EventTimeZero EventTimeZero;
+  NeXusDataset::EventIndex EventIndex;
+  NeXusDataset::CueIndex CueIndex;
+  NeXusDataset::CueTimestampZero CueTimestampZero;
+
+  void register_meta_data(hdf5::node::Group const &HDFGroup,
+                          MetaData::TrackerPtr const &Tracker) override;
+
+private:
+  void
+  padDatasetsWithZeroesEqualToNumberOfEvents(FlatbufferMessage const &Message);
+
+  JsonConfig::Field<uint64_t> EventIndexInterval{
+      this, "cue_interval", std::numeric_limits<uint64_t>::max()};
+  JsonConfig::Field<uint64_t> ChunkSize{this, "chunk_size", 1 << 20};
+  uint64_t EventsWritten{0};
+  uint64_t LastEventIndex{0};
+  MetaData::Value<uint64_t> EventsWrittenMetadataField;
+};
+} // namespace ev44
+} // namespace WriterModule
