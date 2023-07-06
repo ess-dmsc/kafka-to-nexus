793e654364514c7849be78d7d5b269dce3236ec7
George O'Neill <george.oneill@ess.eu>
2023-07-06 13:24:04 +0100

    initial work

---
+++ src/WriterModule/mdat/mdat_Writer.cpp
@@ -0,0 +1,51 @@
+#include "ep00_Writer.h"
+#include "FlatbufferMessage.h"
+#include "WriterRegistrar.h"
+#include <ep00_epics_connection_info_generated.h>
+
+namespace WriterModule {
+namespace ep00 {
+
+InitResult ep00_Writer::reopen(hdf5::node::Group &HDFGroup) {
+  auto Open = NeXusDataset::Mode::Open;
+  try {
+    TimestampDataset = NeXusDataset::ConnectionStatusTime(HDFGroup, Open);
+    StatusDataset = NeXusDataset::ConnectionStatus(HDFGroup, Open);
+  } catch (std::exception &E) {
+    LOG_ERROR(
+        R"(Failed to reopen datasets in HDF file with error message: "{}")",
+        std::string(E.what()));
+    return InitResult::ERROR;
+  }
+  return InitResult::OK;
+}
+
+InitResult ep00_Writer::init_hdf(hdf5::node::Group &HDFGroup) const {
+  auto Create = NeXusDataset::Mode::Create;
+  try {
+    NeXusDataset::ConnectionStatusTime(
+        HDFGroup, Create, ChunkSize); // NOLINT(bugprone-unused-raii)
+    NeXusDataset::ConnectionStatus(HDFGroup, Create,
+                                   ChunkSize); // NOLINT(bugprone-unused-raii)
+  } catch (std::exception const &E) {
+    auto message = hdf5::error::print_nested(E);
+    LOG_ERROR("ep00 could not init_hdf HDFGroup: {}  trace: {}",
+              static_cast<std::string>(HDFGroup.link().path()), message);
+    return InitResult::ERROR;
+  }
+  return InitResult::OK;
+}
+
+void ep00_Writer::write(FileWriter::FlatbufferMessage const &Message) {
+  auto FlatBuffer = GetEpicsConnectionInfo(Message.data());
+  std::string const Status = EnumNameEventType(FlatBuffer->type());
+  StatusDataset.appendStringElement(Status);
+  auto FBTimestamp = FlatBuffer->timestamp();
+  TimestampDataset.appendElement(FBTimestamp);
+}
+
+static WriterModule::Registry::Registrar<ep00_Writer>
+    RegisterWriter("ep00", "epics_con_status");
+
+} // namespace ep00
+} // namespace WriterModule
