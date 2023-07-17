f6619eec1b5e46b6742cf99e6cccf2f7c4819227
Afonso Mukai <28659574+amues@users.noreply.github.com>
2023-07-17 13:13:58 +0200

    Update BrokerSettings.h (#700)
    * Update BrokerSettings.h
    
    We are using 6 times the default, not the default for fetch.max.bytes.
    
    * GO FORMAT YOURSELF (clang-format)
    
    ---------
    
    Co-authored-by: cow-bot <dm-jenkins-integration@esss.se>

--- src/Kafka/BrokerSettings.h
+++ src/Kafka/BrokerSettings.h
@@ -36,9 +36,7 @@ struct BrokerSettings {
   std::map<std::string, std::string> KafkaConfiguration = {
       {"socket.timeout.ms", "10000"},
       {"message.max.bytes", std::to_string(message_max_bytes)},
-      {"fetch.max.bytes",
-       std::to_string(fetch_max_bytes)}, // this is the default value, here as
-                                         // documentation
+      {"fetch.max.bytes", std::to_string(fetch_max_bytes)},
       {"receive.message.max.bytes",
        std::to_string(
            receive_max_bytes)}, // must be at least fetch.max.bytes + 512
