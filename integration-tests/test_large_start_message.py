766692e62f9023693372edcf6cabe3661e3954dd
George O'Neill <george.oneill@ess.eu>
2023-08-31 08:57:03 +0200

    adjusting timeout for test_large_start_message

--- integration-tests/test_large_start_message.py
+++ integration-tests/test_large_start_message.py
@@ -15,7 +15,7 @@ from helpers.writer import (
 )
 def test_large_start_message(worker_pool, kafka_address, json_padding, file_nr):
     file_path = build_relative_file_path(f"output_file_large_msg_{file_nr}.nxs")
-    wait_writers_available(worker_pool, nr_of=1, timeout=10)
+    wait_writers_available(worker_pool, nr_of=1, timeout=30)
     now = datetime.now()
     start_time = now - timedelta(seconds=10)
     stop_time = now
@@ -29,9 +29,9 @@ def test_large_start_message(worker_pool, kafka_address, json_padding, file_nr):
         start_time=start_time,
         stop_time=stop_time,
     )
-    wait_start_job(worker_pool, write_job, timeout=90)
+    wait_start_job(worker_pool, write_job, timeout=150)
 
-    wait_no_working_writers(worker_pool, timeout=60)
+    wait_no_working_writers(worker_pool, timeout=90)
 
     with OpenNexusFile(file_path) as file:
         assert not file.swmr_mode
