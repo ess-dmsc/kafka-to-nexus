d9e3baa3f421561037cbf81046e20a9313a3d8f6
George O'Neill <george.oneill@ess.eu>
2023-09-21 11:58:22 +0200

    removal of unused methods in mdat_Extractor

--- src/AccessMessageMetadata/mdat/mdat_Extractor.h
+++ src/AccessMessageMetadata/mdat/mdat_Extractor.h
@@ -35,8 +35,6 @@
 #pragma once
 #include "FlatbufferReader.h"
 #include "flatbuffers/flatbuffers.h"
-#include <pl72_run_start_generated.h>
-//#include "WriterModuleBase.h"
 #include <iostream>
 
 /// \brief A separate namespace for this specific file writing module. Use this
@@ -81,62 +79,8 @@ public:
   ///
   /// \return `true` if the data was verified as "correct", `false` otherwise.
   // cppcheck-suppress functionStatic
-  bool verify(FileWriter::FlatbufferMessage const &/*Message*/) const override {
-    std::cout << "ReaderClass::verify()\n";
-    return true;
-  }
+  bool verify(FileWriter::FlatbufferMessage const &/*Message*/) const override { return true; }
 
-  /// \brief Extract the name of the data source from the flatbuffer.
-  ///
-  /// When setting up the file writer, a point (group) is tied to a specific
-  /// module instance using the four character identifier of a file writing
-  /// module and the name of the source of the data. This function is used to
-  /// extract the name of a source given the relevant flatbuffer. Although the
-  /// name can be as simple as a string contained in a flatbuffer, it might be
-  /// appropriate in some cases to construct the name from (e.g.) a string and a
-  /// channel number.
-  ///
-  /// \note In the current implementation, this member function might be called
-  /// several times on the same flatbuffer.
-  /// \note Try to avoid throwing any exceptions here as it (appears) to likely
-  /// either crash the application or leave it in an inconsistent state.
-  ///
-  /// \param Message The structure containing a pointer to a buffer
-  /// containing data received from the Kafka broker and the size of the
-  /// buffer.
-  ///
-  /// \return The name of the source of the data in the flatbuffer pointed to by
-  /// the Message parameter.
-  // cppcheck-suppress functionStatic
-  std::string source_name(FileWriter::FlatbufferMessage const &Message) const override {
-    return GetRunStart(Message.data())->nexus_structure()->c_str();
-  }
-
-  /// \brief Extract the timestamp of a flatbuffer.
-  ///
-  /// The file writer can be set to write data only in a specific "time window".
-  /// Thus function is in that case used to extract the time of a flatbuffer and
-  /// thus decide if its data should be written to file or not. Note that there
-  /// are currently no specification or documentation as to which epoch is to
-  /// be used nor unit of time and prefix. With that said, the current
-  /// implementations appears to try to use (when possible) Unix epoch and
-  /// nanoseconds.
-  ///
-  /// \note Try to avoid throwing any exceptions here as it (appears) to likely
-  /// either crash the application or leave it in an inconsistent state.
-  ///
-  /// \param Message The structure containing a pointer to a buffer
-  /// containing data received from the Kafka broker and the size of the buffer.
-  ///
-  /// \return The timestamp of the flatbuffer as nanoseconds since Unix epoch
-  /// (see above).
-  // cppcheck-suppress functionStatic
-  uint64_t timestamp(FileWriter::FlatbufferMessage const &Message) const override {
-    uint64_t returnTime = GetRunStart(Message.data())->start_time();
-    if( !returnTime )
-      returnTime = GetRunStart(Message.data())->stop_time();
-    return returnTime;
-  }
 };
 
 } // namespace mdatWriter
