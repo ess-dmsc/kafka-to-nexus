c74f670b51d1d8b58f3f26960d048375caf980fa
Afonso Mukai <afonso.mukai@ess.eu>
2023-10-05 16:07:29 +0200

    Reformat check-formatting.sh

--- jenkins-scripts/check-formatting.sh
+++ jenkins-scripts/check-formatting.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Run from the project's root directory to check for formatting issues with
+# clang-format.
+
 clang-format --version
 
-find . \(  -name '*.cpp' -or -name '*.cxx' -or -name '*.h' -or -name '*.hpp' \) -exec clang-format --dry-run -Werror {} +
+find . \( \
+    -name '*.cpp' \
+    -or \
+    -name '*.cxx' \
+    -or \
+    -name '*.h' \
+    -or \
+    -name '*.hpp' \
+  \) -exec clang-format --dry-run -Werror {} +
