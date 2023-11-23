f351e7287cd1ffe8a6521757c0e2d726c43af30c
Afonso Mukai <afonso.mukai@ess.eu>
2023-11-23 16:31:19 +0100

    Store int test result and use as exit code

--- integration-tests/run-integration-tests.sh
+++ integration-tests/run-integration-tests.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-set -e
-
 docker exec filewriter bash -c 'cd integration-tests; scl enable rh-python38 -- ~/venv/bin/pytest --writer-binary=../kafka-to-nexus --junitxml=IntegrationTestsOutput.xml'
+
+result=$?
+
+set -e
 
 docker cp filewriter:/home/jenkins/integration-tests/IntegrationTestsOutput.xml .
 docker cp filewriter:/home/jenkins/integration-tests/logs/. logs
+
+exit $result
