c1a436ff0d8ae26ec9d3f1c378f7f57c3332e760
Afonso Mukai <afonso.mukai@ess.eu>
2023-11-23 16:21:19 +0100

    Update docker cp

--- integration-tests/run-integration-tests.sh
+++ integration-tests/run-integration-tests.sh
@@ -5,4 +5,4 @@ set -e
 docker exec filewriter bash -c 'cd integration-tests; scl enable rh-python38 -- ~/venv/bin/pytest --writer-binary=../kafka-to-nexus --junitxml=IntegrationTestsOutput.xml'
 
 docker cp filewriter:/home/jenkins/integration-tests/IntegrationTestsOutput.xml .
-docker cp filewriter:/home/jenkins/integration-tests/logs/* logs/
+docker cp filewriter:/home/jenkins/integration-tests/logs/. logs
