2e2437569f2e1b402d2341aa4e1cb01bee9f99fb
Afonso Mukai <afonso.mukai@ess.eu>
2023-11-23 16:09:29 +0100

    Fix path to venv

--- integration-tests/run-integration-tests.sh
+++ integration-tests/run-integration-tests.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-docker exec filewriter bash -c 'cd integration-tests; scl enable rh-python38 -- venv/bin/pytest --writer-binary=../kafka-to-nexus --junitxml=IntegrationTestsOutput.xml'
+docker exec filewriter bash -c 'cd integration-tests; scl enable rh-python38 -- ~/venv/bin/pytest --writer-binary=../kafka-to-nexus --junitxml=IntegrationTestsOutput.xml'
 
 docker cp filewriter:/home/jenkins/integration-tests/IntegrationTestsOutput.xml .
 docker cp filewriter:/home/jenkins/integration-tests/logs/* logs/
