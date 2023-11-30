5896802c5e89689dd848a84173dbc5d539246405
Daniel Cacabelos <daniel.cacabelos@ess.eu>
2023-11-30 16:40:48 +0100

    WIP: Tests: run ep01 only

--- integration-tests/run-integration-tests.sh
+++ integration-tests/run-integration-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker exec filewriter bash -c 'cd integration-tests; scl enable rh-python38 -- ~/venv/bin/pytest --writer-binary=../kafka-to-nexus --junitxml=IntegrationTestsOutput.xml'
+docker exec filewriter bash -c 'cd integration-tests; scl enable rh-python38 -- ~/venv/bin/pytest --writer-binary=../kafka-to-nexus --junitxml=IntegrationTestsOutput.xml -k ep0'
 
 result=$?
 
@@ -8,5 +8,6 @@ set -e
 
 docker cp filewriter:/home/jenkins/integration-tests/IntegrationTestsOutput.xml .
 docker cp filewriter:/home/jenkins/integration-tests/logs/. logs
+docker cp filewriter:/home/jenkins/integration-tests/output-files/. output-files
 
 exit $result
