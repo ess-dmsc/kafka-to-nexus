89550d56d0b80a204ce77912fbee6a5835f0bcb3
Daniel Cacabelos <daniel.cacabelos@ess.eu>
2024-02-08 14:52:53 +0000

    Tests: Consolidate JobCreatorTests into one file

---
+++ src/tests/CommandSystem/CommandHandlerTests.cpp
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: BSD-2-Clause
+//
+// This code has been produced by the European Spallation Source
+// and its partner institutes under the BSD 2 Clause License.
+//
+// See LICENSE.md at the top level for license information.
+//
+// Screaming Udder!                              https://esss.se
+
+#include "JobCreator.h"
+#include <gtest/gtest.h>
--- src/tests/JobCreatorTests.cpp
+++ src/tests/JobCreatorTests.cpp
@@ -149,3 +149,82 @@ TEST(ExtractMdat, ExtractsAllMdatModulesFromModuleList) {
     ASSERT_NE(Module.WriterModule, "mdat");
   });
 }
+
+TEST(ExtractStreamSettings, IfSourceNotDefinedThenThrows) {
+  std::string Command{R"""({
+        "dtype": "double",
+        "topic": "my_test_topic"
+  })"""};
+
+  ModuleHDFInfo Info;
+  Info.WriterModule = "f142";
+  Info.ConfigStream = Command;
+
+  ASSERT_THROW(FileWriter::extractModuleInformationFromJsonForSource(Info),
+               std::runtime_error);
+}
+
+TEST(ExtractStreamSettings, IfTopicNotDefinedThenThrows) {
+  std::string Command{R"""({
+        "dtype": "double",
+        "source": "my_test_pv"
+  })"""};
+
+  ModuleHDFInfo Info;
+  Info.WriterModule = "f142";
+  Info.ConfigStream = Command;
+
+  ASSERT_THROW(FileWriter::extractModuleInformationFromJsonForSource(Info),
+               std::runtime_error);
+}
+
+TEST(ExtractStreamSettings, IfWriterModuleNotDefinedThenThrows) {
+  std::string Command{R"""({
+        "dtype": "double",
+        "source": "my_test_pv",
+        "topic": "my_test_topic"
+  })"""};
+
+  ModuleHDFInfo Info;
+  Info.ConfigStream = Command;
+
+  ASSERT_THROW(FileWriter::extractModuleInformationFromJsonForSource(Info),
+               std::runtime_error);
+}
+
+TEST(ExtractStreamSettings, IfValidThenBasicStreamSettingsExtracted) {
+  std::string Command{R"""({
+        "dtype": "double",
+        "source": "my_test_pv",
+        "topic": "my_test_topic"
+  })"""};
+
+  ModuleHDFInfo Info;
+  Info.WriterModule = "f142";
+  Info.ConfigStream = Command;
+
+  auto Settings = FileWriter::extractModuleInformationFromJsonForSource(Info);
+
+  ASSERT_EQ("my_test_topic", Settings.Topic);
+  ASSERT_EQ("f142", Settings.Module);
+  ASSERT_EQ("my_test_pv", Settings.Source);
+}
+
+TEST(ExtractStreamSettings, NoAttributesExtracted) {
+  std::string Command{R"""({
+        "dtype": "double",
+        "source": "my_test_pv",
+        "topic": "my_test_topic",
+    "attributes": {
+          "NX_class": "NXlog"
+    }
+  })"""};
+
+  ModuleHDFInfo Info;
+  Info.WriterModule = "f142";
+  Info.ConfigStream = Command;
+
+  auto Settings = FileWriter::extractModuleInformationFromJsonForSource(Info);
+
+  ASSERT_EQ("", Settings.Attributes);
+}
