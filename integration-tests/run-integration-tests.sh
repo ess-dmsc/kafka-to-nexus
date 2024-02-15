167e4b0e78bd71e414b23f3c6357cc1fd627f08e
Afonso Mukai <afonso.mukai@ess.eu>
2024-02-15 15:31:24 +0100

    Debug integration test

--- integration-tests/run-integration-tests.sh
+++ integration-tests/run-integration-tests.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -x
+
 docker exec ${FILEWRITER_FILEWRITER_CONTAINER_NAME:-filewriter} bash -c "cd integration-tests; scl enable rh-python38 -- ~/venv/bin/pytest --writer-binary=../kafka-to-nexus --kafka-broker=${FILEWRITER_KAFKA_CONTAINER_NAME:-kafka}:9093 --junitxml=IntegrationTestsOutput.xml"
 
 result=$?
