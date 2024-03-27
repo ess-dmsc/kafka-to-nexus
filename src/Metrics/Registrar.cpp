61361ad04dfa04f2cbce7736a7cfa99d5a07684e
Matt Clarke <matt.clarke@ess.eu>
2024-03-27 09:51:43 +0100

    use a vector for the reporter list

--- src/Metrics/Registrar.cpp
+++ src/Metrics/Registrar.cpp
@@ -1,7 +1,6 @@
 #include "Registrar.h"
 #include "Metric.h"
 #include <algorithm>
-#include <logger.h>
 
 namespace Metrics {
 
@@ -10,9 +9,9 @@ void Registrar::registerMetric(Metric &NewMetric,
   if (NewMetric.getName().empty()) {
     throw std::runtime_error("Metrics cannot be registered with an empty name");
   }
-  for (auto const &[log_to, reporter] : ReporterList) {
+  for (auto const &reporter : ReporterList) {
     if (std::find(SinkTypes.begin(), SinkTypes.end(),
-                  log_to) != SinkTypes.end()) {
+                  reporter->getSinkType()) != SinkTypes.end()) {
       std::string NewName = prependPrefix(NewMetric.getName());
 
       if (!reporter->addMetric(NewMetric, NewName)) {
@@ -26,14 +25,8 @@ void Registrar::registerMetric(Metric &NewMetric,
 
 std::unique_ptr<IRegistrar>
 Registrar::getNewRegistrar(std::string const &MetricsPrefix) const {
-  std::vector<std::shared_ptr<Reporter>> Reporters;
-  Reporters.reserve(ReporterList.size());
-
-  for (auto const &SinkTypeAndReporter : ReporterList) {
-    // cppcheck-suppress useStlAlgorithm
-    Reporters.push_back(SinkTypeAndReporter.second);
-  }
-  return std::make_unique<Registrar>(prependPrefix(MetricsPrefix), Reporters);
+  return std::make_unique<Registrar>(prependPrefix(MetricsPrefix),
+                                     ReporterList);
 }
 
 std::string Registrar::prependPrefix(std::string const &Name) const {
--- src/Metrics/Registrar.h
+++ src/Metrics/Registrar.h
@@ -15,7 +15,7 @@ class IRegistrar {
 public:
   virtual void registerMetric(Metric &NewMetric,
                               std::vector<LogTo> const &SinkTypes) const = 0;
-  virtual std::unique_ptr<IRegistrar>
+  [[nodiscard]] virtual std::unique_ptr<IRegistrar>
   getNewRegistrar(std::string const &MetricsPrefix) const = 0;
   virtual ~IRegistrar() = default;
 };
@@ -25,13 +25,10 @@ public:
 /// Deregistration of metric happens via Metric's destructor
 class Registrar : public IRegistrar {
 public:
-  Registrar(std::string MetricsPrefix,
-            std::vector<std::shared_ptr<Reporter>> Reporters = {})
-      : Prefix(std::move(MetricsPrefix)) {
-    for (auto const &NewReporter : Reporters) {
-      ReporterList.emplace(NewReporter->getSinkType(), NewReporter);
-    }
-  };
+  explicit Registrar(
+      std::string MetricsPrefix,
+      std::vector<std::shared_ptr<Reporter>> const &Reporters = {})
+      : Prefix(std::move(MetricsPrefix)), ReporterList(Reporters){};
   ~Registrar() override = default;
 
   void registerMetric(Metric &NewMetric,
@@ -44,7 +41,7 @@ private:
   [[nodiscard]] std::string prependPrefix(std::string const &Name) const;
   std::string Prefix;
   /// List of reporters we might want to add a metric to
-  std::map<LogTo, std::shared_ptr<Reporter>> ReporterList;
+  std::vector<std::shared_ptr<Reporter>> ReporterList;
 };
 
 } // namespace Metrics
