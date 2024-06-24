1c61a14cc3e71af49698e8e8e61a3555ce05f7c9
Matt Clarke <matt.clarke@ess.eu>
2024-06-24 10:11:52 +0200

    update file-maker

--- apps/file-maker.cpp
+++ apps/file-maker.cpp
@@ -3,8 +3,8 @@
 #include "MetaData/Tracker.h"
 #include "Metrics/Metric.h"
 #include "logger.h"
-#include <da00_dataarray_generated.h>
 #include <ep01_epics_connection_generated.h>
+#include <ev44_events_generated.h>
 #include <f144_logdata_generated.h>
 #include <iostream>
 #include <memory>
@@ -12,168 +12,93 @@
 
 using std::chrono_literals::operator""ms;
 
-std::string const example_json = R"({
-"children": [
-  {
-    "name": "entry",
-    "type": "group",
-    "attributes": [
-      {
-        "name": "NX_class",
-        "dtype": "string",
-        "values": "NXentry"
-      }
-    ],
-    "children": [
-      {
-        "module": "dataset",
-        "config": {
-          "name": "title",
-          "values": "This is a title",
-          "type": "string"
-        }
-      },
-      {
-        "module": "mdat",
-        "config": {
-          "name": "start_time"
-        }
-      },
-      {
-        "module": "mdat",
-        "config": {
-          "name": "end_time"
-        }
-      },
-      {
-        "name": "instrument",
-        "type": "group",
-        "attributes": [
-          {
-            "name": "NX_class",
-            "dtype": "string",
-            "values": "NXinstrument"
-          }
-        ],
-        "children": [
-          {
-            "name": "mini_chopper",
-            "type": "group",
-            "attributes": [
-              {
-                "name": "NX_class",
-                "dtype": "string",
-                "values": "NXdisk_chopper"
-              }
-            ],
-            "children": [
-              {
-                "name": "delay",
-                "type": "group",
-                "attributes": [
-                  {
-                    "name": "NX_class",
-                    "dtype": "string",
-                    "values": "NXlog"
-                  }
-                ],
-                "children": [
-                  {
-                    "module": "f144",
-                    "config": {
-                      "source": "delay:source:chopper",
-                      "topic": "local_choppers",
-                      "dtype": "double",
-                      "value_units": "ns"
-                    }
-                  }
-                ]
-              },
-              {
-                "name": "speed",
-                "type": "group",
-                "attributes": [
-                  {
-                    "name": "NX_class",
-                    "dtype": "string",
-                    "values": "NXlog"
-                  }
-                ],
-                "children": [
-                  {
-                    "module": "da00",
-                    "config": {
-                      "source": "fake:da00:source",
-                      "topic": "local_motion",
-                      "attributes": [
-                        {
-                          "name": "test_attribute",
-                          "data": "test_value"
-                        }
-                      ],
-                      "variables": [
-                        {
-                          "name": "value",
-                          "unit": "counts",
-                          "label": "Integrated counts on strip detector",
-                          "data_type": "int32",
-                          "axes": [
-                            "x"
-                          ],
-                          "shape": [
-                            5
-                          ]
-                        }
-                      ],
-                      "constants": [
-                        {
-                          "name": "x",
-                          "unit": "cm",
-                          "label": "Binned position along x-axis",
-                          "data_type": "float32",
-                          "axes": [
-                            "x"
-                          ],
-                          "shape": [
-                            4
-                          ],
-                          "data": [
-                            10,
-                            20.1,
-                            30.2,
-                            40.3
-                          ]
-                        },
-                        {
-                          "name": "y",
-                          "unit": "fm",
-                          "label": "Position along y-axis",
-                          "data_type": "int8",
-                          "axes": [
-                            "y"
-                          ],
-                          "shape": [
-                            3
-                          ],
-                          "data": [
-                            9,
-                            6,
-                            3
-                          ]
-                        }
-                      ]
-                    }
-                  }
-                ]
-              }
-            ]
-          }
-        ]
-      }
-    ]
-  }
-]
-} )";
+std::string const example_json = R"(
+{
+	"children": [{
+		"name": "entry",
+		"type": "group",
+		"attributes": [{
+			"name": "NX_class",
+			"dtype": "string",
+			"values": "NXentry"
+		}],
+		"children": [{
+				"module": "dataset",
+				"config": {
+					"name": "title",
+					"values": "This is a title",
+					"type": "string"
+				}
+			},
+			{
+				"module": "mdat",
+				"config": {
+					"name": "start_time"
+				}
+			},
+			{
+				"module": "mdat",
+				"config": {
+					"name": "end_time"
+				}
+			},
+			{
+				"name": "instrument",
+				"type": "group",
+				"attributes": [{
+					"name": "NX_class",
+					"dtype": "string",
+					"values": "NXinstrument"
+				}],
+				"children": [{
+					"name": "mini_chopper",
+					"type": "group",
+					"attributes": [{
+						"name": "NX_class",
+						"dtype": "string",
+						"values": "NXdisk_chopper"
+					}],
+					"children": [{
+						"name": "delay",
+						"type": "group",
+						"attributes": [{
+							"name": "NX_class",
+							"dtype": "string",
+							"values": "NXlog"
+						}],
+						"children": [{
+							"module": "f144",
+							"config": {
+								"source": "delay:source:chopper",
+								"topic": "local_choppers",
+								"dtype": "double",
+                                                                "value_units": "ns"
+							}
+						}]
+					},{
+                                                "name": "speed",
+                                                "type": "group",
+                                                "attributes": [{
+                                                        "name": "NX_class",
+                                                        "dtype": "string",
+                                                        "values": "NXlog"
+                                                }],
+                                                "children": [{
+                                                        "module": "f144",
+                                                        "config": {
+                                                                "source": "speed:source:chopper",
+                                                                "topic": "local_motion",
+                                                                "dtype": "double",
+                                                                "value_units": "Hz"
+                                                        }
+                                                }]
+                                                }]
+				}]
+			}
+		]
+	}]
+}
+                             )";
 
 class FakeRegistrar : public Metrics::IRegistrar {
 public:
@@ -213,7 +138,12 @@ public:
       ++offset;
       return {Kafka::PollStatus::Message, std::move(temp)};
     }
-    return {Kafka::PollStatus::TimedOut, FileWriter::Msg()};
+    if (at_end_of_partition) {
+      return {Kafka::PollStatus::TimedOut, FileWriter::Msg()};
+    } else {
+      at_end_of_partition = true;
+      return {Kafka::PollStatus::EndOfPartition, FileWriter::Msg()};
+    }
   };
 
   void addPartitionAtOffset([[maybe_unused]] std::string const &Topic,
@@ -236,6 +166,7 @@ public:
   std::string topic;
   int32_t partition = 0;
   std::shared_ptr<std::vector<FileWriter::Msg>> messages;
+  bool at_end_of_partition = false;
 };
 
 class StubConsumerFactory : public Kafka::ConsumerFactoryInterface {
@@ -310,6 +241,27 @@ create_f144_message_double(std::string const &source, double value,
 }
 
 std::pair<std::unique_ptr<uint8_t[]>, size_t>
+create_f144_message_array_double(std::string const &source,
+                                 const std::vector<double> &values,
+                                 int64_t timestamp_ms) {
+  auto builder = flatbuffers::FlatBufferBuilder();
+  auto source_name_offset = builder.CreateString(source);
+  auto values_offset = CreateArrayDoubleDirect(builder, &values).Union();
+
+  f144_LogDataBuilder f144_builder(builder);
+  f144_builder.add_value(values_offset);
+  f144_builder.add_source_name(source_name_offset);
+  f144_builder.add_timestamp(timestamp_ms * 1000000);
+  f144_builder.add_value_type(Value::ArrayDouble);
+  Finishf144_LogDataBuffer(builder, f144_builder.Finish());
+
+  size_t buffer_size = builder.GetSize();
+  auto buffer = std::make_unique<uint8_t[]>(buffer_size);
+  std::memcpy(buffer.get(), builder.GetBufferPointer(), buffer_size);
+  return {std::move(buffer), buffer_size};
+}
+
+std::pair<std::unique_ptr<uint8_t[]>, size_t>
 create_ep01_message_double(std::string const &source, ConnectionInfo status,
                            int64_t timestamp_ms) {
   auto builder = flatbuffers::FlatBufferBuilder();
@@ -328,43 +280,29 @@ create_ep01_message_double(std::string const &source, ConnectionInfo status,
 }
 
 std::pair<std::unique_ptr<uint8_t[]>, size_t>
-create_da00_message_int32s(std::string const &source, int64_t timestamp_ms,
-                           const std::vector<int32_t> &data) {
+create_ev44_message(std::string const &source, int64_t message_id,
+                    int64_t timestamp_ns,
+                    std::vector<int32_t> const &time_of_flight,
+                    std::vector<int32_t> const &pixel_ids) {
   auto builder = flatbuffers::FlatBufferBuilder();
-  builder.ForceDefaults(true);
-
   auto source_name_offset = builder.CreateString(source);
-  auto var_name_offset = builder.CreateString("value");
 
-  auto var_axis = builder.CreateString("x");
-  std::vector<flatbuffers::Offset<flatbuffers::String>> var_axes_offset = {
-      var_axis};
-  auto var_axes = builder.CreateVector(var_axes_offset);
+  std::vector<int64_t> reference_times = {timestamp_ns};
+  auto reference_time_offset = builder.CreateVector(reference_times);
+  std::vector<int32_t> reference_index = {0};
+  auto reference_index_offset = builder.CreateVector(reference_index);
 
-  std::vector<int64_t> var_shape = {static_cast<int64_t>(data.size())};
-  auto var_shape_offset = builder.CreateVector(var_shape);
+  auto time_of_flight_offset = builder.CreateVector(time_of_flight);
+  auto pixel_ids_offset = builder.CreateVector(pixel_ids);
 
-  std::uint8_t *p_data;
-  auto payload =
-      builder.CreateUninitializedVector(data.size(), sizeof(data[0]), &p_data);
-  std::memcpy(p_data, data.data(), sizeof(data[0]) * data.size());
-
-  auto variable_offset =
-      Createda00_Variable(builder, var_name_offset, 0, 0, 0, da00_dtype::int32,
-                          var_axes, var_shape_offset, payload);
-  std::vector<flatbuffers::Offset<da00_Variable>> variable_offsets = {
-      variable_offset};
-  auto variables = builder.CreateVector(variable_offsets);
-
-  auto da00 = Createda00_DataArray(builder, source_name_offset, timestamp_ms,
-                                   variables);
-  builder.Finish(da00, "da00");
-
-  auto verifier =
-      flatbuffers::Verifier(builder.GetBufferPointer(), builder.GetSize());
-  if (!Verifyda00_DataArrayBuffer(verifier)) {
-    throw std::runtime_error("could not verify da00");
-  }
+  Event44MessageBuilder ev44_builder(builder);
+  ev44_builder.add_source_name(source_name_offset);
+  ev44_builder.add_message_id(message_id);
+  ev44_builder.add_reference_time(reference_time_offset);
+  ev44_builder.add_reference_time_index(reference_index_offset);
+  ev44_builder.add_time_of_flight(time_of_flight_offset);
+  ev44_builder.add_pixel_id(pixel_ids_offset);
+  FinishEvent44MessageBuffer(builder, ev44_builder.Finish());
 
   size_t buffer_size = builder.GetSize();
   auto buffer = std::make_unique<uint8_t[]>(buffer_size);
@@ -406,24 +344,20 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char **argv) {
   msg = create_f144_message_double("delay:source:chopper", 101, 1100);
   add_message(consumer_factory.get(), std::move(msg), 1100ms, offset++, 0);
 
-  msg = create_f144_message_double("speed:source:chopper", 1000, 1200);
+  msg = create_f144_message_array_double("speed:source:chopper",
+                                         {1000, 1010, 1020}, 1200);
   add_message(consumer_factory.get(), std::move(msg), 1200ms, offset++, 0);
 
   msg = create_ep01_message_double("speed:source:chopper",
                                    ConnectionInfo::CONNECTED, 1201);
   add_message(consumer_factory.get(), std::move(msg), 1201ms, offset++, 0);
 
-  msg = create_f144_message_double("speed:source:chopper", 1010, 1250);
+  msg = create_f144_message_array_double("speed:source:chopper",
+                                         {2000, 2010, 2020}, 1250);
   add_message(consumer_factory.get(), std::move(msg), 1250ms, offset++, 0);
 
   msg = create_f144_message_double("delay:source:chopper", 102, 2100);
   add_message(consumer_factory.get(), std::move(msg), 2100ms, offset++, 0);
-
-  msg = create_da00_message_int32s("fake:da00:source", 2200, {0, 1, 2, 3, 4});
-  add_message(consumer_factory.get(), std::move(msg), 2200ms, offset++, 0);
-
-  msg = create_da00_message_int32s("fake:da00:source", 2300, {5, 6, 7, 8, 9});
-  add_message(consumer_factory.get(), std::move(msg), 2300ms, offset++, 0);
 
   Command::StartInfo start_info;
   start_info.NexusStructure = example_json;
