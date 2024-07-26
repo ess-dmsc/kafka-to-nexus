2795694b4aa8464dc568b5639e2ff6d08cab33c3
Matt Clarke <matt.clarke@ess.eu>
2024-07-26 11:34:20 +0200

    clang formatting

--- tests/Stream/SourceFilterTest.cpp
+++ tests/Stream/SourceFilterTest.cpp
@@ -10,8 +10,8 @@
 #include "Stream/SourceFilter.h"
 #include "WriterModule/f144/f144_Writer.h"
 #include <chrono>
-#include <gtest/gtest.h>
 #include <f144_logdata_generated.h>
+#include <gtest/gtest.h>
 
 class StubMessageWriter : public Stream::MessageWriter {
 public:
@@ -43,8 +43,11 @@ create_f144_message_double(std::string const &source, double value,
   return {std::move(buffer), buffer_size};
 }
 
-FileWriter::FlatbufferMessage create_f144_message(std::string const &source, double value, int64_t timestamp_ms) {
-  auto const [buffer, size] = create_f144_message_double(source, value, timestamp_ms);
+FileWriter::FlatbufferMessage create_f144_message(std::string const &source,
+                                                  double value,
+                                                  int64_t timestamp_ms) {
+  auto const [buffer, size] =
+      create_f144_message_double(source, value, timestamp_ms);
   return {buffer.get(), size};
 }
 
@@ -52,7 +55,8 @@ TEST(SourceFilter, messages_within_start_and_stop_are_allowed_through) {
   auto writer = std::make_unique<StubMessageWriter>();
   auto registrar = std::make_unique<Metrics::Registrar>("");
   auto f144_writer = std::make_unique<WriterModule::f144::f144_Writer>();
-  Stream::SourceFilter filter{time_point{0ms}, time_point::max(), false, writer.get(), std::move(registrar)};
+  Stream::SourceFilter filter{time_point{0ms}, time_point::max(), false,
+                              writer.get(), std::move(registrar)};
   filter.add_writer_module_for_message(f144_writer.get());
 
   filter.filter_message(create_f144_message("::source::", 1, 100));
@@ -65,7 +69,8 @@ TEST(SourceFilter, out_of_order_messages_are_allowed_through) {
   auto writer = std::make_unique<StubMessageWriter>();
   auto registrar = std::make_unique<Metrics::Registrar>("");
   auto f144_writer = std::make_unique<WriterModule::f144::f144_Writer>();
-  Stream::SourceFilter filter{time_point{0ms}, time_point::max(), false, writer.get(), std::move(registrar)};
+  Stream::SourceFilter filter{time_point{0ms}, time_point::max(), false,
+                              writer.get(), std::move(registrar)};
   filter.add_writer_module_for_message(f144_writer.get());
 
   filter.filter_message(create_f144_message("::source::", 2, 200));
@@ -78,7 +83,8 @@ TEST(SourceFilter, invalid_message_is_filtered_out) {
   auto writer = std::make_unique<StubMessageWriter>();
   auto registrar = std::make_unique<Metrics::Registrar>("");
   auto f144_writer = std::make_unique<WriterModule::f144::f144_Writer>();
-  Stream::SourceFilter filter{time_point{0ms}, time_point::max(), false, writer.get(), std::move(registrar)};
+  Stream::SourceFilter filter{time_point{0ms}, time_point::max(), false,
+                              writer.get(), std::move(registrar)};
   filter.add_writer_module_for_message(f144_writer.get());
 
   FileWriter::FlatbufferMessage invalid;
@@ -91,7 +97,8 @@ TEST(SourceFilter, message_before_start_is_not_allowed_through) {
   auto writer = std::make_unique<StubMessageWriter>();
   auto registrar = std::make_unique<Metrics::Registrar>("");
   auto f144_writer = std::make_unique<WriterModule::f144::f144_Writer>();
-  Stream::SourceFilter filter{time_point{1000ms}, time_point::max(), false, writer.get(), std::move(registrar)};
+  Stream::SourceFilter filter{time_point{1000ms}, time_point::max(), false,
+                              writer.get(), std::move(registrar)};
   filter.add_writer_module_for_message(f144_writer.get());
 
   filter.filter_message(create_f144_message("::source::", 1, 100));
@@ -103,7 +110,8 @@ TEST(SourceFilter, message_on_start_is_allowed_through) {
   auto writer = std::make_unique<StubMessageWriter>();
   auto registrar = std::make_unique<Metrics::Registrar>("");
   auto f144_writer = std::make_unique<WriterModule::f144::f144_Writer>();
-  Stream::SourceFilter filter{time_point{1000ms}, time_point::max(), false, writer.get(), std::move(registrar)};
+  Stream::SourceFilter filter{time_point{1000ms}, time_point::max(), false,
+                              writer.get(), std::move(registrar)};
   filter.add_writer_module_for_message(f144_writer.get());
 
   filter.filter_message(create_f144_message("::source::", 1, 1000));
@@ -115,7 +123,8 @@ TEST(SourceFilter, first_message_after_stop_is_allowed_through) {
   auto writer = std::make_unique<StubMessageWriter>();
   auto registrar = std::make_unique<Metrics::Registrar>("");
   auto f144_writer = std::make_unique<WriterModule::f144::f144_Writer>();
-  Stream::SourceFilter filter{time_point{0ms}, time_point{1000ms}, false, writer.get(), std::move(registrar)};
+  Stream::SourceFilter filter{time_point{0ms}, time_point{1000ms}, false,
+                              writer.get(), std::move(registrar)};
   filter.add_writer_module_for_message(f144_writer.get());
 
   filter.filter_message(create_f144_message("::source::", 1, 1001));
@@ -128,7 +137,8 @@ TEST(SourceFilter, message_after_stop_sets_filter_to_finished) {
   auto writer = std::make_unique<StubMessageWriter>();
   auto registrar = std::make_unique<Metrics::Registrar>("");
   auto f144_writer = std::make_unique<WriterModule::f144::f144_Writer>();
-  Stream::SourceFilter filter{time_point{0ms}, time_point{1000ms}, false, writer.get(), std::move(registrar)};
+  Stream::SourceFilter filter{time_point{0ms}, time_point{1000ms}, false,
+                              writer.get(), std::move(registrar)};
   filter.add_writer_module_for_message(f144_writer.get());
 
   filter.filter_message(create_f144_message("::source::", 1, 1001));
@@ -136,12 +146,15 @@ TEST(SourceFilter, message_after_stop_sets_filter_to_finished) {
   EXPECT_EQ(true, filter.has_finished());
 }
 
-TEST(SourceFilter, messages_with_same_timestamp_ignored_when_allowed_repeated_is_false) {
+TEST(SourceFilter,
+     messages_with_same_timestamp_ignored_when_allowed_repeated_is_false) {
   auto allow_repeated_timestamps = false;
   auto writer = std::make_unique<StubMessageWriter>();
   auto registrar = std::make_unique<Metrics::Registrar>("");
   auto f144_writer = std::make_unique<WriterModule::f144::f144_Writer>();
-  Stream::SourceFilter filter{time_point{0ms}, time_point::max(), allow_repeated_timestamps, writer.get(), std::move(registrar)};
+  Stream::SourceFilter filter{time_point{0ms}, time_point::max(),
+                              allow_repeated_timestamps, writer.get(),
+                              std::move(registrar)};
   filter.add_writer_module_for_message(f144_writer.get());
 
   filter.filter_message(create_f144_message("::source::", 1, 1000));
@@ -151,12 +164,15 @@ TEST(SourceFilter, messages_with_same_timestamp_ignored_when_allowed_repeated_is
   EXPECT_EQ(1u, writer->messages_received.size());
 }
 
-TEST(SourceFilter, messages_with_same_timestamp_allowed_when_allowed_repeated_is_true) {
+TEST(SourceFilter,
+     messages_with_same_timestamp_allowed_when_allowed_repeated_is_true) {
   auto allow_repeated_timestamps = true;
   auto writer = std::make_unique<StubMessageWriter>();
   auto registrar = std::make_unique<Metrics::Registrar>("");
   auto f144_writer = std::make_unique<WriterModule::f144::f144_Writer>();
-  Stream::SourceFilter filter{time_point{0ms}, time_point::max(), allow_repeated_timestamps, writer.get(), std::move(registrar)};
+  Stream::SourceFilter filter{time_point{0ms}, time_point::max(),
+                              allow_repeated_timestamps, writer.get(),
+                              std::move(registrar)};
   filter.add_writer_module_for_message(f144_writer.get());
 
   filter.filter_message(create_f144_message("::source::", 1, 1000));
@@ -170,7 +186,8 @@ TEST(SourceFilter, can_change_stop_time_after_construction) {
   auto writer = std::make_unique<StubMessageWriter>();
   auto registrar = std::make_unique<Metrics::Registrar>("");
   auto f144_writer = std::make_unique<WriterModule::f144::f144_Writer>();
-  Stream::SourceFilter filter{time_point{0ms}, time_point::max(), false, writer.get(), std::move(registrar)};
+  Stream::SourceFilter filter{time_point{0ms}, time_point::max(), false,
+                              writer.get(), std::move(registrar)};
   filter.add_writer_module_for_message(f144_writer.get());
 
   filter.set_stop_time(time_point{1000ms});
@@ -182,13 +199,16 @@ TEST(SourceFilter, can_change_stop_time_after_construction) {
   EXPECT_EQ(1u, writer->messages_received.size());
 }
 
-TEST(SourceFilter, last_message_before_start_time_is_allowed_through_after_valid_message) {
-  // For values that don't update very often, the forwarder periodically sends the current value to Kafka.
-  // This is to ensure that the data file contains the value despite it not changing.
+TEST(SourceFilter,
+     last_message_before_start_time_is_allowed_through_after_valid_message) {
+  // For values that don't update very often, the forwarder periodically sends
+  // the current value to Kafka. This is to ensure that the data file contains
+  // the value despite it not changing.
   auto writer = std::make_unique<StubMessageWriter>();
   auto registrar = std::make_unique<Metrics::Registrar>("");
   auto f144_writer = std::make_unique<WriterModule::f144::f144_Writer>();
-  Stream::SourceFilter filter{time_point{1000ms}, time_point::max(), false, writer.get(), std::move(registrar)};
+  Stream::SourceFilter filter{time_point{1000ms}, time_point::max(), false,
+                              writer.get(), std::move(registrar)};
   filter.add_writer_module_for_message(f144_writer.get());
 
   filter.filter_message(create_f144_message("::source::", 1, 100));
@@ -201,13 +221,16 @@ TEST(SourceFilter, last_message_before_start_time_is_allowed_through_after_valid
   EXPECT_EQ("some_source", first.getSourceName());
 }
 
-TEST(SourceFilter, last_message_before_start_time_handles_out_of_order_messages) {
-  // For values that don't update very often, the forwarder periodically sends the current value to Kafka.
-  // This is to ensure that the data file contains the value despite it not changing.
+TEST(SourceFilter,
+     last_message_before_start_time_handles_out_of_order_messages) {
+  // For values that don't update very often, the forwarder periodically sends
+  // the current value to Kafka. This is to ensure that the data file contains
+  // the value despite it not changing.
   auto writer = std::make_unique<StubMessageWriter>();
   auto registrar = std::make_unique<Metrics::Registrar>("");
   auto f144_writer = std::make_unique<WriterModule::f144::f144_Writer>();
-  Stream::SourceFilter filter{time_point{1000ms}, time_point::max(), false, writer.get(), std::move(registrar)};
+  Stream::SourceFilter filter{time_point{1000ms}, time_point::max(), false,
+                              writer.get(), std::move(registrar)};
   filter.add_writer_module_for_message(f144_writer.get());
 
   filter.filter_message(create_f144_message("some_source", 2, 200));
@@ -220,9 +243,12 @@ TEST(SourceFilter, last_message_before_start_time_handles_out_of_order_messages)
   EXPECT_EQ("some_source", first.getSourceName());
 }
 
-TEST(SourceFilter, last_message_before_start_time_is_allowed_through_on_destruction_if_no_updates) {
-  // For values that don't update very often, the forwarder periodically sends the current value to Kafka.
-  // This is to ensure that the data file contains the value despite it not changing.
+TEST(
+    SourceFilter,
+    last_message_before_start_time_is_allowed_through_on_destruction_if_no_updates) {
+  // For values that don't update very often, the forwarder periodically sends
+  // the current value to Kafka. This is to ensure that the data file contains
+  // the value despite it not changing.
   auto writer = std::make_unique<StubMessageWriter>();
   auto registrar = std::make_unique<Metrics::Registrar>("");
   auto f144_writer = std::make_unique<WriterModule::f144::f144_Writer>();
@@ -240,9 +266,12 @@ TEST(SourceFilter, last_message_before_start_time_is_allowed_through_on_destruct
   EXPECT_EQ("some_source", first.getSourceName());
 }
 
-TEST(SourceFilter, last_message_before_start_time_is_not_allowed_through_on_destruction_if_already_written) {
-  // For values that don't update very often, the forwarder periodically sends the current value to Kafka.
-  // This is to ensure that the data file contains the value despite it not changing.
+TEST(
+    SourceFilter,
+    last_message_before_start_time_is_not_allowed_through_on_destruction_if_already_written) {
+  // For values that don't update very often, the forwarder periodically sends
+  // the current value to Kafka. This is to ensure that the data file contains
+  // the value despite it not changing.
   auto writer = std::make_unique<StubMessageWriter>();
   auto registrar = std::make_unique<Metrics::Registrar>("");
   auto f144_writer = std::make_unique<WriterModule::f144::f144_Writer>();
@@ -266,7 +295,8 @@ TEST(SourceFilter, messages_written_for_each_module_when_more_than_one_module) {
   auto registrar = std::make_unique<Metrics::Registrar>("");
   auto f144_writer_1 = std::make_unique<WriterModule::f144::f144_Writer>();
   auto f144_writer_2 = std::make_unique<WriterModule::f144::f144_Writer>();
-  Stream::SourceFilter filter{time_point{0ms}, time_point::max(), false, writer.get(), std::move(registrar)};
+  Stream::SourceFilter filter{time_point{0ms}, time_point::max(), false,
+                              writer.get(), std::move(registrar)};
   filter.add_writer_module_for_message(f144_writer_1.get());
   filter.add_writer_module_for_message(f144_writer_2.get());
 
