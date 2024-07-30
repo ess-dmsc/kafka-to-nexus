bcb3d1170350c3eddc9df37bfc1945bd8d7af38b
Matt Clarke <matt.clarke@ess.eu>
2024-07-30 10:18:18 +0200

    fixed local names to match new convention

--- src/Stream/PartitionFilter.cpp
+++ src/Stream/PartitionFilter.cpp
@@ -12,54 +12,54 @@
 
 namespace Stream {
 
-PartitionFilter::PartitionFilter(time_point StopAtTime, duration StopTimeLeeway,
-                                 duration TimeLimit)
-    : StopTime(StopAtTime), StopLeeway(StopTimeLeeway), TimeLimit(TimeLimit) {
+PartitionFilter::PartitionFilter(time_point stop_time, duration stop_time_leeway,
+                                 duration time_limit)
+    : _stop_time(stop_time), _stop_leeway(stop_time_leeway), _time_limit(time_limit) {
   // Deal with potential overflow problem
-  if (time_point::max() - StopTime <= StopTimeLeeway) {
-    StopTime -= StopTimeLeeway;
+  if (time_point::max() - _stop_time <= stop_time_leeway) {
+    _stop_time -= stop_time_leeway;
   }
 }
 
 bool PartitionFilter::hasExceededTimeLimit() const {
-  return std::chrono::system_clock::now() > StatusOccurrenceTime + TimeLimit;
+  return std::chrono::system_clock::now() > _status_occurrence_time + _time_limit;
 }
 
 bool PartitionFilter::hasTopicTimedOut() const {
-  return hasExceededTimeLimit() && State == PartitionState::TIMEOUT;
+  return hasExceededTimeLimit() && _state == PartitionState::TIMEOUT;
 }
 
 void PartitionFilter::updateStatusOccurrenceTime(
-    PartitionState ComparisonState) {
-  if (State != ComparisonState) {
-    State = ComparisonState;
-    StatusOccurrenceTime = std::chrono::system_clock::now();
+    PartitionState comparison_state) {
+  if (_state != comparison_state) {
+    _state = comparison_state;
+    _status_occurrence_time = std::chrono::system_clock::now();
   }
 }
 
-void PartitionFilter::forceStop() { ForceStop = true; }
+void PartitionFilter::forceStop() { _force_stop = true; }
 
-bool PartitionFilter::hasForceStopBeenRequested() const { return ForceStop; }
+bool PartitionFilter::hasForceStopBeenRequested() const { return _force_stop; }
 
-bool PartitionFilter::shouldStopPartition(Kafka::PollStatus CurrentPollStatus) {
+bool PartitionFilter::shouldStopPartition(Kafka::PollStatus current_poll_status) {
   if (hasForceStopBeenRequested()) {
     return true;
   }
-  switch (CurrentPollStatus) {
+  switch (current_poll_status) {
   case Kafka::PollStatus::Message:
     at_end_of_partition_ = false;
-    State = PartitionState::DEFAULT;
+    _state = PartitionState::DEFAULT;
     return false;
   case Kafka::PollStatus::EndOfPartition:
     at_end_of_partition_ = true;
-    State = PartitionState::END_OF_PARTITION;
+    _state = PartitionState::END_OF_PARTITION;
     return false;
   case Kafka::PollStatus::TimedOut:
     updateStatusOccurrenceTime(PartitionState::TIMEOUT);
     if (!at_end_of_partition_) {
       return false;
     }
-    return std::chrono::system_clock::now() > StopTime + StopLeeway;
+    return std::chrono::system_clock::now() > _stop_time + _stop_leeway;
   case Kafka::PollStatus::Error:
     updateStatusOccurrenceTime(PartitionState::ERROR);
     return hasExceededTimeLimit();
