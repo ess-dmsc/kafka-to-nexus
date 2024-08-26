8d3e0dbd07d835effd4b85658708e65c13999fae
Matt Clarke <matt.clarke@ess.eu>
2024-08-26 14:35:53 +0200

    added test file

---
+++ tests/JsonToFlatbuffersTests.cpp
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: BSD-2-Clause
+//
+// This code has been produced by the European Spallation Source
+// and its partner institutes under the BSD 2 Clause License.
+//
+// See LICENSE.md at the top level for license information.
+//
+// Screaming Udder!                              https://esss.se
+
+#include <gtest/gtest.h>
+
+TEST(json_to_fb, test) {
+  ASSERT_TRUE(true);
+}
