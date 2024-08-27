9b063c3c29f9067ac103d5063fae22f15acf81c6
Matt Clarke <matt.clarke@ess.eu>
2024-08-27 08:00:15 +0200

    fixed duplicate function declaration

--- src/Master.cpp
+++ src/Master.cpp
@@ -14,13 +14,6 @@
 #include <functional>
 namespace FileWriter {
 
-std::filesystem::path construct_filepath(std::filesystem::path const &prefix,
-                                         std::string const &filename);
-
-std::filesystem::path
-construct_template_path(std::filesystem::path const &prefix,
-                        std::string const &instrument_name);
-
 Master::Master(MainOpt &Config, std::unique_ptr<Command::HandlerBase> Listener,
                std::unique_ptr<Status::StatusReporterBase> Reporter,
                std::unique_ptr<Metrics::IRegistrar> Registrar)
@@ -81,14 +74,15 @@ void Master::startWriting(Command::StartMessage const &StartInfo) {
   }
 }
 
-std::filesystem::path construct_filepath(std::filesystem::path const &prefix,
-                                         std::string const &filename) {
+std::filesystem::path
+Master::construct_filepath(std::filesystem::path const &prefix,
+                           std::string const &filename) {
   return prefix / std::filesystem::path(filename).relative_path();
 }
 
 std::filesystem::path
-construct_template_path(std::filesystem::path const &prefix,
-                        std::string const &instrument_name) {
+Master::construct_template_path(std::filesystem::path const &prefix,
+                                std::string const &instrument_name) {
   std::filesystem::path local_template_path =
       fmt::format("nexus_templates/{0}/{0}.hdf", instrument_name);
   return prefix / local_template_path;
