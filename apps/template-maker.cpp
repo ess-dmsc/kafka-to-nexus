57402554206ba952bfc0e60a9cf1e723902a3766
Jonas Petersson <jonas.petersson@ess.eu>
2024-09-03 16:14:20 +0200

    fix removed faketracker

--- apps/template-maker.cpp
+++ apps/template-maker.cpp
@@ -35,18 +35,6 @@ public:
   }
 };
 
-class FakeTracker : public MetaData::ITracker {
-public:
-  void
-  registerMetaData([[maybe_unused]] MetaData::ValueBase NewMetaData) override {}
-  void clearMetaData() override {}
-  void
-  writeToJSONDict([[maybe_unused]] nlohmann::json &JSONNode) const override {}
-  void
-  writeToHDF5File([[maybe_unused]] hdf5::node::Group &RootNode) const override {
-  }
-};
-
 int main([[maybe_unused]] int argc, [[maybe_unused]] char **argv) {
   CLI::App app{"template-maker app"};
   std::string json_file;
@@ -59,7 +47,7 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char **argv) {
 
   std::unique_ptr<Metrics::IRegistrar> registrar =
       std::make_unique<FakeRegistrar>();
-  auto tracker = std::make_shared<FakeTracker>();
+  auto tracker = std::make_shared<MetaData::Tracker>();
 
   Command::StartMessage start_info;
 
