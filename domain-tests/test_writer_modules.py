002316f30a1b12fde6db12928640e434f335a64c
Matt Clarke <matt.clarke@ess.eu>
2024-11-13 16:31:36 +0100

    repeated timestamps are tested in the domain tests

--- domain-tests/test_writer_modules.py
+++ domain-tests/test_writer_modules.py
@@ -9,7 +9,7 @@ from conftest import write_file
 def local_file(request):
     return write_file(
         request,
-        "writer_modules.hdf",
+        "output_files/writer_modules.hdf",
         "nexus_templates/writer_modules_template.json",
         "data_files/writer_modules_data.json",
     )
