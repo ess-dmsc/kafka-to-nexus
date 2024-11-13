c70386e3f2334a247330c7774cf639f2864bbea0
Matt Clarke <matt.clarke@ess.eu>
2024-11-13 17:08:33 +0100

    domain test for data before and after

---
+++ domain-tests/test_data_before_and_after.py
@@ -0,0 +1,27 @@
+import h5py
+import numpy as np
+import pytest
+from conftest import write_file
+
+
+# This fixture is used to create the file
+@pytest.fixture(scope="module")
+def local_file(request):
+    return write_file(
+        request,
+        "output_files/data_before_and_after.hdf",
+        "nexus_templates/data_before_and_after.json",
+        "data_files/data_before_and_after.json",
+    )
+
+
+def test_last_data_before_start_is_written_but_earlier_values_ignored(local_file):
+    with h5py.File(local_file, "r") as f:
+        assert f["/entry/instrument/chopper/delay/value"][0] == 5
+        assert f["/entry/instrument/chopper/delay/time"][0] == 9_999_000_000
+
+
+def test_first_data_after_stop_written_but_later_values_ignored(local_file):
+    with h5py.File(local_file, "r") as f:
+        assert f["/entry/instrument/chopper/delay/value"][~0] == 17
+        assert f["/entry/instrument/chopper/delay/time"][~0] == 16_000_000_000
