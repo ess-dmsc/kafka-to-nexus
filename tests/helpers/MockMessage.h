199ae932c44834ced55e028ddbd001861482587b
George O'Neill <george.oneill@ess.eu>
2025-02-17 14:56:42 +0100

    update of mock message for compilability

--- tests/helpers/MockMessage.h
+++ tests/helpers/MockMessage.h
@@ -13,23 +13,25 @@
 
 class MockMessage : public RdKafka::Message {
 public:
-  MAKE_CONST_MOCK0(errstr, std::string());
-  MAKE_CONST_MOCK0(err, RdKafka::ErrorCode());
-  MAKE_CONST_MOCK0(topic, RdKafka::Topic *());
-  MAKE_CONST_MOCK0(topic_name, std::string());
-  MAKE_CONST_MOCK0(partition, int32_t());
-  MAKE_CONST_MOCK0(payload, void *());
-  MAKE_CONST_MOCK0(len, size_t());
-  MAKE_CONST_MOCK0(key, const std::string *());
-  MAKE_CONST_MOCK0(key_pointer, const void *());
-  MAKE_CONST_MOCK0(key_len, size_t());
-  MAKE_CONST_MOCK0(offset, int64_t());
-  MAKE_CONST_MOCK0(timestamp, RdKafka::MessageTimestamp());
-  MAKE_CONST_MOCK0(msg_opaque, void *());
-  MAKE_CONST_MOCK0(latency, int64_t());
-  MAKE_CONST_MOCK0(status, RdKafka::Message::Status());
-  MAKE_CONST_MOCK0(broker_id, int32_t());
-  MAKE_MOCK0(headers, RdKafka::Headers *());
-  MAKE_MOCK1(headers, RdKafka::Headers *(RdKafka::ErrorCode *));
-  MAKE_MOCK0(c_ptr, rd_kafka_message_s *());
+  MAKE_CONST_MOCK0(errstr, std::string(), override);
+  MAKE_CONST_MOCK0(err, RdKafka::ErrorCode(), override);
+  MAKE_CONST_MOCK0(topic, RdKafka::Topic *(), override);
+  MAKE_CONST_MOCK0(topic_name, std::string(), override);
+  MAKE_CONST_MOCK0(partition, int32_t(), override);
+  MAKE_CONST_MOCK0(payload, void *(), override);
+  MAKE_CONST_MOCK0(len, size_t(), override);
+  MAKE_CONST_MOCK0(key, const std::string *(), override);
+  MAKE_CONST_MOCK0(key_pointer, const void *(), override);
+  MAKE_CONST_MOCK0(key_len, size_t(), override);
+  MAKE_CONST_MOCK0(offset, int64_t(), override);
+  MAKE_CONST_MOCK0(timestamp, RdKafka::MessageTimestamp(), override);
+  MAKE_CONST_MOCK0(msg_opaque, void *(), override);
+  MAKE_CONST_MOCK0(latency, int64_t(), override);
+  MAKE_CONST_MOCK0(status, RdKafka::Message::Status(), override);
+  MAKE_CONST_MOCK0(broker_id, int32_t(), override);
+  MAKE_CONST_MOCK0(leader_epoch, int32_t(), override);
+  MAKE_MOCK0(headers, RdKafka::Headers *(), override);
+  MAKE_MOCK1(headers, RdKafka::Headers *(RdKafka::ErrorCode *), override);
+  MAKE_MOCK0(c_ptr, rd_kafka_message_s *(), override);
+  MAKE_MOCK0(offset_store, RdKafka::Error *(), override);
 };
