645ca75ad8000d713d1d18785359122024e0d4d2
George O'Neill <george.oneill@ess.eu>
2025-05-22 20:34:32 +0200

    server first pass

--- src/Metrics/Registrar.cpp
+++ src/Metrics/Registrar.cpp
@@ -23,6 +23,42 @@ void Registrar::registerMetric(Metric &NewMetric,
   }
 }
 
+void Registrar::initServer(){
+	sockaddr_in address{};
+	int opt = 1, addrlen = sizeof(address);
+
+	server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_fd < 0)
+		throw std::runtime_error("Failed to init socket!");
+
+	setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;	//	restrict to 10.100.x.y?
+	address.sin_port = htons(9999);
+
+	if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0)
+		throw std::runtime_error("Failed to bind to port");
+
+	if (listen(server_fd, 3) < 0)
+		throw std::runtime_error("Failed to listen");
+
+	while (true){	//	threaded connections?
+		client_fd = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
+		std::string response = "";
+		for (auto const &reporter : ReporterList) {
+			for (auto const &MetricNameValue : reporter->getMetrics()) {
+				response += MetricNameValue.second.Name + ": " + MetricNameValue.second.Value + "\n";
+			}
+		}
+		send(client_fd, response.c_str(), response.size(), 0);
+		close(client_fd);
+	}
+}
+
+void Registrar::killServer(){	//	where/when/should we even need to call this?
+	close(server_fd);
+}
+
 std::string Registrar::queryMetric(Metric &Metric) const {
 	std::string StrValue = Metric.getStringValue();
 	if(StrValue != "") return StrValue;
