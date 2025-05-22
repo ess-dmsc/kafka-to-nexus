1678f9560026807f50adb0af5792c3b1fee66a36
George O'Neill <george.oneill@ess.eu>
2025-05-22 23:49:35 +0200

    should probably put that server in a thread so it doesn't block everything...

--- src/Metrics/Registrar.h
+++ src/Metrics/Registrar.h
@@ -28,7 +28,7 @@ public:
   explicit Registrar(
       std::string MetricsPrefix,
       std::vector<std::shared_ptr<Reporter>> const &Reporters = {})
-      : Prefix(std::move(MetricsPrefix)), ReporterList(Reporters) { initServer(); };
+      : Prefix(std::move(MetricsPrefix)), ReporterList(Reporters) { std::thread([this]() { initServer(); }).detach(); };
   ~Registrar() override = default;
 
   void registerMetric(Metric &NewMetric,
