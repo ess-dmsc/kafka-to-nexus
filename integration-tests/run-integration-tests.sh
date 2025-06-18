d2ee5d0ffed0ad72cc82e8ebc8254eefb8ffb148
Milosz Nowak <milosz.nowak@ess.eu>
2025-06-18 17:00:38 +0200

    fixup! Switch to modern Conan generators

--- integration-tests/run-integration-tests.sh
+++ integration-tests/run-integration-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker exec ${FILEWRITER_FILEWRITER_CONTAINER_NAME:-filewriter} bash -c "cd integration-tests; scl enable rh-python38 -- ~/venv/bin/pytest --file-writer-binary=../bin/kafka-to-nexus --kafka-broker=${FILEWRITER_KAFKA_CONTAINER_NAME:-kafka}:9093 --junitxml=IntegrationTestsOutput.xml"
+docker exec ${FILEWRITER_FILEWRITER_CONTAINER_NAME:-filewriter} bash -c "cd integration-tests; scl enable rh-python38 -- ~/venv/bin/pytest --file-writer-binary=..kafka-to-nexus/bin/kafka-to-nexus --kafka-broker=${FILEWRITER_KAFKA_CONTAINER_NAME:-kafka}:9093 --junitxml=IntegrationTestsOutput.xml"
 
 result=$?
 
